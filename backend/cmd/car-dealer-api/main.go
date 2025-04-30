933f002f500725d7aaf5258756fc75c5db507e90
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-04-30 23:27:23 +0200

    Quick demo implemented.

--- backend/cmd/car-dealer-api/main.go
+++ backend/cmd/car-dealer-api/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("Hello World!")
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run() // listen and serve on 0.0.0.0:8080
 }
