a02e35f242c1bfbb1adb245982b2a1d25516a0ed
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-05-01 01:58:07 +0200

    Implemented basic demo how generic repository can be used.

--- backend/cmd/car-dealer-api/main.go
+++ backend/cmd/car-dealer-api/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/susek555/BD2/car-dealer-api/internal/domains/user"
 	"github.com/susek555/BD2/car-dealer-api/internal/initializers"
 )
 
@@ -13,10 +17,16 @@ func init() {
 
 func main() {
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	router.GET("/users/all", getUsers)
 	router.Run() // listen and serve on 0.0.0.0:8080
+}
+
+// This code will be in UserController
+func getUsers(c *gin.Context) {
+	userRepository := user.GetUserRepository(initializers.DB)
+	users, err := userRepository.GetAllUsers()
+	if err != nil {
+		log.Fatal("Something went wrong")
+	}
+	c.IndentedJSON(http.StatusOK, users)
 }
