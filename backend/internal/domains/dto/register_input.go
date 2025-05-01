b65b019037407d2dafa5774817dd6ef0f153b75f
KamilMarszalek <kamilmar553@gmail.com>
2025-05-02 00:24:37 +0200

    Created RegisterInput

---
+++ backend/internal/domains/dto/register_input.go
@@ -0,0 +1,7 @@
+package dto
+
+type RegisterInput struct {
+	Username string `json:"username" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=8"`
+}
