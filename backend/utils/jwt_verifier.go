3b5b199a45bf4d68fc8950122a6431f7b156926c
KamilMarszalek <kamilmar553@gmail.com>
2025-05-01 23:32:08 +0200

    Created JWT verifier

---
+++ backend/utils/jwt_verifier.go
@@ -0,0 +1,37 @@
+package utils
+
+import (
+	"errors"
+	"fmt"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type JWTVerifier struct {
+	secret []byte
+}
+
+func NewJWTVerifier(secret string) JWTVerifier {
+	return JWTVerifier{secret: []byte(secret)}
+}
+
+func (j *JWTVerifier) VerifyToken(token string) (int64, error) {
+	claims := &CustomClaims{}
+
+	parsed, err := jwt.ParseWithClaims(
+		token,
+		claims,
+		func(t *jwt.Token) (interface{}, error) { return j.secret, nil },
+		jwt.WithValidMethods([]string{jwt.SigningMethodHS256.Alg()}),
+	)
+	if err != nil {
+		if errors.Is(err, jwt.ErrTokenExpired) {
+			return 0, fmt.Errorf("token expired: %w", err)
+		}
+		return 0, fmt.Errorf("token parse error: %w", err)
+	}
+
+	if !parsed.Valid {
+		return 0, errors.New("invalid token")
+	}
+	return claims.UserID, nil
+}
