42b5d79d8756f79a6aa7b3bb9721778e76d2c85e
KamilMarszalek <kamilmar553@gmail.com>
2025-05-01 23:33:08 +0200

    Applied authentication middleware to router

--- backend/cmd/car-dealer-api/main.go
+++ backend/cmd/car-dealer-api/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"github.com/susek555/BD2/car-dealer-api/middleware"
+	"github.com/susek555/BD2/car-dealer-api/utils"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/susek555/BD2/car-dealer-api/internal/domains/user"
@@ -16,7 +19,9 @@ func init() {
 }
 
 func main() {
+	verifier := utils.NewJWTVerifier(os.Getenv("JWT_SECRET"))
 	router := gin.Default()
+	router.Use(middleware.Authenticate(verifier))
 	router.GET("/users/all", getUsers)
 	router.Run() // listen and serve on 0.0.0.0:8080
 }
