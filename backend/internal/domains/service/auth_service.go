2e2e6211eb88a6f212b01af07102e1cccd4392e9
KamilMarszalek <kamilmar553@gmail.com>
2025-05-02 00:39:40 +0200

    Implemented auth service

--- backend/internal/domains/service/auth_service.go
+++ backend/internal/domains/service/auth_service.go
@@ -1,1 +1,66 @@
 package service
+
+import (
+	"context"
+	"errors"
+	"github.com/susek555/BD2/car-dealer-api/internal/domains/dto"
+
+	"github.com/susek555/BD2/car-dealer-api/internal/domains/user"
+	"github.com/susek555/BD2/car-dealer-api/pkg/passwords"
+	"github.com/susek555/BD2/car-dealer-api/pkg/utils"
+)
+
+var (
+	ErrEmailTaken         = errors.New("email already in use")
+	ErrInvalidCredentials = errors.New("invalid email or password")
+)
+
+type Service interface {
+	Register(ctx context.Context, in dto.RegisterInput) (token string, err error)
+	Login(ctx context.Context, in dto.LoginInput) (token string, err error)
+}
+
+type service struct {
+	repo   user.UserRepositoryInterface
+	jwtKey []byte
+}
+
+func NewService(repo user.UserRepositoryInterface, jwtKey []byte) Service {
+	return &service{repo: repo, jwtKey: jwtKey}
+}
+
+func (s *service) Register(ctx context.Context, in dto.RegisterInput) (string, error) {
+	u, err := s.repo.GetUserByEmail(in.Email)
+	if err == nil && u.ID != 0 {
+		return "", ErrEmailTaken
+	}
+
+	hash, err := passwords.Hash(in.Password)
+	if err != nil {
+		return "", err
+	}
+
+	userModel := user.User{
+		Username: in.Username,
+		Email:    in.Email,
+		Password: hash,
+	}
+	if err := s.repo.CreateUser(userModel); err != nil {
+		return "", err
+	}
+
+	return utils.GenerateToken(in.Email, int64(userModel.ID), s.jwtKey)
+}
+
+func (s *service) Login(ctx context.Context, in dto.LoginInput) (string, error) {
+	u, err := s.repo.GetUserByEmail(in.Email)
+	if err != nil || u.ID == 0 {
+		return "", ErrInvalidCredentials
+	}
+
+	if !passwords.Match(in.Password, u.Password) {
+		return "", ErrInvalidCredentials
+	}
+
+	return utils.GenerateToken(u.Email, int64(u.ID), s.jwtKey)
+}
