94b24646c5c67e893060e722a895e9bdeb4dd6ed
KamilMarszalek <kamilmar553@gmail.com>
2025-05-02 10:42:45 +0200

    Improved generic

--- backend/internal/domains/generic/generic_service.go
+++ backend/internal/domains/generic/generic_service.go
@@ -1,34 +1,30 @@
 package generic
 
-type GenericService[T CRUDRepository[T]] struct {
-	Repo CRUDRepository[T]
+type GenericService[T any, R CRUDService[T]] struct {
+	Repo R
 }
 
 type CRUDService[T any] interface {
-	Create(entity T) error
-	Update(entity T) error
-	Delete(id uint) error
-	GetByID(id uint) (T, error)
-	GetAll() ([]T, error)
+	CRUDRepository[T]
 }
 
-func (service GenericService[T]) Create(entity T) error {
+func (service GenericService[T, R]) Create(entity T) error {
 	err := service.Repo.Create(entity)
 	return err
 }
 
-func (service GenericService[T]) GetAll() ([]T, error) {
+func (service GenericService[T, R]) GetAll() ([]T, error) {
 	return service.Repo.GetAll()
 }
 
-func (service GenericService[T]) GetByID(id uint) (T, error) {
-	return service.Repo.GetByID(id)
+func (service GenericService[T, R]) GetByID(id uint) (T, error) {
+	return service.Repo.GetById(id)
 }
 
-func (service GenericService[T]) Update(entity T) error {
+func (service GenericService[T, R]) Update(entity T) error {
 	return service.Repo.Update(entity)
 }
 
-func (service GenericService[T]) Delete(id uint) error {
+func (service GenericService[T, R]) Delete(id uint) error {
 	return service.Repo.Delete(id)
 }
