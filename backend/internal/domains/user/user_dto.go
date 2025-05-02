63cfd92fac5e85d61f4c04da001e54b59576dea9
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-05-03 00:59:30 +0200

    Refactored dtos - added different ones for create, update and retrieve operations.

--- backend/internal/domains/user/user_dto.go
+++ backend/internal/domains/user/user_dto.go
@@ -1,13 +1,29 @@
 package user
 
-type UserDTO struct {
-	ID            uint   `json:"id"`
-	Username      string `json:"username"`
-	Password      string `json:"password"`
-	Email         string `json:"email"`
-	Selector      string `json:"selector"`
-	CompanyName   string `json:"company_name,omitempty"`
-	CompanyNIP    string `json:"company_nip,omitempty"`
-	PersonName    string `json:"person_name,omitempty"`
-	PersonSurname string `json:"person_surname,omitempty"`
+type CreateUserDTO struct {
+	Username      string  `json:"username"`
+	Password      string  `json:"password"`
+	Email         string  `json:"email"`
+	Selector      string  `json:"selector"`
+	CompanyName   *string `json:"company_name"`
+	CompanyNIP    *string `json:"company_nip"`
+	PersonName    *string `json:"person_name"`
+	PersonSurname *string `json:"person_surname"`
+}
+
+type RetrieveUserDTO struct {
+	Username      string  `json:"username"`
+	Selector      string  `json:"selector"`
+	Email         string  `json:"email"`
+	CompanyName   *string `json:"company_name,omitempty"`
+	CompanyNIP    *string `json:"company_nip,omitempty"`
+	PersonName    *string `json:"person_name,omitempty"`
+	PersonSurname *string `json:"person_surname,omitempty"`
+}
+
+type UpdateUserDTO struct {
+	ID       uint    `json:"id"`
+	Username *string `json:"username"`
+	Password *string `json:"password"`
+	Email    *string `json:"email"`
 }
