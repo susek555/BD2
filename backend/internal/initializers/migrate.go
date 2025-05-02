ff520ba20d22e4d63115119afda13c29720a47ae
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-05-02 15:11:09 +0200

    Added subtypes migration.

--- backend/internal/initializers/migrate.go
+++ backend/internal/initializers/migrate.go
@@ -3,5 +3,9 @@ package initializers
 import "github.com/susek555/BD2/car-dealer-api/internal/domains/user"
 
 func MigrateModels() {
-	DB.AutoMigrate(&user.User{})
+	DB.AutoMigrate(
+		&user.User{},
+		&user.Person{},
+		&user.Company{},
+	)
 }
