492bfa3ca5884d782772467507b4407d767157a6
KamilMarszalek <kamilmar553@gmail.com>
2025-05-02 11:45:40 +0200

    Added automigrate for RefreshToken

--- backend/internal/initializers/migrate.go
+++ backend/internal/initializers/migrate.go
@@ -1,11 +1,16 @@
 package initializers
 
-import "github.com/susek555/BD2/car-dealer-api/internal/domains/user"
+import (
+	"github.com/susek555/BD2/car-dealer-api/internal/domains/refresh_token"
+	"github.com/susek555/BD2/car-dealer-api/internal/domains/user"
+)
 
 func MigrateModels() {
 	DB.AutoMigrate(
 		&user.User{},
 		&user.Person{},
 		&user.Company{},
+		&refresh_token.RefreshToken{},
 	)
+
 }
