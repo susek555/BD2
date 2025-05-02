3c0b8fbf73840d8cf786f4b56b8ee9791a5ebea1
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-05-02 15:04:07 +0200

    Refactored user to handle subtpyes (person, company).

--- backend/internal/domains/user/user_model.go
+++ backend/internal/domains/user/user_model.go
@@ -1,8 +1,23 @@
 package user
 
 type User struct {
-	ID       uint   `json:"id" gorm:"primaryKey"`
+	ID       uint   `json:"id" gorm:"primaryKey;autoIncrement"`
 	Username string `json:"username"`
 	Password string `json:"password"`
 	Email    string `json:"email" gorm:"unique"`
+	Selector string `json:"selector" gorm:"check:selector IN ('P', 'C')"`
+	Company  *Company
+	Person   *Person
+}
+
+func (u *User) GetSubtype() UserSubtype {
+	switch u.Selector {
+	case "P":
+		return u.Person
+	case "C":
+		return u.Company
+	default:
+		return nil
+	}
+
 }
