98d58396845a7706ba50fe4ae819eeafd03b585b
KamilMarszalek <kamilmar553@gmail.com>
2025-05-03 18:24:42 +0200

    Deleted unused file

--- backend/internal/domains/dto/register_input.go
+++
@@ -1,7 +0,0 @@
-package dto
-
-type RegisterInput struct {
-	Username string `json:"username" binding:"required"`
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required,min=8"`
-}
