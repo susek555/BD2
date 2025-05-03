693273ff1f65dac001c4c58813cd04fde4ee95c8
Damian D'Souza <dsouzadamian189@gmail.com>
2025-05-03 20:53:52 +0200

    remove some values, add option to create business or personal account

--- frontend/app/ui/signup-form.tsx
+++ frontend/app/ui/signup-form.tsx
@@ -1,15 +1,19 @@
+"use client";
+
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import {
   AtSymbolIcon,
   BuildingOfficeIcon,
-  HomeIcon,
   KeyIcon,
   UserCircleIcon,
   UserIcon
 } from '@heroicons/react/24/outline';
+import { useState } from 'react';
 import { Button } from './button';
 
 export default function SignupForm() {
+  const [accountType, setAccountType] = useState('personal');
+
   return (
     <form className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
@@ -17,19 +21,55 @@ export default function SignupForm() {
           Sign up
         </h1>
         <div className="w-full">
-          <div>
+          <div className="mb-4">
+            <label className="mb-3 block text-xs font-medium text-gray-900">
+              Account Type
+            </label>
+            <div className="flex gap-4">
+              <div className="flex items-center">
+                <input
+                  id="personal"
+                  type="radio"
+                  name="accountType"
+                  value="personal"
+                  checked={accountType === 'personal'}
+                  onChange={() => setAccountType('personal')}
+                  className="h-4 w-4 text-blue-500 focus:ring-blue-400"
+                />
+                <label htmlFor="personal" className="ml-2 text-sm font-medium text-gray-900">
+                  Personal
+                </label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  id="business"
+                  type="radio"
+                  name="accountType"
+                  value="business"
+                  checked={accountType === 'business'}
+                  onChange={() => setAccountType('business')}
+                  className="h-4 w-4 text-blue-500 focus:ring-blue-400"
+                />
+                <label htmlFor="business" className="ml-2 text-sm font-medium text-gray-900">
+                  Business
+                </label>
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-4">
             <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="login"
+              className="mb-3 block text-xs font-medium text-gray-900"
+              htmlFor="username"
             >
-              Login
+              Username
             </label>
             <div className="relative">
               <input
                 className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="login"
+                id="username"
                 type="text"
-                name="login"
+                name="username"
                 placeholder="Enter your username"
                 required
               />
@@ -37,89 +77,97 @@ export default function SignupForm() {
             </div>
           </div>
 
-          <div className="mt-4">
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="name"
-            >
-              Name
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="name"
-                type="text"
-                name="name"
-                placeholder="Enter your name"
-                required
-              />
-              <UserIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-            </div>
-          </div>
+          {accountType === 'personal' && (
+            <>
+              <div className="mb-4">
+                <label
+                  className="mb-3 block text-xs font-medium text-gray-900"
+                  htmlFor="name"
+                >
+                  Name
+                </label>
+                <div className="relative">
+                  <input
+                    className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                    id="name"
+                    type="text"
+                    name="name"
+                    placeholder="Enter your name"
+                    required
+                  />
+                  <UserIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                </div>
+              </div>
 
-          <div className="mt-4">
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="surname"
-            >
-              Surname
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="surname"
-                type="text"
-                name="surname"
-                placeholder="Enter your surname"
-                required
-              />
-              <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-            </div>
-          </div>
+              <div className="mb-4">
+                <label
+                  className="mb-3 block text-xs font-medium text-gray-900"
+                  htmlFor="surname"
+                >
+                  Surname
+                </label>
+                <div className="relative">
+                  <input
+                    className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                    id="surname"
+                    type="text"
+                    name="surname"
+                    placeholder="Enter your surname"
+                    required
+                  />
+                  <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                </div>
+              </div>
+            </>
+          )}
 
-          <div className="mt-4">
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="address"
-            >
-              Address
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="address"
-                type="text"
-                name="address"
-                placeholder="Enter your address"
-                required
-              />
-              <HomeIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-            </div>
-          </div>
+          {accountType === 'business' && (
+            <>
+              <div className="mb-4">
+                <label
+                  className="mb-3 block text-xs font-medium text-gray-900"
+                  htmlFor="businessName"
+                >
+                  Business Name
+                </label>
+                <div className="relative">
+                  <input
+                    className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                    id="businessName"
+                    type="text"
+                    name="businessName"
+                    placeholder="Enter your business name"
+                    required
+                  />
+                  <BuildingOfficeIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                </div>
+              </div>
 
-          <div className="mt-4">
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="city"
-            >
-              City
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="city"
-                type="text"
-                name="city"
-                placeholder="Enter your city"
-                required
-              />
-              <BuildingOfficeIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-            </div>
-          </div>
+              <div className="mb-4">
+                <label
+                  className="mb-3 block text-xs font-medium text-gray-900"
+                  htmlFor="nip"
+                >
+                  NIP (Tax ID)
+                </label>
+                <div className="relative">
+                  <input
+                    className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                    id="nip"
+                    type="text"
+                    name="nip"
+                    placeholder="Enter your NIP"
+                    required
+                  />
+                  <BuildingOfficeIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                </div>
+              </div>
+            </>
+          )}
 
-          <div className="mt-4">
+          <div className="mb-4">
             <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              className="mb-3 block text-xs font-medium text-gray-900"
               htmlFor="password"
             >
               Password
@@ -138,9 +186,9 @@ export default function SignupForm() {
             </div>
           </div>
 
-          <div className="mt-4">
+          <div className="mb-4">
             <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              className="mb-3 block text-xs font-medium text-gray-900"
               htmlFor="confirmPassword"
             >
               Confirm Password
@@ -159,12 +207,10 @@ export default function SignupForm() {
             </div>
           </div>
         </div>
-        <Button className="mt-4 w-full">
+
+        <Button className="mt-6 w-full">
           Sign up <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
         </Button>
-        <div className="flex h-8 items-end space-x-1">
-          {/* Add form errors here */}
-        </div>
       </div>
     </form>
   );
