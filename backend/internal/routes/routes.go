35d48a9fff716f7a3fc2188cdbf0ac696a52f09b
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-05-04 15:16:59 +0200

    Extracted auth endpoints to routes.go.

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -1,12 +1,19 @@
 package routes
 
 import (
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/susek555/BD2/car-dealer-api/internal/domains/auth"
 	"github.com/susek555/BD2/car-dealer-api/internal/domains/user"
 	"github.com/susek555/BD2/car-dealer-api/internal/initializers"
+	"github.com/susek555/BD2/car-dealer-api/pkg/jwt"
+	"github.com/susek555/BD2/car-dealer-api/pkg/middleware"
 )
 
 func RegisterRoutes(router *gin.Engine) {
+	registerAuthRoutes(router)
 	registerUserRoutes(router)
 }
 
@@ -23,4 +30,22 @@ func registerUserRoutes(router *gin.Engine) {
 		userRoutes.GET("/email/:email", userHandler.GetUserByEmail)
 		userRoutes.DELETE("/:id", userHandler.DeleteUser)
 	}
+}
+
+func registerAuthRoutes(router *gin.Engine) {
+	secret := os.Getenv("JWT_SECRET")
+	if secret == "" {
+		log.Fatal("JWT_SECRET not set")
+	}
+	jwtKey := []byte(secret)
+	verifier := jwt.NewJWTVerifier(secret)
+	authService := auth.NewService(initializers.DB, jwtKey)
+	authHandler := auth.NewHandler(authService)
+	authRoutes := router.Group("/auth")
+	{
+		authRoutes.POST("/register", authHandler.Register)
+		authRoutes.POST("/login", authHandler.Login)
+		authRoutes.POST("/refresh", authHandler.Refresh)
+	}
+	router.POST("/logout", middleware.Authenticate(verifier), authHandler.Logout)
 }
