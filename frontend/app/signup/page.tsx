5d0ad93c94d924baae7875ecceac3950a3c70946
Michal <01187288@pw.edu.pl>
2025-05-04 15:57:08 +0200

    Fixed form type reseting after refresh or submit in signup form

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -1,12 +1,20 @@
 import SignupForm from '@/app/ui/signup-form';
+import { SearchParams } from 'next/dist/server/request/search-params';
 import { Suspense } from 'react';
 
-export default function SignupPage() {
+export default async function SignupPage(props: {
+  searchParams?: Promise<{
+    accountType: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const baseAccountType = searchParams?.accountType || 'personal';
+
   return (
     <main className="flex items-center justify-center min-h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4">
         <Suspense>
-          <SignupForm />
+          <SignupForm baseAccountType={baseAccountType}/>
         </Suspense>
       </div>
     </main>
