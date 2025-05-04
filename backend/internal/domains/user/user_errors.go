8d1ed8abd5740e382fdea0636a6454f02134dcef
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-05-04 21:18:44 +0200

    Unit tests for user mapping added.

--- backend/internal/domains/user/user_errors.go
+++ backend/internal/domains/user/user_errors.go
@@ -11,6 +11,7 @@ import (
 var (
 	ErrInvalidSelector error = errors.New("user selector has to be P (person) or C (company)")
 	ErrHashPassword    error = errors.New("error occured while hashing password")
+	ErrCreateUser      error = errors.New("not all required fields (username, password, email) provided ")
 	ErrCreateCompany   error = errors.New("company_name and company_nip must be provided")
 	ErrCreatePerson    error = errors.New("person_name and person_surname must be provided")
 )
