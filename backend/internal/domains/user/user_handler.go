74cf34c33e4570a41731367a8b1a4e4659ec242e
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-05-04 19:52:12 +0200

    Deleted not needed create user endpoint (creation is done via /register).

--- backend/internal/domains/user/user_handler.go
+++ backend/internal/domains/user/user_handler.go
@@ -26,31 +26,6 @@ func NewHandler(s UserServiceInterface) *Handler {
 	return &Handler{service: s}
 }
 
-// CreateUser godoc
-//
-//	@Summary		Create user
-//	@Description	Creates user from DTO and inserts its data to database. Whenever you want to create user you have to specifiy subtype (selector, P or C), fullfil only respective fields, they are required.
-//	@Tags			users
-//	@Accept			json
-//	@Produce		json
-//	@Param			body	body		CreateUserDTO			true	"Creation form"
-//	@Success		201		{object}	CreateUserDTO			"User created"
-//	@Failure		400		{object}	custom_errors.HTTPError	"Invalid input dat(a, propably wrong selector (only "P" or "C" accepted)"
-//	@Failure		500		{object}	custom_errors.HTTPError	"Internal server error"
-//	@Router			/users [post]
-func (h *Handler) CreateUser(c *gin.Context) {
-	var userDTO CreateUserDTO
-	if err := c.ShouldBindJSON(&userDTO); err != nil {
-		custom_errors.HandleError(c, err, ErrorMap)
-		return
-	}
-	if err := h.service.Create(userDTO); err != nil {
-		custom_errors.HandleError(c, err, ErrorMap)
-		return
-	}
-	c.JSON(http.StatusCreated, userDTO)
-}
-
 // GetAllUsers godoc
 //
 //	@Summary		Get all users
