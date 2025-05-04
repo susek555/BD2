6640ec30b1f56f2ca14bec48cc3a9eb99c99afac
Damian D'Souza <dsouzadamian189@gmail.com>
2025-05-05 01:33:52 +0200

    remove login and save cookies functions

--- frontend/app/lib/api/auth.ts
+++ frontend/app/lib/api/auth.ts
@@ -1,6 +1,5 @@
 "use server";
 
-import { cookies } from 'next/headers';
 
 const API_URL = process.env.API_URL;
 
@@ -40,26 +39,6 @@ export type loginResult = {
   }
 }
 
-async function saveTokensToCookies(access_token: string, refresh_token: string) {
-  const cookieStore = await cookies();
-
-  cookieStore.set('access_token', access_token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    maxAge: 60 * 60 * 2, // 2 hours
-    path: '/',
-  });
-
-  cookieStore.set('refresh_token', refresh_token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    maxAge: 60 * 60 * 24 * 30, // 30 days
-    path: '/',
-  });
-
-  console.log("saved coookies");
-}
-
 /**
  * Sends registration data to the backend and stores tokens if successful
  * @param userData Registration data in the required format
@@ -88,17 +67,13 @@ export async function registerUser(userData: {
 
     const data: registerResponse = await response.json();
 
-    console.log(data);
+    console.log("Registration response: ", data);
 
     if (!response.ok) {
       return {
         success: false,
         errors: data.errors || {},
       };
-    }
-
-    if (data.access_token && data.refresh_token) {
-      saveTokensToCookies(data.access_token, data.refresh_token);
     }
 
     return {
@@ -113,62 +88,4 @@ export async function registerUser(userData: {
       },
     };
   }
-}
-
-/**
- * Authenticates user and stores tokens in cookies
- * @param credentials User login credentials
- * @returns Response with success status, data or error information
- */
-export async function loginUser(credentials: {
-  login: string;
-  password: string;
-}): Promise<loginResult> {
-  try {
-    const response = await fetch(`${API_URL}/auth/login`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(credentials),
-    });
-
-    const data: loginResponse = await response.json();
-
-    if (!response.ok) {
-      return {
-        success: false,
-        errors: data.errors || {},
-      };
-    }
-
-    if (data.access_token && data.refresh_token) {
-      saveTokensToCookies(data.access_token, data.refresh_token);
-    }
-
-    return {
-      success: true,
-    };
-  } catch (error: any) {
-    console.error('Login error:', error);
-    return {
-      success: false,
-      errors: {
-        other: error.message || 'An unexpected error occurred during registration'
-      },
-    };
-  }
-}
-
-/**
- * Logs out the user by clearing tokens
- */
-export async function logoutUser() {
-  const cookieStore = await cookies();
-  cookieStore.delete('access_token');
-  cookieStore.delete('refresh_token');
-
-  return {
-    success: true,
-  };
 }
