70dcba941166912b0a0c89ff7d839c012e0d08e7
KamilMarszalek <kamilmar553@gmail.com>
2025-05-04 09:07:12 +0200

    Added test for Register method - company

--- backend/internal/domains/auth/auth_service_test.go
+++ backend/internal/domains/auth/auth_service_test.go
@@ -26,7 +26,7 @@ func hashPass(t *testing.T, raw string) string {
 	---  TESTY  --------------------------------------------------------------
 */
 
-func TestService_Register(t *testing.T) {
+func TestService_Register_Person(t *testing.T) {
 	t.Run("email not taken - should return access and refresh tokens", func(t *testing.T) {
 		ctx := context.Background()
 		uRepo := mocks.NewUserRepositoryInterface(t)
@@ -34,6 +34,55 @@ func TestService_Register(t *testing.T) {
 		person := user.Person{Name: "John", Surname: "Doe"}
 
 		in := user.CreateUserDTO{Email: "john@example.com", Password: "sekret", Username: "john", PersonName: &person.Name, PersonSurname: &person.Surname, Selector: "P"}
+
+		uRepo.On("GetByEmail", in.Email).Return(user.User{}, errors.New("not found"))
+		uRepo.On("Create", mock.Anything).Return(nil)
+		rtSvc.On("Create", mock.Anything).Return(nil)
+
+		svc := &service{repo: uRepo, refreshTokenService: rtSvc, jwtKey: jwtKey}
+
+		access, refresh, err := svc.Register(ctx, in)
+
+		assert.NoError(t, err)
+		assert.NotEmpty(t, access)
+		assert.NotEmpty(t, refresh)
+
+		uRepo.AssertExpectations(t)
+		rtSvc.AssertExpectations(t)
+	})
+
+	t.Run("when email taken it should return ErrEmailTaken", func(t *testing.T) {
+		ctx := context.Background()
+		uRepo := mocks.NewUserRepositoryInterface(t)
+		rtSvc := mocks.NewRefreshTokenServiceInterface(t)
+
+		existing := user.User{ID: 1, Email: "marta@example.com"}
+		uRepo.On("GetByEmail", existing.Email).Return(existing, nil)
+
+		svc := &service{repo: uRepo, refreshTokenService: rtSvc, jwtKey: jwtKey}
+
+		_, _, err := svc.Register(ctx, user.CreateUserDTO{Email: existing.Email})
+
+		assert.ErrorIs(t, err, ErrEmailTaken)
+		uRepo.AssertExpectations(t)
+	})
+}
+
+func TestService_Register_Company(t *testing.T) {
+	t.Run("email not taken - should return access and refresh tokens", func(t *testing.T) {
+		ctx := context.Background()
+		uRepo := mocks.NewUserRepositoryInterface(t)
+		rtSvc := mocks.NewRefreshTokenServiceInterface(t)
+		company := user.Company{Name: "Awesome Name", NIP: "123233234234"}
+
+		in := user.CreateUserDTO{
+			Email:       "john@example.com",
+			Password:    "sekret",
+			Username:    "john",
+			CompanyName: &company.Name,
+			CompanyNIP:  &company.NIP,
+			Selector:    "C",
+		}
 
 		uRepo.On("GetByEmail", in.Email).Return(user.User{}, errors.New("not found"))
 		uRepo.On("Create", mock.Anything).Return(nil)
