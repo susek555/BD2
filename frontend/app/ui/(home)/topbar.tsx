80060bbfb423c0e7dae7966f466c67968038b854
Michal <01187288@pw.edu.pl>
2025-05-04 18:55:30 +0200

    Added search field

--- frontend/app/ui/(home)/topbar.tsx
+++ frontend/app/ui/(home)/topbar.tsx
@@ -7,16 +7,16 @@ export default function TopBar() {
     return (
       <div className="flex w-full flex-row">
         <Link
-          className="mb-2 flex w-20 items-end justify-start rounded-br-lg bg-blue-600 p-2 md:w-60"
+          className="mb-2 flex w-20 items-end justify-start rounded-br-lg bg-blue-600 md:w-60 md:h-12"
           href="/"
         >
             <div className="flex h-full w-full items-center justify-center rounded-md bg-blue-600 p-0 text-white">
             <p className="font-bold">HOME</p>
             </div>
         </Link>
-        <div className="flex grow flex-col justify-between md:flex-row md:items-center md:space-x-2">
+        <div className="flex grow flex-col justify-between px-2 py-0 md:flex-row md:items-top md:space-x-2">
           <div className="flex grow justify-center">
-            <SearchField />
+            <SearchField placeholder="Search for cars..."/>
           </div>
           <div className="flex justify-end px-2">
             <LoginButtons />
