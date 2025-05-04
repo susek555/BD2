aa4364e867b1c380f76e8983102942688b0b7769
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-05-04 15:16:59 +0200

    Extracted auth endpoints to routes.go.

--- backend/cmd/car-dealer-api/main.go
+++ backend/cmd/car-dealer-api/main.go
@@ -3,17 +3,13 @@ package main
 
 import (
 	"log"
-	"os"
 
-	"github.com/susek555/BD2/car-dealer-api/internal/domains/auth"
 	"github.com/susek555/BD2/car-dealer-api/internal/routes"
 
 	"github.com/gin-gonic/gin"
 
 	_ "github.com/susek555/BD2/car-dealer-api/docs"
 	"github.com/susek555/BD2/car-dealer-api/internal/initializers"
-	"github.com/susek555/BD2/car-dealer-api/pkg/jwt"
-	"github.com/susek555/BD2/car-dealer-api/pkg/middleware"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
@@ -32,31 +28,8 @@ func init() {
 // @host			localhost:8080
 // @schemes		http
 func main() {
-	secret := os.Getenv("JWT_SECRET")
-	if secret == "" {
-		log.Fatal("JWT_SECRET not set")
-	}
-	jwtKey := []byte(secret)
-	verifier := jwt.NewJWTVerifier(secret)
-
-	db := initializers.DB
-
-	authSvc := auth.NewService(db, jwtKey)
-	authH := auth.NewHandler(authSvc)
-
 	router := gin.Default()
-
-	authGroup := router.Group("/auth")
-	authGroup.POST("/register", authH.Register)
-	authGroup.POST("/login", authH.Login)
-	authGroup.POST("/refresh", authH.Refresh)
-
-	api := router.Group("/")
-	api.Use(middleware.Authenticate(verifier))
 	routes.RegisterRoutes(router)
-	{
-		api.POST("/logout", authH.Logout)
-	}
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	if err := router.Run(":8080"); err != nil {
 		log.Fatalf("server error: %v", err)
