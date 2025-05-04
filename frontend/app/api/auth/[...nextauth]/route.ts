1c16f83ba0ec888cd31ab02b9c265eb15680f87d
Damian D'Souza <dsouzadamian189@gmail.com>
2025-05-05 01:34:38 +0200

    implement mostly working nextauth config

---
+++ frontend/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,66 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const API_URL = process.env.API_URL;
+
+// TODO: add error handling
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        login: { label: "Login", type: "text" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+
+        const res = await fetch(`${API_URL}/auth/login`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: credentials?.login,
+            password: credentials?.password,
+          }),
+        });
+        const user = await res.json();
+        console.log(user);
+        console.log(JSON.stringify({
+          email: credentials?.login,
+          password: credentials?.password,
+        }));
+
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      }
+    })
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+    async session({ session, token, user }) {
+      session.user = token as any;
+      return session;
+    },
+  },
+  pages: {
+    signIn: '/login',
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  }
+})
+
+export { handler as GET, handler as POST };
