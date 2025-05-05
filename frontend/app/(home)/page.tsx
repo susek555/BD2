09c0d82d9368ad10a893d854985cd8da43e43c1a
Michal <01187288@pw.edu.pl>
2025-05-05 19:29:12 +0200

    Added filters field template

--- frontend/app/(home)/page.tsx
+++ frontend/app/(home)/page.tsx
@@ -1,14 +1,18 @@
 import SideBar from "@/app/ui/(home)/sidebar";
+import { Suspense } from "react";
+
 
 export default function Home() {
   return (
   <main>
     <div className="flex flex-col md:flex-row flex-grow">
-      <div className="w-full md:w-80 py-4 flex-none">
+      <div className="w-full md:w-80 py-4 h-full flex-none">
       <SideBar />
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
-      <p>Main Page</p>
+        <Suspense>
+          <p>Main Page</p>
+        </Suspense>
       </div>
     </div>
   </main>
--- frontend/app/ui/(home)/filters.tsx
+++ frontend/app/ui/(home)/filters.tsx
@@ -1,6 +1,16 @@
+"use client";
+
+import { BaseFilterTemplate } from "@/app/ui/(home)/base-filter-template/base-filter-template";
+
 export default function Filters() {
+
+    function temp(selected: string[]) {}
+
     return (
-        <p>Filters</p>
+        <>
+            <p>Filters</p>
+            <BaseFilterTemplate name="Model" options={["A5", "B4"]} onChange={temp}></BaseFilterTemplate>
+        </>
     );
 }
 
