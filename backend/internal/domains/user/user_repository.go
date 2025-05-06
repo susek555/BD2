047d44d411dd1629db99464effa87461a1c2d9d9
KamilMarszalek <kamilmar553@gmail.com>
2025-05-06 19:09:56 +0200

    Added GetByCompanyNip and GetByUsername method

--- backend/internal/domains/user/user_repository.go
+++ backend/internal/domains/user/user_repository.go
@@ -9,6 +9,8 @@ import (
 type UserRepositoryInterface interface {
 	generic.CRUDRepository[User]
 	GetByEmail(email string) (User, error)
+	GetByCompanyNip(nip string) (User, error)
+	GetByUsername(username string) (User, error)
 }
 
 type UserRepository struct {
@@ -57,6 +59,30 @@ func (r *UserRepository) GetByEmail(email string) (User, error) {
 	return u, nil
 }
 
+func (r *UserRepository) GetByUsername(username string) (User, error) {
+	var u User
+	err := r.DB.Preload("Company").Preload("Person").Where("username = ?", username).First(&u).Error
+	if err != nil {
+		return User{}, err
+	}
+	return u, nil
+}
+
+func (r *UserRepository) GetByCompanyNip(nip string) (User, error) {
+	var u User
+	err := r.DB.
+		Joins("JOIN companies ON companies.user_id = users.id").
+		Where("companies.nip = ?", nip).
+		Preload("Company").
+		Preload("Person").
+		First(&u).Error
+
+	if err != nil {
+		return User{}, err
+	}
+	return u, nil
+}
+
 func (r *UserRepository) Update(user User) error {
 	return r.DB.Transaction(func(tx *gorm.DB) error {
 		if err := tx.Save(user).Error; err != nil {
@@ -71,5 +97,4 @@ func (r *UserRepository) Delete(id uint) error {
 	return r.DB.Transaction(func(tx *gorm.DB) error {
 		return r.DB.Delete(&User{}, id).Error
 	})
-
 }
