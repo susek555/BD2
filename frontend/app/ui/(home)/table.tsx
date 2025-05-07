eb4d9427083776060bd2d193e3b38d8bc997ed77
Michal <01187288@pw.edu.pl>
2025-05-07 17:51:53 +0200

    Adjusted displaying of car details on home screen

--- frontend/app/ui/(home)/table.tsx
+++ frontend/app/ui/(home)/table.tsx
@@ -9,8 +9,21 @@ export default async function OffersTable() {
                 <div className="flex flex-row gap-4">
                     <div className="p-2.5">
                         <Suspense>
-                            <CarImageSkeleton />
+                            <CarImageSkeleton /> {/* TODO implement fetching image from internet */}
                         </Suspense>
+                    </div>
+                    <div className="flex py-2.5 h-40 flex-col w-full">
+                        <p className="font-bold text-2xl">Car name</p>
+                        <div className="px-4 py-3 flex flex-row gap-10">
+                            <p className="text-bg">Production year: 2000</p>
+                            <p className="text-bg">Mileage: 150000 km</p>
+                        </div>
+                        <div className="px-4 flex flex-row gap-10">
+                            <p className="text-bg">First owner</p>
+                        </div>
+                        <div className="flex justify-end items-end h-full self-end">
+                            <p className="px-3 font-bold text-2xl">10000 zł</p>
+                        </div>
                     </div>
                 </div>
             </div>
