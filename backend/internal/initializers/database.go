9428c37a706e203296552be5eefc1492e9c5ce5d
KamilMarszalek <kamilmar553@gmail.com>
2025-05-09 19:40:43 +0200

    Fixed bug with disabled foreign keys constraint

--- backend/internal/initializers/database.go
+++ backend/internal/initializers/database.go
@@ -12,7 +12,10 @@ var DB *gorm.DB
 
 func ConnectToDatabase() {
 	dsn := os.Getenv("DATABASE_URL")
-	dbHandle, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{})
+	dbHandle, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{
+		DisableForeignKeyConstraintWhenMigrating: false,
+	})
+	dbHandle.Exec("PRAGMA foreign_keys = ON")
 	if err != nil {
 		log.Fatal("Connection with database couldn't be established.")
 	}
