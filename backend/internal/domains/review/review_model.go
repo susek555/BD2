44716d205e2fb9e0334174d27c9874dbbff72f39
KamilMarszalek <kamilmar553@gmail.com>
2025-05-09 21:29:30 +0200

    Made rating uint

--- backend/internal/domains/review/review_model.go
+++ backend/internal/domains/review/review_model.go
@@ -5,7 +5,7 @@ import "github.com/susek555/BD2/car-dealer-api/internal/domains/user"
 type Review struct {
 	ID          uint       `json:"id" gorm:"primary_key;autoIncrement"`
 	Description string     `json:"description" gorm:"not null"`
-	Rating      int        `json:"rating" gorm:"not null;check:rating BETWEEN 1 AND 5"`
+	Rating      uint       `json:"rating" gorm:"not null;check:rating BETWEEN 1 AND 5"`
 	ReviewerID  uint       `json:"reviewer_id"`
 	Reviewer    *user.User `gorm:"foreignKey:ReviewerID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	RevieweeId  uint       `json:"reviewee_id"`
