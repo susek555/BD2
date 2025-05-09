548d538e4a0cfc6572567e7d3d9af32b22392f75
Damian D'Souza <dsouzadamian189@gmail.com>
2025-05-09 23:35:20 +0200

    modify custom types, add ExtendedJWT type

--- frontend/types/next-auth.d.ts
+++ frontend/types/next-auth.d.ts
@@ -1,4 +1,6 @@
 import "next-auth";
+import { JWT } from "next-auth/jwt";
+
 
 declare module "next-auth" {
   interface Session {
@@ -8,11 +10,38 @@ declare module "next-auth" {
       username: string;
       email: string;
       selector: "P" | "C";
-      person_name?: string;
-      person_surname?: string;
-      company_name?: string;
-      company_surname?: string;
+      personName?: string;
+      personSurname?: string;
+      companyName?: string;
+      companySurname?: string;
+      accessTokenExpires: number;
       errors?: string[];
     };
   }
+  interface User {
+    accessToken: string;
+    refreshToken: string;
+    username: string;
+    email: string;
+    selector: "P" | "C";
+    personName?: string;
+    personSurname?: string;
+    companyName?: string;
+    companySurname?: string;
+    errors?: string[];
+  }
+}
+
+interface ExtendedJWT extends JWT {
+  accessToken: string;
+  refreshToken: string;
+  username: string;
+  email: string;
+  selector: "P" | "C";
+  personName?: string;
+  personSurname?: string;
+  companyName?: string;
+  companySurname?: string;
+  accessTokenExpires: number;
+  error?: string;
 }
