c390f3a0afd6adf3403817b17a3fa97c54412e6e
KamilMarszalek <kamilmar553@gmail.com>
2025-05-10 00:24:47 +0200

    Added needed constraints

--- backend/internal/domains/review/review_model.go
+++ backend/internal/domains/review/review_model.go
@@ -6,8 +6,8 @@ type Review struct {
 	ID          uint       `json:"id" gorm:"primary_key;autoIncrement"`
 	Description string     `json:"description" gorm:"not null"`
 	Rating      uint       `json:"rating" gorm:"not null;check:rating BETWEEN 1 AND 5"`
-	ReviewerID  uint       `json:"reviewer_id"`
+	ReviewerID  uint       `json:"reviewer_id" gorm:"not null;check:reviewer_id <> reviewee_id;uniqueIndex:ux_review_pair"`
 	Reviewer    *user.User `gorm:"foreignKey:ReviewerID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	RevieweeId  uint       `json:"reviewee_id"`
-	Reviewee    *user.User `gorm:"foreignKey:RevieweeId;references:ID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
+	RevieweeId  uint       `json:"reviewee_id" gorm:"not null;uniqueIndex:ux_review_pair"`
+	Reviewee    *user.User `gorm:"foreignKey:RevieweeId;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
