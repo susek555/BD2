bf351b32be383057eae4c31f94b094f9b771ba62
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-05-10 00:29:41 +0200

    Typos fixed.

--- backend/internal/domains/manufacturer/manufacturer_handler.go
+++ backend/internal/domains/manufacturer/manufacturer_handler.go
@@ -18,12 +18,12 @@ func NewHandler(s ManufacturerServiceInterface) *Handler {
 //
 //	@Summary		Get all manufacturers
 //	@Description	Returns a list of all manufacturers stored in the database.
-//	@Tags			manufacturers
+//	@Tags			cars
 //	@Accept			json
 //	@Produce		json
-//	@Success		200	{array}		RetrieveManufacturerDTO	"List of manufacturers"
+//	@Success		200	{array}		Manufacturer "List of manufacturers"
 //	@Failure		500	{object}	custom_errors.HTTPError	"Internal server error"
-//	@Router			/car/manufacturers [get]
+//	@Router			/cars/manufacturers [get]
 func (h *Handler) GetAllManufactures(c *gin.Context) {
 	manufacturers, err := h.service.GetAll()
 	if err != nil {
--- backend/internal/domains/model/model_handler.go
+++ backend/internal/domains/model/model_handler.go
@@ -26,7 +26,7 @@ func NewHandler(s ModelServiceInterace) *Handler {
 //
 //	@Summary		Get all models by manufacturer id
 //	@Description	Returns a list of all models stored in the database for a given manufacturer id.
-//	@Tags			models
+//	@Tags			cars
 //	@Accept			json
 //	@Produce		json
 //	@Param			id	path		int						true	"Manufacturer ID"
@@ -34,14 +34,14 @@ func NewHandler(s ModelServiceInterace) *Handler {
 //	@Failure		400	{object}	custom_errors.HTTPError	"Id is not a number"
 //	@Failure		404	{object}	custom_errors.HTTPError	"Models not found"
 //	@Failure		500	{object}	custom_errors.HTTPError	"Internal server error"
-//	@Router			/car/models//id/{id} [get]
+//	@Router			/cars/models//id/{id} [get]
 func (h *Handler) GetModelsByManufacturerID(c *gin.Context) {
 	id, err := strconv.ParseUint(c.Param("id"), 10, 32)
 	if err != nil {
 		custom_errors.HandleError(c, err, ErrorMap)
 		return
 	}
-	models, err := h.service.GetByManufactuerID(uint(id))
+	models, err := h.service.GetByManufacturerID(uint(id))
 	if err != nil {
 		custom_errors.HandleError(c, err, ErrorMap)
 		return
@@ -53,17 +53,17 @@ func (h *Handler) GetModelsByManufacturerID(c *gin.Context) {
 //
 //	@Summary		Get all models by manufacturer name
 //	@Description	Returns a list of all models stored in the database for a given manufacturer name.
-//	@Tags			models
+//	@Tags			cars
 //	@Accept			json
 //	@Produce		json
 //	@Param			name	path		string					true	"Manufacturer name"
 //	@Success		200		{array}		RetrieveModelDTO		"List of models"
 //	@Failure		404		{object}	custom_errors.HTTPError	"Models not found"
 //	@Failure		500		{object}	custom_errors.HTTPError	"Internal server error"
-//	@Router			/car/models/name/{name} [get]
+//	@Router			/cars/models/name/{name} [get]
 func (h *Handler) GetModelsByManufacturerName(c *gin.Context) {
 	name := c.Param("email")
-	models, err := h.service.GetByManufacuterName(name)
+	models, err := h.service.GetByManufacturerName(name)
 	if err != nil {
 		custom_errors.HandleError(c, err, ErrorMap)
 		return
