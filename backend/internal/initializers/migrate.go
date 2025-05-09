060e4cb7ca5b0734761ec535db911d5e207f5c03
KamilMarszalek <kamilmar553@gmail.com>
2025-05-09 16:15:43 +0200

    Added missing line

--- backend/internal/initializers/migrate.go
+++ backend/internal/initializers/migrate.go
@@ -2,6 +2,7 @@ package initializers
 
 import (
 	"github.com/susek555/BD2/car-dealer-api/internal/domains/refresh_token"
+	"github.com/susek555/BD2/car-dealer-api/internal/domains/review"
 	"github.com/susek555/BD2/car-dealer-api/internal/domains/user"
 )
 
@@ -11,6 +12,7 @@ func MigrateModels() {
 		&user.Person{},
 		&user.Company{},
 		&refresh_token.RefreshToken{},
+		&review.Review{},
 	)
 
 }
