1fae1cbad6061409cdb1bf07a1d11a17086ac52f
Michal <01187288@pw.edu.pl>
2025-05-09 16:51:19 +0200

    Added offer Type field to sidebar

--- frontend/app/ui/(home)/sidebar.tsx
+++ frontend/app/ui/(home)/sidebar.tsx
@@ -1,12 +1,14 @@
 import Link from 'next/link';
 import Filters from '@/app/ui/(home)/filters';
 import Sorting from './sorting';
+import OfferType from './offer-type';
 
 
 export default function SideBar() {
     return (
       <div className="flex h-full flex-col px-3 py-2 md:px-2 rounded-r-lg border-black border-[2px]">
         <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+          <OfferType  />
           <Sorting />
           <Filters />
           <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
