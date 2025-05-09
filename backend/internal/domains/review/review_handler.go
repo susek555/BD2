0be205c0aa92c468892129ad0c8ebfccb351be4e
KamilMarszalek <kamilmar553@gmail.com>
2025-05-09 15:46:18 +0200

    Implemented review_handler

--- backend/internal/domains/review/review_handler.go
+++ backend/internal/domains/review/review_handler.go
@@ -1,1 +1,50 @@
 package review
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/susek555/BD2/car-dealer-api/pkg/custom_errors"
+	"net/http"
+	"strconv"
+)
+
+type Handler struct {
+	service ReviewServiceInterface
+}
+
+func NewHandler(service ReviewServiceInterface) *Handler {
+	return &Handler{service: service}
+}
+
+func (h *Handler) GetAllReviews(c *gin.Context) {
+	reviews, err := h.service.GetAll()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, custom_errors.NewHTTPError(err.Error()))
+		return
+	}
+	c.JSON(http.StatusOK, reviews)
+}
+
+func (h *Handler) GetReviewById(c *gin.Context) {
+	id, err := strconv.ParseUint(c.Param("id"), 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, custom_errors.NewHTTPError(err.Error()))
+		return
+	}
+	review, err := h.service.GetById(uint(id))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, custom_errors.NewHTTPError(err.Error()))
+	}
+	c.JSON(http.StatusOK, review)
+}
+
+func (h *Handler) CreateReview(c *gin.Context) {
+	var review Review
+	if err := c.ShouldBindJSON(&review); err != nil {
+		c.JSON(http.StatusBadRequest, custom_errors.NewHTTPError(err.Error()))
+		return
+	}
+	if err := h.service.Create(review); err != nil {
+		c.JSON(http.StatusBadRequest, custom_errors.NewHTTPError(err.Error()))
+	}
+	c.JSON(http.StatusCreated, review)
+}
