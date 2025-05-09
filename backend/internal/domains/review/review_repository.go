5effa9f7096150d7b152c2326686bf0280254859
KamilMarszalek <kamilmar553@gmail.com>
2025-05-09 14:30:38 +0200

    Created review repository

---
+++ backend/internal/domains/review/review_repository.go
@@ -0,0 +1,61 @@
+package review
+
+import "github.com/susek555/BD2/car-dealer-api/internal/domains/generic"
+
+type ReviewRepositoryInterface interface {
+	generic.CRUDRepository[Review]
+	GetByReviewerId(reviewerId uint) ([]Review, error)
+	GetByReviewedId(reviewedId uint) ([]Review, error)
+}
+
+type ReviewRepository struct {
+	repository *generic.GormRepository[Review]
+}
+
+func NewReviewRepository(repository *generic.GormRepository[Review]) ReviewRepositoryInterface {
+	return &ReviewRepository{repository: repository}
+}
+
+func (repo *ReviewRepository) Create(review Review) error {
+	return repo.repository.Create(review)
+}
+
+func (repo *ReviewRepository) GetAll() ([]Review, error) {
+	return repo.repository.GetAll()
+}
+
+func (repo *ReviewRepository) GetById(id uint) (Review, error) {
+	return repo.repository.GetById(id)
+}
+
+func (repo *ReviewRepository) Update(review Review) error {
+	return repo.repository.Update(review)
+}
+
+func (repo *ReviewRepository) Delete(id uint) error {
+	return repo.repository.Delete(id)
+}
+
+func (repo *ReviewRepository) GetByReviewerId(reviewerId uint) ([]Review, error) {
+	var reviews []Review
+	err := repo.repository.
+		DB.
+		Where("reviewer_id = ?", reviewerId).
+		Preload("Reviewed").
+		Preload("Reviewer").
+		Find(&reviews).
+		Error
+	return reviews, err
+}
+
+func (repo *ReviewRepository) GetByReviewedId(reviewedId uint) ([]Review, error) {
+	var reviews []Review
+	err := repo.repository.
+		DB.
+		Where("reviewer_id = ?", reviewedId).
+		Preload("Reviewed").
+		Preload("Reviewer").
+		Find(&reviews).
+		Error
+	return reviews, err
+}
