cb196da2653ff27d5cf1faf64569ad8f898d1ec7
KamilMarszalek <kamilmar553@gmail.com>
2025-05-09 13:32:42 +0200

    Made review better suited for our needs

--- backend/internal/domains/review/review_model.go
+++ backend/internal/domains/review/review_model.go
@@ -3,9 +3,11 @@ package review
 import "github.com/susek555/BD2/car-dealer-api/internal/domains/user"
 
 type Review struct {
-	ID          int64      `json:"id" gorm:"primary_key"`
+	ID          uint       `json:"id" gorm:"primary_key"`
 	Description string     `json:"description" gorm:"not null"`
-	Rating      int        `json:"rating" gorm:"not null"`
-	Reviewer    *user.User `gorm:"foreignKey:ReviewerID;references:ID"`
-	Reviewed    *user.User `gorm:"foreignKey:ReviewedID;references:ID"`
+	Rating      int        `json:"rating" gorm:"not null;check:rating BETWEEN 1 AND 5"`
+	ReviewerID  uint       `json:"reviewer_id"`
+	Reviewer    *user.User `gorm:"foreignKey:ReviewerID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	ReviewedID  uint       `json:"reviewed_id"`
+	Reviewed    *user.User `gorm:"foreignKey:ReviewedID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
 }
