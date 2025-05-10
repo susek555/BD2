daef31cc6e1402b14b85f7e7fc9260ec1c5ef730
Damian D'Souza <dsouzadamian189@gmail.com>
2025-05-10 14:57:54 +0200

    update eslint config

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,16 +1,29 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: '^_', // Ignore unused function arguments
+          varsIgnorePattern: '^_', // Ignore unused variables
+          caughtErrorsIgnorePattern: '^_', // Ignore unused catch clause parameters
+          ignoreRestSiblings: true, //  Ignore unused properties when using object destructuring
+        },
+      ],
+    }
+  }),
 ];
 
 export default eslintConfig;
