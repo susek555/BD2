3fab8371b684f31fa0bfcb6a18585e0f5c9c749c
KamilMarszalek <kamilmar553@gmail.com>
2025-05-10 21:00:13 +0200

    Added simple auction model

---
+++ backend/internal/domains/auction/auction_model.go
@@ -0,0 +1,9 @@
+package auction
+
+import "time"
+
+type Auction struct {
+	OfferID     uint      `json:"id" gorm:"primaryKey"`
+	DateEnd     time.Time `json:"date_end" gorm:"not null;index"`
+	BuyNowPrice uint      `json:"buy_now_price" gorm:"index"`
+}
