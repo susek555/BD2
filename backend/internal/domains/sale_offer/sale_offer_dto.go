b198750d2e11d505d292267645aeefd571afc0f4
KamilMarszalek <kamilmar553@gmail.com>
2025-05-10 23:38:29 +0200

    Added RetrieveSaleOfferDTO

--- backend/internal/domains/sale_offer/sale_offer_dto.go
+++ backend/internal/domains/sale_offer/sale_offer_dto.go
@@ -24,3 +24,25 @@ type CreateSaleOfferDTO struct {
 	Drive              car_params.Drive        `json:"drive"`
 	ModelID            uint                    `json:"model_id"`
 }
+
+type RetrieveSaleOfferDTO struct {
+	Description        string                  `json:"description"`
+	Price              uint                    `json:"price"`
+	Margin             uint                    `json:"margin"`
+	Vin                string                  `json:"vin"`
+	ProductionYear     uint                    `json:"production_year"`
+	Mileage            uint                    `json:"mileage"`
+	NumberOfDoors      uint                    `json:"number_of_doors"`
+	NumberOfSeats      uint                    `json:"number_of_seats"`
+	EnginePower        uint                    `json:"engine_power"`
+	EngineCapacity     uint                    `json:"engine_capacity"`
+	RegistrationNumber string                  `json:"registration_number"`
+	RegistrationDate   string                  `json:"registration_date"`
+	Color              car_params.Color        `json:"color"`
+	FuelType           car_params.FuelType     `json:"fuel_type"`
+	Transmission       car_params.Transmission `json:"transmission"`
+	NumberOfGears      uint                    `json:"number_of_gears"`
+	Drive              car_params.Drive        `json:"drive"`
+	Brand              string                  `json:"brand"`
+	Model              string                  `json:"model"`
+}
