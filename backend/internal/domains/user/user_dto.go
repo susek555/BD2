62ae0fdd2a56f4ead95163af1f82e7c7ed44a853
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-05-10 17:33:15 +0200

    Changed ID to uint in RetrieveUserDTO.

--- backend/internal/domains/user/user_dto.go
+++ backend/internal/domains/user/user_dto.go
@@ -12,7 +12,7 @@ type CreateUserDTO struct {
 }
 
 type RetrieveUserDTO struct {
-	ID            string  `json:"id"`
+	ID            uint    `json:"id"`
 	Username      string  `json:"username"`
 	Email         string  `json:"email"`
 	CompanyName   *string `json:"company_name,omitempty"`
