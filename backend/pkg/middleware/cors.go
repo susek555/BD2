2cf97f3d2f5d82fecbd92624c29df15e20fb4952
KamilMarszalek <kamilmar553@gmail.com>
2025-05-10 16:12:34 +0200

    Added options

--- backend/pkg/middleware/cors.go
+++ backend/pkg/middleware/cors.go
@@ -7,7 +7,7 @@ import (
 
 var CorsConfig = cors.Config{
 	AllowOrigins:     []string{"http://localhost:3000"},
-	AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+	AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 	AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "X-Requested-With"},
 	AllowCredentials: true,
 	MaxAge:           12 * time.Hour,
