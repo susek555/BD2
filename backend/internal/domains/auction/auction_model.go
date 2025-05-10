2a62fe5b915a7538bb778f588d6a19b17f123a36
KamilMarszalek <kamilmar553@gmail.com>
2025-05-10 21:01:15 +0200

    Added foreign key

--- backend/internal/domains/auction/auction_model.go
+++ backend/internal/domains/auction/auction_model.go
@@ -1,9 +1,13 @@
 package auction
 
-import "time"
+import (
+	"github.com/susek555/BD2/car-dealer-api/internal/domains/sale_offer"
+	"time"
+)
 
 type Auction struct {
-	OfferID     uint      `json:"id" gorm:"primaryKey"`
-	DateEnd     time.Time `json:"date_end" gorm:"not null;index"`
-	BuyNowPrice uint      `json:"buy_now_price" gorm:"index"`
+	OfferID     uint                 `json:"id" gorm:"primaryKey"`
+	DateEnd     time.Time            `json:"date_end" gorm:"not null;index"`
+	BuyNowPrice uint                 `json:"buy_now_price" gorm:"index"`
+	Offer       sale_offer.SaleOffer `gorm:"foreignKey:OfferID;references:ID;constraint:OnDelete:CASCADE"`
 }
