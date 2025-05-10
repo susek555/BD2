5c255790f9842485d247867238f3b2cefccab347
KamilMarszalek <kamilmar553@gmail.com>
2025-05-10 21:44:24 +0200

    Implemented create auction dto

---
+++ backend/internal/domains/auction/auction_dto.go
@@ -0,0 +1,29 @@
+package auction
+
+import (
+	"github.com/susek555/BD2/car-dealer-api/internal/domains/car/car_params"
+	"time"
+)
+
+type CreateAuctionDTO struct {
+	Description        string                  `json:"description"`
+	Price              uint                    `json:"price"`
+	Margin             uint                    `json:"margin"`
+	Vin                string                  `json:"vin"`
+	ProductionYear     uint                    `json:"production_year"`
+	Mileage            uint                    `json:"mileage"`
+	NumberOfDoors      uint                    `json:"number_of_doors"`
+	NumberOfSeats      uint                    `json:"number_of_seats"`
+	EnginePower        uint                    `json:"engine_power"`
+	EngineCapacity     uint                    `json:"engine_capacity"`
+	RegistrationNumber string                  `json:"registration_number"`
+	RegistrationDate   string                  `json:"registration_date"`
+	Color              car_params.Color        `json:"color"`
+	FuelType           car_params.FuelType     `json:"fuel_type"`
+	Transmission       car_params.Transmission `json:"transmission"`
+	NumberOfGears      uint                    `json:"number_of_gears"`
+	Drive              car_params.Drive        `json:"drive"`
+	ModelID            uint                    `json:"model_id"`
+	DateEnd            time.Time               `json:"date_end"`
+	BuyNowPrice        uint                    `json:"buy_now_price"`
+}
