af88e4afea025319e6cb4b32d327a7410027b8da
KamilMarszalek <kamilmar553@gmail.com>
2025-05-10 22:17:19 +0200

    Fixed CreateAuctionDTO

--- backend/internal/domains/auction/auction_dto.go
+++ backend/internal/domains/auction/auction_dto.go
@@ -2,11 +2,10 @@ package auction
 
 import (
 	"github.com/susek555/BD2/car-dealer-api/internal/domains/sale_offer"
-	"time"
 )
 
 type CreateAuctionDTO struct {
 	sale_offer.CreateSaleOfferDTO
-	DateEnd     time.Time `json:"date_end"`
-	BuyNowPrice uint      `json:"buy_now_price"`
+	DateEnd     string `json:"date_end"`
+	BuyNowPrice uint   `json:"buy_now_price"`
 }
