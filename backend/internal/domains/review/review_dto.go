cfe352ec232b31bba5660a0f1d860166b58936b2
KamilMarszalek <kamilmar553@gmail.com>
2025-05-10 11:13:24 +0200

    Added ReviewInput with mapping to object

--- backend/internal/domains/review/review_dto.go
+++ backend/internal/domains/review/review_dto.go
@@ -2,7 +2,7 @@ package review
 
 import "github.com/susek555/BD2/car-dealer-api/internal/domains/user"
 
-type ReviewDTO struct {
+type ReviewOutput struct {
 	ID          uint    `json:"id"`
 	Description string  `json:"description"`
 	Rating      uint    `json:"rating"`
@@ -10,13 +10,29 @@ type ReviewDTO struct {
 	Reviewee    UserDTO `json:"reviewee"`
 }
 
+type ReviewInput struct {
+	Description string `json:"description"`
+	Rating      uint   `json:"rating"`
+	ReviewerId  uint   `json:"reviewer_id"`
+	RevieweeId  uint   `json:"reviewee_id"`
+}
+
 type UserDTO struct {
 	ID       uint   `json:"id"`
 	Username string `json:"username"`
 }
 
-func (r *Review) MapToDTO() ReviewDTO {
-	reviewDTO := ReviewDTO{}
+func (ri *ReviewInput) MapToObject() Review {
+	return Review{
+		Description: ri.Description,
+		Rating:      ri.Rating,
+		ReviewerID:  ri.ReviewerId,
+		RevieweeId:  ri.RevieweeId,
+	}
+}
+
+func (r *Review) MapToDTO() ReviewOutput {
+	reviewDTO := ReviewOutput{}
 	reviewDTO.ID = r.ID
 	reviewDTO.Description = r.Description
 	reviewDTO.Rating = r.Rating
