6b8c0886db70740a1d4bea7d0cb212a7230d8f7b
KamilMarszalek <kamilmar553@gmail.com>
2025-05-10 21:05:23 +0200

    Added ptr

--- backend/internal/domains/auction/auction_model.go
+++ backend/internal/domains/auction/auction_model.go
@@ -6,8 +6,8 @@ import (
 )
 
 type Auction struct {
-	OfferID     uint                 `json:"id" gorm:"primaryKey"`
-	DateEnd     time.Time            `json:"date_end" gorm:"not null;index"`
-	BuyNowPrice uint                 `json:"buy_now_price" gorm:"index"`
-	Offer       sale_offer.SaleOffer `gorm:"foreignKey:OfferID;references:ID;constraint:OnDelete:CASCADE"`
+	OfferID     uint                  `json:"id" gorm:"primaryKey"`
+	DateEnd     time.Time             `json:"date_end" gorm:"not null;index"`
+	BuyNowPrice uint                  `json:"buy_now_price" gorm:"index"`
+	Offer       *sale_offer.SaleOffer `gorm:"foreignKey:OfferID;references:ID;constraint:OnDelete:CASCADE"`
 }
