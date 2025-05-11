c060f9d8267da0dbbb3c32b549734d55620f7a51
KamilMarszalek <kamilmar553@gmail.com>
2025-05-11 14:36:41 +0200

    Completed auction handler

--- backend/internal/domains/auction/auction_handler.go
+++ backend/internal/domains/auction/auction_handler.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/susek555/BD2/car-dealer-api/pkg/custom_errors"
 	"net/http"
+	"strconv"
 )
 
 type Handler struct {
@@ -25,6 +26,7 @@ func (h *Handler) CreateAuction(c *gin.Context) {
 	auction, err := in.MapToAuction()
 	if err != nil {
 		c.JSON(http.StatusBadRequest, custom_errors.NewHTTPError(err.Error()))
+		return
 	}
 	err = h.service.Create(auction)
 	if err != nil {
@@ -39,6 +41,7 @@ func (h *Handler) GetAllAuctions(c *gin.Context) {
 	auctions, err := h.service.GetAll()
 	if err != nil {
 		c.JSON(http.StatusBadRequest, custom_errors.NewHTTPError(err.Error()))
+		return
 	}
 	var auctionsDTO []RetrieveAuctionDTO
 	for _, auction := range auctions {
@@ -46,4 +49,53 @@ func (h *Handler) GetAllAuctions(c *gin.Context) {
 		auctionsDTO = append(auctionsDTO, *dto)
 	}
 	c.JSON(http.StatusOK, auctionsDTO)
+}
+
+func (h *Handler) GetAuctionById(c *gin.Context) {
+	id, err := strconv.ParseUint(c.Param("id"), 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, custom_errors.NewHTTPError(err.Error()))
+		return
+	}
+	auction, err := h.service.GetById(uint(id))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, custom_errors.NewHTTPError(err.Error()))
+		return
+	}
+	dto := MapToDTO(auction)
+	c.JSON(http.StatusOK, dto)
+}
+
+func (h *Handler) DeleteAuctionById(c *gin.Context) {
+	id, err := strconv.ParseUint(c.Param("id"), 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, custom_errors.NewHTTPError(err.Error()))
+		return
+	}
+	err = h.service.Delete(uint(id))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, custom_errors.NewHTTPError(err.Error()))
+		return
+	}
+	c.JSON(http.StatusNoContent, gin.H{})
+}
+
+func (h *Handler) UpdateAuction(c *gin.Context) {
+	var auctionInput UpdateAuctionDTO
+	if err := c.ShouldBindJSON(&auctionInput); err != nil {
+		c.JSON(http.StatusBadRequest, custom_errors.NewHTTPError(err.Error()))
+		return
+	}
+	auction, err := auctionInput.MapToAuction()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, custom_errors.NewHTTPError(err.Error()))
+		return
+	}
+	err = h.service.Update(auction)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, custom_errors.NewHTTPError(err.Error()))
+		return
+	}
+	dto := MapToDTO(auction)
+	c.JSON(http.StatusOK, dto)
 }
