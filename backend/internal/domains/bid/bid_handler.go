9ab667d3a7196ecc3eaffb0889c0ed956a3a55bf
KamilMarszalek <kamilmar553@gmail.com>
2025-05-11 17:42:55 +0200

    Implemented handler with create

---
+++ backend/internal/domains/bid/bid_handler.go
@@ -0,0 +1,29 @@
+package bid
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/susek555/BD2/car-dealer-api/pkg/custom_errors"
+	"net/http"
+)
+
+type Handler struct {
+	service BidServiceInterface
+}
+
+func NewHandler(service BidServiceInterface) *Handler {
+	return &Handler{service: service}
+}
+
+func (h *Handler) CreateBid(c *gin.Context) {
+	var in Bid
+	if err := c.ShouldBindJSON(&in); err != nil {
+		c.JSON(http.StatusBadRequest, custom_errors.NewHTTPError(err.Error()))
+		return
+	}
+	err := h.service.Create(&in)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, custom_errors.NewHTTPError(err.Error()))
+		return
+	}
+	c.JSON(http.StatusCreated, gin.H{})
+}
