0bd2fd52a4a01a70ecb46da1f88b72f14d34ee56
Michal <01187288@pw.edu.pl>
2025-05-11 16:11:46 +0200

    Corrected placeholder image if fetching real one is failed

--- frontend/app/offer/[id]/page.tsx
+++ frontend/app/offer/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { fetchOfferDetails } from "@/app/lib/data";
 import Photos from "@/app/ui/offer/photos";
 import { CarImageOfferPageSkeleton } from "@/app/ui/skeletons";
-import { Suspense } from "react";
+import { Suspense,lazy } from "react";
 
 export default async function Page(props: { params: Promise<{id: string }> }) {
     const { params } = props;
@@ -16,9 +16,10 @@ export default async function Page(props: { params: Promise<{id: string }> }) {
                 <div className="mx-10" />
                 <div className="flex flex-col gap-4">
                     <div className="md:h-120 md:w-200 border-0 border-gray-300">
-                        <Suspense fallback={<CarImageOfferPageSkeleton />}>
+                        {/* <Suspense fallback={<CarImageOfferPageSkeleton />}>
                             <Photos imagesURLs={offer.imagesURLs} />
-                        </Suspense>
+                        </Suspense> */}
+                        <Photos imagesURLs={offer.imagesURLs} />
                         {/* Add your offer details here */}
                     </div>
                 </div>
