f54d478d14f7aa8f92d68ee5ae09178275940c19
KamilMarszalek <kamilmar553@gmail.com>
2025-05-11 15:23:28 +0200

    Fixed delete

--- backend/internal/domains/auction/auction_repository.go
+++ backend/internal/domains/auction/auction_repository.go
@@ -1,6 +1,7 @@
 package auction
 
 import (
+	"github.com/susek555/BD2/car-dealer-api/internal/domains/car"
 	"github.com/susek555/BD2/car-dealer-api/internal/domains/generic"
 	"github.com/susek555/BD2/car-dealer-api/internal/domains/sale_offer"
 	"gorm.io/gorm"
@@ -74,10 +75,11 @@ func (a *AuctionRepository) Update(auction *sale_offer.Auction) error {
 }
 
 func (a *AuctionRepository) Delete(id uint) error {
-	return a.DB.Transaction(func(tx *gorm.DB) error {
-		if err := tx.Delete(&sale_offer.Auction{}, id).Error; err != nil {
-			return err
-		}
-		return nil
-	})
+	return a.DB.
+		Transaction(func(tx *gorm.DB) error {
+			if err := tx.Delete(&car.Car{}, id).Error; err != nil {
+				return err
+			}
+			return nil
+		})
 }
