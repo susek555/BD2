caf4afaa541647b560c839ca23c3ca578cd1377a
Damian D'Souza <dsouzadamian189@gmail.com>
2025-05-11 11:22:12 +0200

    fix conflict

--- frontend/app/test/page.tsx
+++ frontend/app/test/page.tsx
@@ -44,8 +44,6 @@ export default function Test() {
     }
   };
 
-
-
   return (
     <div className="flex items-center justify-center min-h-screen p-8 font-[family-name:var(--font-geist-sans)]">
       <div className="flex flex-col items-center gap-4">
