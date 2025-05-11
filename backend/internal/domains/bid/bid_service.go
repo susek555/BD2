0c764ae9ebcec28095d56afa37e5c9e5e1a379c0
KamilMarszalek <kamilmar553@gmail.com>
2025-05-11 18:26:53 +0200

    Added missing methods to service

--- backend/internal/domains/bid/bid_service.go
+++ backend/internal/domains/bid/bid_service.go
@@ -1,7 +1,7 @@
 package bid
 
 type BidServiceInterface interface {
-	Create(bid *Bid) error
+	BidRepositoryInterface
 }
 
 type BidService struct {
@@ -16,4 +16,28 @@ func NewBidService(repo BidRepositoryInterface) BidServiceInterface {
 
 func (service *BidService) Create(bid *Bid) error {
 	return service.Repo.Create(bid)
+}
+
+func (service *BidService) GetAll() ([]Bid, error) {
+	return service.Repo.GetAll()
+}
+
+func (service *BidService) GetById(id uint) (*Bid, error) {
+	return service.Repo.GetById(id)
+}
+
+func (service *BidService) GetByBidderId(bidderId uint) ([]Bid, error) {
+	return service.Repo.GetByBidderId(bidderId)
+}
+
+func (service *BidService) GetByAuctionId(auctionID uint) ([]Bid, error) {
+	return service.Repo.GetByAuctionId(auctionID)
+}
+
+func (service *BidService) GetHighestBid(auctionId uint) (*Bid, error) {
+	return service.Repo.GetHighestBid(auctionId)
+}
+
+func (service *BidService) GetHighestBidByUserId(auctionId, userId uint) (*Bid, error) {
+	return service.Repo.GetHighestBidByUserId(auctionId, userId)
 }
