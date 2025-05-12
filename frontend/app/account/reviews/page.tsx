115f83ba10f6198383309d4dbcb631553539a3d3
Damian D'Souza <dsouzadamian189@gmail.com>
2025-05-13 00:14:55 +0200

    add initial account pages

---
+++ frontend/app/account/reviews/page.tsx
@@ -0,0 +1,9 @@
+async function ReviewsData() {}
+
+export default function ReviewsPage() {
+  return (
+    <div className='p-6'>
+      <h2 className='mb-4 text-xl font-semibold text-gray-900'>Reviews</h2>
+    </div>
+  );
+}
