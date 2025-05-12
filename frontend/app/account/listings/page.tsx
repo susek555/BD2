df2ee7d2c86211e63bcca8e37fa48ad3f1d57be9
Damian D'Souza <dsouzadamian189@gmail.com>
2025-05-13 00:14:55 +0200

    add initial account pages

---
+++ frontend/app/account/listings/page.tsx
@@ -0,0 +1,9 @@
+async function ListingsData() {}
+
+export default function ListingsPage() {
+  return (
+    <div className='p-6'>
+      <h2 className='mb-4 text-xl font-semibold text-gray-900'>My Listings</h2>
+    </div>
+  );
+}
---
+++ frontend/app/account/settings/page.tsx
@@ -0,0 +1,11 @@
+async function SettingsData() {}
+
+export default function SettingsPage() {
+  return (
+    <div className='p-6'>
+      <h2 className='mb-4 text-xl font-semibold text-gray-900'>
+        Account Settings
+      </h2>
+    </div>
+  );
+}
