370d4675df8d7f9e799e781a34c7acc8e7d68a30
KamilMarszalek <kamilmar553@gmail.com>
2025-05-13 19:43:10 +0200

    Moved mapping to separate file

---
+++ backend/internal/domains/review/review_mapping.go
@@ -0,0 +1,31 @@
+package review
+
+import "github.com/susek555/BD2/car-dealer-api/internal/domains/user"
+
+func (ri *CreateReviewDTO) MapToObject(reviewerId uint) Review {
+	return Review{
+		Description: ri.Description,
+		Rating:      ri.Rating,
+		ReviewerID:  reviewerId,
+		RevieweeId:  ri.RevieweeId,
+	}
+}
+
+func (r *Review) MapToDTO() RetrieveReviewDTO {
+	reviewDTO := RetrieveReviewDTO{}
+	reviewDTO.ID = r.ID
+	reviewDTO.Description = r.Description
+	reviewDTO.Rating = r.Rating
+	reviewee := MapToUserDTO(r.Reviewee)
+	reviewDTO.Reviewee = reviewee
+	reviewer := MapToUserDTO(r.Reviewer)
+	reviewDTO.Reviewer = reviewer
+	return reviewDTO
+}
+
+func MapToUserDTO(u *user.User) UserDTO {
+	return UserDTO{
+		ID:       u.ID,
+		Username: u.Username,
+	}
+}
