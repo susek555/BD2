4bc547b2293775069a9676c0cf77dbbcab403157
KamilMarszalek <kamilmar553@gmail.com>
2025-05-13 20:32:11 +0200

    Improved user id retriever

--- backend/internal/domains/auth/userId_retriever.go
+++ backend/internal/domains/auth/userId_retriever.go
@@ -2,10 +2,10 @@ package auth
 
 import "github.com/gin-gonic/gin"
 
-func GetUserId(c *gin.Context) uint {
+func GetUserId(c *gin.Context) (uint, error) {
 	userId, exists := c.Get("userID")
 	if !exists {
-		return 0
+		return 0, ErrUserIdNotFound
 	}
-	return userId.(uint)
+	return userId.(uint), nil
 }
