5b7120096cfc4e3c905337d18aebc8dbc59468e6
KamilMarszalek <kamilmar553@gmail.com>
2025-05-13 19:43:10 +0200

    Moved mapping to separate file

--- backend/internal/domains/review/review_dto.go
+++ backend/internal/domains/review/review_dto.go
@@ -1,7 +1,5 @@
 package review
 
-import "github.com/susek555/BD2/car-dealer-api/internal/domains/user"
-
 type RetrieveReviewDTO struct {
 	ID          uint    `json:"id"`
 	Description string  `json:"description"`
@@ -19,32 +17,4 @@ type CreateReviewDTO struct {
 type UserDTO struct {
 	ID       uint   `json:"id"`
 	Username string `json:"username"`
-}
-
-func (ri *CreateReviewDTO) MapToObject(reviewerId uint) Review {
-	return Review{
-		Description: ri.Description,
-		Rating:      ri.Rating,
-		ReviewerID:  reviewerId,
-		RevieweeId:  ri.RevieweeId,
-	}
-}
-
-func (r *Review) MapToDTO() RetrieveReviewDTO {
-	reviewDTO := RetrieveReviewDTO{}
-	reviewDTO.ID = r.ID
-	reviewDTO.Description = r.Description
-	reviewDTO.Rating = r.Rating
-	reviewee := MapToUserDTO(r.Reviewee)
-	reviewDTO.Reviewee = reviewee
-	reviewer := MapToUserDTO(r.Reviewer)
-	reviewDTO.Reviewer = reviewer
-	return reviewDTO
-}
-
-func MapToUserDTO(u *user.User) UserDTO {
-	return UserDTO{
-		ID:       u.ID,
-		Username: u.Username,
-	}
 }
