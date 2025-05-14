ca513b91b3266a1269234bf26262b1170f613a07
Michal <01187288@pw.edu.pl>
2025-05-14 17:58:43 +0200

    Passed fetched AddOfferFormData to actual component

--- frontend/app/offer/add/page.tsx
+++ frontend/app/offer/add/page.tsx
@@ -1,11 +1,15 @@
+import { fetchAddOfferFormData } from "@/app/lib/data";
 import AddOfferForm from "@/app/ui/offer/add/add-offer-form";
 
 export default async function Page() {
+
+    const formData = await fetchAddOfferFormData();
+
     return (
         <div className="px-20 py-10">
             <h1 className="text-3xl font-bold pl-10">Add Offer</h1>
             <div className="m-5"/>
-            <AddOfferForm />
+            <AddOfferForm inputsData = {formData}/>
         </div>
     );
 }
