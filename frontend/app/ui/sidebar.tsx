9dcf65cb8d6d7c4f25dfc7d69a29893fef076732
Michal <01187288@pw.edu.pl>
2025-05-14 15:50:47 +0200

    Moved producers  filter field to seperate module

--- frontend/app/ui/sidebar.tsx
+++ frontend/app/ui/sidebar.tsx
@@ -1,24 +1,25 @@
-import {
-  fetchFilterFields,
-  fetchSortingOptions,
-  prepareRangeFields,
-} from '@/app/lib/data';
 import Filters from '@/app/ui/(home)/filters';
-import OfferType from './offer-type';
 import Sorting from './sorting';
+import OfferType from './offer-type';
+import { fetchFilterFields, fetchProducersAndModels, fetchSortingOptions, prepareRangeFields } from '@/app/lib/data';
+import ProducersAndModels from '@/app/ui/producers-and-models';
+
 
 export default async function SideBar() {
-  const filters = await fetchFilterFields();
-  const ranges = await prepareRangeFields();
-  const sortingOptions = await fetchSortingOptions();
+    const filters = await fetchFilterFields();
+    const ranges = await prepareRangeFields();
+    const sortingOptions = await fetchSortingOptions();
+    const producersAndModels = await fetchProducersAndModels();
 
-  return (
-    <div className='flex h-full flex-col rounded-lg border-2 border-gray-300 bg-white p-4'>
-      <div className='flex flex-col space-y-4'>
-        <OfferType />
-        <Sorting sortingOptions={sortingOptions} />
-        <Filters filtersData={filters} rangesData={ranges} />
+    return (
+      <div className="flex h-full flex-col px-3 py-2 md:px-2 rounded-r-lg border-black border-[2px]">
+        <div className="flex grow flex-col justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+          <OfferType />
+          <Sorting sortingOptions={sortingOptions}/>
+          <ProducersAndModels producersAndModels={producersAndModels}/>
+          <Filters filtersData={filters} rangesData={ranges}/>
+          <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+        </div>
       </div>
-    </div>
-  );
+    );
 }
