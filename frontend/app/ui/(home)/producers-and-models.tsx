e3480b104c7cecfdeb7c747cb0eaf53cae937f62
Michal <01187288@pw.edu.pl>
2025-05-14 16:03:14 +0200

    Dynamically showed models filter field

--- frontend/app/ui/(home)/producers-and-models.tsx
+++ frontend/app/ui/(home)/producers-and-models.tsx
@@ -1,9 +1,10 @@
 'use client'
 
 import { syncFiltersWithParams } from "@/app/lib/(home)/syncWithParams";
-import { ModelFieldData } from "@/app/lib/definitions"
+import { ModelFieldData, FilterFieldData } from "@/app/lib/definitions";
 import { BaseFilterTemplate } from "@/app/ui/(home)/base-filter-template/base-filter-template";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 export default function ProducersAndModels({ producersAndModels }: { producersAndModels: ModelFieldData }) {
     const searchParams = useSearchParams();
@@ -11,17 +12,31 @@ export default function ProducersAndModels({ producersAndModels }: { producersAn
     const { replace } = useRouter();
 
     const producers = syncFiltersWithParams([producersAndModels.producers], searchParams)[0];
+    let models: FilterFieldData = { fieldName: "Models", options: [] };
 
-    function handleProducerChange(name: string, selected: string[]) {
+    // Stan widoczności pola Models
+    const [showModels, setShowModels] = useState<boolean>(!!producers.selected && producers.selected.length > 0);
+
+    function handleChange(name: string, selected: string[]) {
         const params = new URLSearchParams(searchParams);
         const sanitizedName = name.replace(/\s+/g, ''); // Remove whitespaces from name
 
         params.set('page', '1'); // Reset to the first page
         if (selected.length > 0) {
             params.set(sanitizedName, selected.join(","));
+            // Natychmiastowa aktualizacja widoczności
+            if (name === producers.fieldName) {
+                setShowModels(true);
+            }
         } else {
             params.delete(sanitizedName);
+            // Ukryj pole Models, jeśli nie ma wybranego producenta
+            if (name === producers.fieldName) {
+                setShowModels(false);
+            }
         }
+
+        console.log("producers.selected", producers.selected);
 
         replace(`${pathname}?${params.toString()}`);
     }
@@ -33,7 +48,16 @@ export default function ProducersAndModels({ producersAndModels }: { producersAn
                 name={producers.fieldName}
                 options={producers.options}
                 selected={producers.selected}
-                onChange={handleProducerChange} />
+                onChange={handleChange}
+            />
+            {showModels && (
+                <BaseFilterTemplate
+                    name={models.fieldName}
+                    options={models.options}
+                    selected={models.selected}
+                    onChange={handleChange}
+                />
+            )}
         </>
-    )
+    );
 }
