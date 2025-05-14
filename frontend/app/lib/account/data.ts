b76ff4b9dfa4bc3dbd511dc8efc98c116609408b
Damian D'Souza <dsouzadamian189@gmail.com>
2025-05-15 00:03:14 +0200

    add fetchHistory function with test data

--- frontend/app/lib/account/data.ts
+++ frontend/app/lib/account/data.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from 'next-auth';
 import { authConfig } from '../authConfig';
-import { UserProfile } from '../definitions';
+import { HistoryOffer, SearchParams, UserProfile } from '../definitions';
 
 export async function fetchSessionData(): Promise<UserProfile> {
   const user = (await getServerSession(authConfig))?.user;
@@ -23,4 +23,72 @@ export async function fetchSessionData(): Promise<UserProfile> {
   }
 
   return userProfile;
+}
+
+export async function fetchHistory(
+  params: SearchParams,
+): Promise<HistoryOffer[]> {
+  const data: HistoryOffer[] = [
+    {
+      id: '1',
+      name: 'Audi A4',
+      productionYear: 2000,
+      mileage: 150000,
+      color: 'Green',
+      price: 10000,
+      isAuction: true,
+      dateEnd: '2025-05-24',
+    },
+    {
+      id: '2',
+      name: 'Volkswagen Golf',
+      productionYear: 2005,
+      mileage: 120000,
+      color: 'Blue',
+      price: 15000,
+      isAuction: false,
+      dateEnd: '2025-04-11',
+    },
+    {
+      id: '3',
+      name: 'Porsche 911',
+      productionYear: 2010,
+      mileage: 80000,
+      color: 'Red',
+      price: 50000,
+      isAuction: true,
+      dateEnd: '2024-11-11',
+    },
+    {
+      id: '4',
+      name: 'Porsche 911',
+      productionYear: 2010,
+      mileage: 80000,
+      color: 'Red',
+      price: 50000,
+      isAuction: true,
+      dateEnd: '2024-11-11',
+    },
+    {
+      id: '5',
+      name: 'Porsche 911',
+      productionYear: 2010,
+      mileage: 80000,
+      color: 'Red',
+      price: 50000,
+      isAuction: true,
+      dateEnd: '2024-11-11',
+    },
+    {
+      id: '6',
+      name: 'Porsche 911',
+      productionYear: 2010,
+      mileage: 80000,
+      color: 'Red',
+      price: 50000,
+      isAuction: true,
+      dateEnd: '2024-11-11',
+    },
+  ];
+  return data;
 }
