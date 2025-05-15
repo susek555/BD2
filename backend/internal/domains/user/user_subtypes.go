3983f5e1cc74b05c7c43bbe47aab5e8480945058
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-05-15 19:12:32 +0200

    Fixed foreign keys constraints.

--- backend/internal/domains/user/user_subtypes.go
+++ backend/internal/domains/user/user_subtypes.go
@@ -6,14 +6,14 @@ type Company struct {
 	UserID uint   `json:"id" gorm:"primaryKey"`
 	NIP    string `json:"nip" gorm:"unique"`
 	Name   string `json:"name"`
-	User   User   `gorm:"foreignKey:UserID;references:ID;constraint:OnDelete:CASCADE"`
+	User   User   `gorm:"foreignKey:UserID;references:ID"`
 }
 
 type Person struct {
 	UserID  uint   `json:"id" gorm:"primaryKey"`
 	Name    string `json:"name"`
 	Surname string `json:"surname"`
-	User    User   `gorm:"foreignKey:UserID;references:ID;constraint:OnDelete:CASCADE"`
+	User    User   `gorm:"foreignKey:UserID;references:ID"`
 }
 
 type UserSubtype interface {
