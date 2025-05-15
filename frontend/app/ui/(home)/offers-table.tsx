1172ecb9eb13395bee3af15d3e475f6d20e97bc8
Damian D'Souza <dsouzadamian189@gmail.com>
2025-05-15 18:39:52 +0200

    refactor to use GenericOffersTable

--- frontend/app/ui/(home)/offers-table.tsx
+++ frontend/app/ui/(home)/offers-table.tsx
@@ -1,6 +1,6 @@
 import { fetchOffers } from '@/app/lib/data';
-import { SearchParams } from '@/app/lib/definitions';
-import Link from 'next/link';
+import { SaleOffer, SearchParams } from '@/app/lib/definitions';
+import GenericOffersTable from '../generic-offer-table';
 import SingleOffer from './single-offer';
 
 export default async function OffersTable({
@@ -8,21 +8,11 @@ export default async function OffersTable({
 }: {
   params: SearchParams;
 }) {
-  const offers = await fetchOffers(params);
-
   return (
-    <div className='flex flex-col gap-4'>
-      {offers.map((offer) => (
-        <Link
-          key={offer.id}
-          href={`/account/activity/${offer.id}`}
-          className={`block rounded-lg transition-shadow hover:shadow-lg ${
-            offer.isAuction ? 'ring-2 ring-blue-500' : 'ring-2 ring-gray-500'
-          }`}
-        >
-          <SingleOffer offer={offer} />
-        </Link>
-      ))}
-    </div>
+    <GenericOffersTable<SaleOffer>
+      fetchFunction={fetchOffers}
+      params={params}
+      ItemComponent={SingleOffer}
+    />
   );
 }
