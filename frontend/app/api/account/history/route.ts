c2eac744d176d7f23dc2de4ad16bd59e83687b3e
Damian D'Souza <dsouzadamian189@gmail.com>
2025-05-16 01:10:47 +0200

    move test data to api routes

---
+++ frontend/app/api/account/history/route.ts
@@ -0,0 +1,71 @@
+import { HistoryOffer } from '@/app/lib/definitions';
+import { NextResponse } from 'next/server';
+
+const API_URL = process.env.API_URL;
+
+export async function GET(request: Request) {
+  const data: HistoryOffer[] = [
+    {
+      id: '1',
+      name: 'Audi A4',
+      productionYear: 2000,
+      mileage: 150000,
+      color: 'Green',
+      price: 10000,
+      isAuction: true,
+      dateEnd: '2025-05-24',
+    },
+    {
+      id: '2',
+      name: 'Volkswagen Golf',
+      productionYear: 2005,
+      mileage: 120000,
+      color: 'Blue',
+      price: 15000,
+      isAuction: false,
+      dateEnd: '2025-04-11',
+    },
+    {
+      id: '3',
+      name: 'Porsche 911',
+      productionYear: 2010,
+      mileage: 80000,
+      color: 'Red',
+      price: 50000,
+      isAuction: true,
+      dateEnd: '2024-11-11',
+    },
+    {
+      id: '4',
+      name: 'Porsche 911',
+      productionYear: 2010,
+      mileage: 80000,
+      color: 'Red',
+      price: 50000,
+      isAuction: true,
+      dateEnd: '2024-11-11',
+    },
+    {
+      id: '5',
+      name: 'Porsche 911',
+      productionYear: 2010,
+      mileage: 80000,
+      color: 'Red',
+      price: 50000,
+      isAuction: true,
+      dateEnd: '2024-11-11',
+    },
+    {
+      id: '6',
+      name: 'Porsche 911',
+      productionYear: 2010,
+      mileage: 80000,
+      color: 'Red',
+      price: 50000,
+      isAuction: true,
+      dateEnd: '2024-11-11',
+    },
+  ];
+
+  return NextResponse.json(data);
+}
---
+++ frontend/app/api/account/listings/route.ts
@@ -0,0 +1,65 @@
+import { BaseOffer } from '@/app/lib/definitions';
+import { NextResponse } from 'next/server';
+
+const API_URL = process.env.API_URL;
+
+export async function GET(request: Request) {
+  const data: BaseOffer[] = [
+    {
+      id: '1',
+      name: 'Audi A4',
+      productionYear: 2000,
+      mileage: 150000,
+      color: 'Green',
+      price: 10000,
+      isAuction: true,
+    },
+    {
+      id: '2',
+      name: 'Volkswagen Golf',
+      productionYear: 2005,
+      mileage: 120000,
+      color: 'Blue',
+      price: 15000,
+      isAuction: false,
+    },
+    {
+      id: '3',
+      name: 'Porsche 911',
+      productionYear: 2010,
+      mileage: 80000,
+      color: 'Red',
+      price: 50000,
+      isAuction: true,
+    },
+    {
+      id: '4',
+      name: 'Porsche 911',
+      productionYear: 2010,
+      mileage: 80000,
+      color: 'Red',
+      price: 50000,
+      isAuction: true,
+    },
+    {
+      id: '5',
+      name: 'Porsche 911',
+      productionYear: 2010,
+      mileage: 80000,
+      color: 'Red',
+      price: 50000,
+      isAuction: true,
+    },
+    {
+      id: '6',
+      name: 'Porsche 911',
+      productionYear: 2010,
+      mileage: 80000,
+      color: 'Red',
+      price: 50000,
+      isAuction: true,
+    },
+  ];
+
+  return NextResponse.json(data);
+}
