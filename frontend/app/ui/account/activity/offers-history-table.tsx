27f7fc109dfe9a3dafd2affb35ba7d2888240e3e
Damian D'Souza <dsouzadamian189@gmail.com>
2025-05-15 19:18:00 +0200

    change imports

--- frontend/app/ui/account/activity/offers-history-table.tsx
+++ frontend/app/ui/account/activity/offers-history-table.tsx
@@ -1,7 +1,7 @@
 import { fetchHistory } from '@/app/lib/account/data';
 import { HistoryOffer, SearchParams } from '@/app/lib/definitions';
-import GenericOffersTable from '../../generic-offer-table';
-import SingleHistoryOffer from './single-offer';
+import GenericOffersTable from '@/app/ui/generic-offer-table';
+import SingleHistoryOffer from './single-history-offer';
 
 export default async function OffersHistory({
   params,
