790ecf43eef983cfd83955decacd80995ade5c99
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-05-15 19:06:33 +0200

    Fixed foreign keys constraints.

--- backend/internal/domains/sale_offer/auction_model.go
+++ backend/internal/domains/sale_offer/auction_model.go
@@ -5,8 +5,8 @@ import (
 )
 
 type Auction struct {
-	OfferID     uint       `json:"id" gorm:"primaryKey"`
-	DateEnd     time.Time  `json:"date_end" gorm:"not null;index"`
-	BuyNowPrice uint       `json:"buy_now_price" gorm:"index"`
-	Offer       *SaleOffer `gorm:"foreignKey:OfferID;references:ID;constraint:OnDelete:CASCADE"`
+	OfferID     uint      `json:"id" gorm:"primaryKey"`
+	DateEnd     time.Time `json:"date_end" gorm:"not null;index"`
+	BuyNowPrice uint      `json:"buy_now_price" gorm:"index"`
+	Offer       SaleOffer `gorm:"foreignKey:OfferID;references:ID"`
 }
