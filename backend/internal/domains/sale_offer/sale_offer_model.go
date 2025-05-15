15c6f37435432b323af793d1cabc13f51807bb55
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-05-15 19:06:33 +0200

    Fixed foreign keys constraints.

--- backend/internal/domains/sale_offer/sale_offer_model.go
+++ backend/internal/domains/sale_offer/sale_offer_model.go
@@ -3,7 +3,6 @@ package sale_offer
 import (
 	"time"
 
-	"github.com/susek555/BD2/car-dealer-api/internal/domains/car"
 	"github.com/susek555/BD2/car-dealer-api/internal/domains/user"
 )
 
@@ -14,8 +13,7 @@ type SaleOffer struct {
 	Description string     `json:"description"`
 	Price       uint       `json:"price"`
 	Margin      uint       `json:"margin"`
-	DateOfIssue time.Time  `jsont:"date_of_issue"`
-	CarID       uint       `json:"car_id"`
-	Car         *car.Car   `gorm:"foreignKey:CarID;references:ID"`
-	Auction     *Auction   `gorm:"foreignKey:OfferID;references:ID;constraint:OnDelete:CASCADE"`
+	DateOfIssue time.Time  `json:"date_of_issue"`
+	Car         *Car       `gorm:"foreignKey:OfferID;references:ID;constraint:OnDelete:CASCADE,OnUpdate:CASCADE"`
+	Auction     *Auction   `gorm:"foreignKey:OfferID;references:ID;constraint:OnDelete:CASCADE,OnUpdate:CASCADE"`
 }
