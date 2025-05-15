ba1dfe9730a61f72e39baa8619ae837769845994
Damian D'Souza <dsouzadamian189@gmail.com>
2025-05-16 00:12:05 +0200

    add skeleton favorites api functinos

---
+++ frontend/app/api/favorites/[id]/route.ts
@@ -0,0 +1,21 @@
+const API_URL = process.env.API_URL;
+
+export async function POST(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  const { id } = await params;
+  // TODO implement API call to add to favorites
+  console.log(`Offer ${id} added to favorites`);
+  return Response.json({ success: true });
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  const { id } = await params;
+  // TODO implement API call to delete from favorites
+  console.log(`Offer ${id} removed from favorites`);
+  return Response.json({ success: true });
+}
