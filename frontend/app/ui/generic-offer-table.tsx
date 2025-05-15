e71b70dd77284216407675ce53b562a54a3ce3d3
Damian D'Souza <dsouzadamian189@gmail.com>
2025-05-15 18:39:32 +0200

    implement GenericOfferTable to use in different sceens

---
+++ frontend/app/ui/generic-offer-table.tsx
@@ -0,0 +1,34 @@
+import { SaleOffer, SearchParams } from '@/app/lib/definitions';
+import Link from 'next/link';
+
+interface GenericTableProps<T extends SaleOffer> {
+  fetchFunction: (params: SearchParams) => Promise<T[]>;
+  params: SearchParams;
+  ItemComponent: React.ComponentType<{ offer: T }>;
+  className?: string;
+}
+
+export default async function GenericOffersTable<T extends SaleOffer>({
+  fetchFunction,
+  params,
+  ItemComponent,
+  className = '',
+}: GenericTableProps<T>) {
+  const offers = await fetchFunction(params);
+
+  return (
+    <div className={`flex flex-col gap-4 ${className}`}>
+      {offers.map((offer) => (
+        <Link
+          key={offer.id}
+          href={`/${offer.id}`}
+          className={`block rounded-lg transition-shadow hover:shadow-lg ${
+            offer.isAuction ? 'ring-2 ring-blue-500' : 'ring-2 ring-gray-500'
+          }`}
+        >
+          <ItemComponent offer={offer} />
+        </Link>
+      ))}
+    </div>
+  );
+}
