3d81d3fa6fa6d9540d5f9df7d6b2f52a25696054
Damian D'Souza <dsouzadamian189@gmail.com>
2025-05-15 17:59:32 +0200

    add skeleton api requests related to listing

---
+++ frontend/app/api/listing/[id]/route.ts
@@ -0,0 +1,31 @@
+const API_URL = process.env.API_URL;
+
+export async function PUT(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  const { id } = await params;
+  console.log('Update listing id:', id);
+  // TODO implement API call to update listing
+  return Response.json({ success: true });
+}
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  const { id } = await params;
+  console.log('Get listing id:', id);
+  // TODO implement API call to get listing
+  return Response.json({ id: params.id });
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  const { id } = await params;
+  console.log('Deleted listing id:', id);
+  // TODO implement API call to delete listing
+  return Response.json({ success: true });
+}
