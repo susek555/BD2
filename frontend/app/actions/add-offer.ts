7d36818353548792d8157b00fc55464b1d7f7083
Michal <01187288@pw.edu.pl>
2025-05-16 13:10:07 +0200

    Prepared conversion to AddOfferFormState

--- frontend/app/actions/add-offer.ts
+++ frontend/app/actions/add-offer.ts
@@ -7,21 +7,23 @@ export async function addOffer(
 ): Promise<AddOfferFormState> {
     console.log("Add Offer form data:", Object.fromEntries(formData.entries()));
 
+    const formDataObj = Object.fromEntries(formData.entries());
+    const validatedFields = AddOfferFormSchema.safeParse(formDataObj);
+    console.log("Add Offer validation result:", validatedFields);
+
+
+    if (!validatedFields.success) {
+        console.log("Add Offer validation errors:", validatedFields.error.flatten().fieldErrors);
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            values: Object.fromEntries(
+                Object.entries(formDataObj).filter(([key]) => !key.includes('password'))
+            ) as AddOfferFormState['values']
+        };
+    }
+
+
     return state;
-
-    // const formDataObj = Object.fromEntries(formData.entries());
-    // const validatedFields = AddOfferFormSchema.safeParse(formDataObj);
-    // console.log("Add Offer validation result:", validatedFields);
-
-    // if (!validatedFields.success) {
-    //     console.log("Add Offer validation errors:", validatedFields.error.flatten().fieldErrors);
-    //     return {
-    //         errors: validatedFields.error.flatten().fieldErrors,
-    //         values: Object.fromEntries(
-    //             Object.entries(formDataObj).filter(([key]) => !key.includes('password'))
-    //         ) as AddOfferFormState['values']
-    //     };
-    // }
 
     // permanentRedirect("/");
 }
