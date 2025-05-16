17d0a14b2d33f78ef8f1b22542a2e37804fd1203
KamilMarszalek <kamilmar553@gmail.com>
2025-05-16 16:25:17 +0200

    Added userId to LoginResponse

--- backend/internal/domains/auth/login_response.go
+++ backend/internal/domains/auth/login_response.go
@@ -5,6 +5,7 @@ type LoginResponse struct {
 	AccessToken   string              `json:"access_token,omitempty"`
 	Selector      string              `json:"selector,omitempty"`
 	Username      string              `json:"username,omitempty"`
+	UserID        uint                `json:"user_id,omitempty"`
 	Email         string              `json:"email,omitempty"`
 	PersonName    string              `json:"person_name,omitempty"`
 	PersonSurname string              `json:"person_surname,omitempty"`
