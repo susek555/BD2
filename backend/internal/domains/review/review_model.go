61b9dfb3e250baa2ef6d395461b68f470003d2bc
KamilMarszalek <kamilmar553@gmail.com>
2025-05-16 18:17:23 +0200

    Changed param name

--- backend/internal/domains/review/review_model.go
+++ backend/internal/domains/review/review_model.go
@@ -5,7 +5,7 @@ import "github.com/susek555/BD2/car-dealer-api/internal/domains/user"
 type Review struct {
 	ID          uint       `json:"id" gorm:"primary_key;autoIncrement"`
 	Description string     `json:"description" gorm:"not null"`
-	Date        string     `json:"date" gorm:"not null;default:CURRENT_TIMESTAMP"`
+	ReviewDate  string     `json:"date" gorm:"not null;default:CURRENT_TIMESTAMP"`
 	Rating      uint       `json:"rating" gorm:"not null;check:rating BETWEEN 1 AND 5"`
 	ReviewerID  uint       `json:"reviewer_id" gorm:"not null;check:reviewer_id <> reviewee_id;uniqueIndex:ux_review_pair"`
 	Reviewer    *user.User `gorm:"foreignKey:ReviewerID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
