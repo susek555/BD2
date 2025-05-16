d000a31de4cd291e0db231495aedb37240d43303
Michal <01187288@pw.edu.pl>
2025-05-16 10:47:15 +0200

    Prepared selection drop-down list function for Add offer form

--- frontend/app/ui/offer/add/add-offer-form.tsx
+++ frontend/app/ui/offer/add/add-offer-form.tsx
@@ -3,24 +3,38 @@
 import { AddOfferFormData } from "@/app/lib/definitions";
 
 export default function AddOfferForm({ inputsData } : { inputsData : AddOfferFormData}) {
+
     function handleSubmit(formData: FormData): any {
         const formDataObj = Object.fromEntries(formData.entries());
         console.log("Add Offer form data:", formDataObj);
         return formDataObj;
     }
 
+    function SelectionLabel({ name, options }: { name: string; options: string[] }) {
+        return (
+            <>
+                <label htmlFor={`${name.toLowerCase()}`} className="text-lg font-semibold">{name}</label>
+                <select id={`${name.toLowerCase()}`} name={`${name.toLowerCase()}`} className="border rounded p-2" required defaultValue="">
+                    <option value="" disabled>Select a {name}</option>
+                    {options.map((item: string) => (
+                        <option key={item} value={item}>{item}</option>
+                    ))}
+                </select>
+            </>
+        )
+    }
 
     return (
         <form className=" w-full md:w-200" action={handleSubmit}>
             <div className="rounded-lg bg-gray-50 px-6 pb-4 pt-8 flex flex-col gap-4">
-                <label htmlFor="category" className="text-lg font-semibold">Category</label>
-                <select id="category" name="category" className="border rounded p-2" required defaultValue="">
-                    <option value="" disabled>Select a category</option>
-                    <option value="electronics">Electronics</option>
-                    <option value="furniture">Furniture</option>
-                    <option value="clothing">Clothing</option>
-                    <option value="books">Books</option>
-                </select>
+                <SelectionLabel name="Producer" options={inputsData.producers} />
+                {/* <label htmlFor="models" className="text-lg font-semibold">Models</label>
+                <select id="models" name="models" className="border rounded p-2" required defaultValue="">
+                    <option value="" disabled>Select a model...</option>
+                    {inputsData.models.map((item: string) => (
+                        <option key={item} value={item}>{item}</option>
+                    ))}
+                </select> */}
 
                 <label htmlFor="description" className="text-lg font-semibold">Description</label>
                 <textarea id="description" name="description" className="border rounded p-2 h-32" required></textarea>
