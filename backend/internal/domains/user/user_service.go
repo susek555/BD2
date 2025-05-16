3880fb5bb9fa7766e6f1dbbaca2d747be8caefb6
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-05-16 02:49:10 +0200

    Small refactor.

--- backend/internal/domains/user/user_service.go
+++ backend/internal/domains/user/user_service.go
@@ -1,13 +1,15 @@
 package user
 
+import "github.com/susek555/BD2/car-dealer-api/pkg/mapping"
+
 type UserServiceInterface interface {
 	Create(CreateUserDTO) error
 	GetAll() ([]RetrieveUserDTO, error)
-	GetById(id uint) (RetrieveUserDTO, error)
-	GetByEmail(email string) (RetrieveUserDTO, error)
-	GetByCompanyNip(email string) (RetrieveUserDTO, error)
-	GetByUsername(username string) (RetrieveUserDTO, error)
-	Update(UpdateUserDTO) error
+	GetById(id uint) (*RetrieveUserDTO, error)
+	GetByEmail(email string) (*RetrieveUserDTO, error)
+	GetByCompanyNip(email string) (*RetrieveUserDTO, error)
+	GetByUsername(username string) (*RetrieveUserDTO, error)
+	Update(*UpdateUserDTO) error
 	Delete(id uint) error
 }
 
@@ -32,51 +34,47 @@ func (s *UserService) GetAll() ([]RetrieveUserDTO, error) {
 	if err != nil {
 		return nil, err
 	}
-	userDTOs := make([]RetrieveUserDTO, 0, len(users))
-	for _, user := range users {
-		dto, _ := user.MapToDTO()
-		userDTOs = append(userDTOs, dto)
-	}
+	userDTOs := mapping.MapSliceToDTOs(users, (*User).MapToDTO)
 	return userDTOs, nil
 }
 
-func (s *UserService) GetById(id uint) (RetrieveUserDTO, error) {
+func (s *UserService) GetById(id uint) (*RetrieveUserDTO, error) {
 	user, err := s.repo.GetById(id)
 	if err != nil {
-		return RetrieveUserDTO{}, err
+		return nil, err
 	}
-	userDTO, _ := user.MapToDTO()
+	userDTO := user.MapToDTO()
 	return userDTO, nil
 }
 
-func (s *UserService) GetByEmail(email string) (RetrieveUserDTO, error) {
+func (s *UserService) GetByEmail(email string) (*RetrieveUserDTO, error) {
 	user, err := s.repo.GetByEmail(email)
 	if err != nil {
-		return RetrieveUserDTO{}, err
+		return nil, err
 	}
-	userDTO, _ := user.MapToDTO()
+	userDTO := user.MapToDTO()
 	return userDTO, nil
 }
 
-func (s *UserService) GetByUsername(username string) (RetrieveUserDTO, error) {
+func (s *UserService) GetByUsername(username string) (*RetrieveUserDTO, error) {
 	user, err := s.repo.GetByUsername(username)
 	if err != nil {
-		return RetrieveUserDTO{}, err
+		return nil, err
 	}
-	userDTO, _ := user.MapToDTO()
+	userDTO := user.MapToDTO()
 	return userDTO, nil
 }
 
-func (s *UserService) GetByCompanyNip(nip string) (RetrieveUserDTO, error) {
+func (s *UserService) GetByCompanyNip(nip string) (*RetrieveUserDTO, error) {
 	user, err := s.repo.GetByCompanyNip(nip)
 	if err != nil {
-		return RetrieveUserDTO{}, err
+		return nil, err
 	}
-	userDTO, _ := user.MapToDTO()
+	userDTO := user.MapToDTO()
 	return userDTO, nil
 }
 
-func (s *UserService) Update(in UpdateUserDTO) error {
+func (s *UserService) Update(in *UpdateUserDTO) error {
 	user, err := s.repo.GetById(in.ID)
 	if err != nil {
 		return err
