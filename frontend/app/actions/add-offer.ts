f217d3ee709ab08057360739ed6cd890e22857ac
Michal <01187288@pw.edu.pl>
2025-05-16 13:01:02 +0200

    Implemented handleSubmit in addOffer

---
+++ frontend/app/actions/add-offer.ts
@@ -0,0 +1,27 @@
+import { AddOfferFormSchema, AddOfferFormState } from "../lib/definitions";
+import { permanentRedirect } from "next/navigation";
+
+export async function addOffer(
+    state: AddOfferFormState,
+    formData: FormData
+): Promise<AddOfferFormState> {
+    console.log("Add Offer form data:", Object.fromEntries(formData.entries()));
+
+    return state;
+
+    // const formDataObj = Object.fromEntries(formData.entries());
+    // const validatedFields = AddOfferFormSchema.safeParse(formDataObj);
+    // console.log("Add Offer validation result:", validatedFields);
+
+    // if (!validatedFields.success) {
+    //     console.log("Add Offer validation errors:", validatedFields.error.flatten().fieldErrors);
+    //     return {
+    //         errors: validatedFields.error.flatten().fieldErrors,
+    //         values: Object.fromEntries(
+    //             Object.entries(formDataObj).filter(([key]) => !key.includes('password'))
+    //         ) as AddOfferFormState['values']
+    //     };
+    // }
+
+    // permanentRedirect("/");
+}
