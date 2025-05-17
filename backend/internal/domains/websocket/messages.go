0c3dce179bb1e7729db8860a7e33b5622e7331c2
KamilMarszalek <kamilmar553@gmail.com>
2025-05-17 12:23:31 +0200

    Fix field naming conventions in websocket message types

--- backend/internal/domains/websocket/messages.go
+++ backend/internal/domains/websocket/messages.go
@@ -6,14 +6,14 @@ type Message struct {
 
 type BidMessage struct {
 	Message
-	AuctionId string `json:"auction_id"`
+	AuctionID string `json:"auction_id"`
 	Amount    int    `json:"amount"`
-	UserId    string `json:"user_id"`
+	UserID    string `json:"user_id"`
 }
 
 type EndAuctionMessage struct {
 	Message
-	AuctionId string `json:"auction_id"`
+	AuctionID string `json:"auction_id"`
 	Winner    string `json:"winner"`
 	Amount    int    `json:"amount"`
 }
