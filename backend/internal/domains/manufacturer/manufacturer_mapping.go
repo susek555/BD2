0e46ed752b3c677454deb57a19928f3276bff984
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-05-17 23:07:29 +0200

    Mapping to names added.

---
+++ backend/internal/domains/manufacturer/manufacturer_mapping.go
@@ -0,0 +1,5 @@
+package manufacturer
+
+func (m *Manufacturer) MapToName() *string {
+	return &m.Name
+}
--- backend/internal/domains/model/model_mapping.go
+++ backend/internal/domains/model/model_mapping.go
@@ -3,3 +3,7 @@ package model
 func (m *Model) MapToDTO() *RetrieveModelDTO {
 	return &RetrieveModelDTO{ID: m.ID, Name: m.Name}
 }
+
+func (m *Model) MapToName() *string {
+	return &m.Name
+}
