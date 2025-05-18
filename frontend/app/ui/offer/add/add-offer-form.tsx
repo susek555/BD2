565afd0922355acfce4fafc4c4be80faec595233
Michal <01187288@pw.edu.pl>
2025-05-18 18:06:16 +0200

    Displayed information about margin influence

--- frontend/app/ui/offer/add/add-offer-form.tsx
+++ frontend/app/ui/offer/add/add-offer-form.tsx
@@ -363,6 +363,14 @@ export default function AddOfferForm({ inputsData } : { inputsData : AddOfferFor
                     </div>
                     <NumberInputField id="price" name="Price" />
                     <SelectionLabel id="margin" name="Margin ( % )" options={["8", "9", "10"]} required={!detailsPart}/>
+                    <div className="flex items-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2h-1V9z" clipRule="evenodd" />
+                        </svg>
+                        <span className="text-sm text-gray-600">
+                            This percentage will be taxed out of set price. Chosen margin will influence the priority of your offer in the search results. The higher the margin, the higher the priority.
+                        </span>
+                    </div>
 
                     {is_auction && (
                         <>
