807c2e67ca4b70aaec930cc3e515299d8f32224f
Michal <01187288@pw.edu.pl>
2025-05-19 19:00:01 +0200

    Enchanted favourite star description

--- frontend/app/ui/favourite.tsx
+++ frontend/app/ui/favourite.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 
-export default function Favourite({isFavourite, id}: {isFavourite: boolean, id: string}) {
+export default function Favourite({isFavourite, id, describe = true}: {isFavourite: boolean, id: string, describe?: boolean}) {
     const [isFav, setIsFav] = useState(isFavourite);
 
     function handleChangeFavourite(newValue: boolean) {
@@ -24,7 +24,7 @@ export default function Favourite({isFavourite, id}: {isFavourite: boolean, id: 
                     >
                         <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clipRule="evenodd" />
                     </svg>
-                    <span className="ml-2 text-yellow-500">Favourite</span>
+                    {describe && <span className="ml-2 text-yellow-500">Favourite</span>}
                 </>
             ) : (
                 <svg
