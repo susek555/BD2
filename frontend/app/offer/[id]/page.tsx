408751f990485466525fdab2a9bd04c8a5e46678
Michal <01187288@pw.edu.pl>
2025-05-19 17:19:50 +0200

    Added favourite star to offer page

--- frontend/app/offer/[id]/page.tsx
+++ frontend/app/offer/[id]/page.tsx
@@ -4,6 +4,7 @@ import Price from "@/app/ui/offer/[id]/price";
 import OfferDescription from "@/app/ui/offer/[id]/description";
 import OfferDetailsTable from "@/app/ui/offer/[id]/details-table";
 import UserDetails from "@/app/ui/offer/[id]/user-details";
+import Favourite from "@/app/ui/offer/[id]/favourite";
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const { params } = props;
@@ -15,41 +16,37 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
     throw new Error('Offer not found');
   }
 
-  return (
-    <>
-      <div className='my-3' />
-      <div className='flex flex-row gap-10 md:px-20'>
-        <div className='flex w-full flex-col gap-4'>
-          <div className='h-full w-full border-0 border-gray-300'>
-            <div className='static w-full'>
-              <Photos imagesURLs={offer.imagesURLs} />
-            </div>
+    return (
+        <>
+          <div className="my-3" />
+          <div className="flex flex-row gap-10 md:px-20">
+              <div className="flex flex-col gap-4 w-full">
+                  <div className="w-full h-full border-0 border-gray-300">
+                      <div className="w-full static">
+                          <Photos imagesURLs={offer.imagesURLs} />
+                      </div>
+                  </div>
+                  <div className="my-3" />
+                  <OfferDescription description={offer.description} />
+                  <div className="my-3" />
+                  <OfferDetailsTable details={offer.details} />
+                  <div className="my-10" />
+              </div>
+              <div className="md:w-120 h-full md:h-130">
+                  <div className="p-4">
+                      <div className="flex flex-row gap-5">
+                          <h1 className="text-3xl font-bold">{offer.name}</h1>
+                          <Favourite isFavourite={false} />
+                      </div>
+                  </div>
+                  <div className="my-10" />
+                  <Price data={{ id: id, price: offer.price ?? 0, isAuction: offer.isAuction, auction: offer.auctionData,  isActive: offer.isActive }} />
+                  <div className="my-4" />
+                  <UserDetails sellerName={offer.sellerName} />
+                  {/* // TODO - maybe add google maps with location */}
+              </div>
           </div>
-          <div className='my-3' />
-          <OfferDescription description={offer.description} />
-          <div className='my-3' />
-          <OfferDetailsTable details={offer.details} />
-          <div className='my-10' />
-        </div>
-        <div className='h-full md:h-130 md:w-120'>
-          <div className='p-4'>
-            <h1 className='text-3xl font-bold'>{offer.name}</h1>
-          </div>
-          <div className='my-10' />
-          <Price
-            data={{
-              id: id,
-              price: offer.price ?? 0,
-              isAuction: offer.isAuction,
-              auction: offer.auctionData,
-              isActive: offer.isActive,
-            }}
-          />
-          <div className='my-4' />
-          <UserDetails sellerName={offer.sellerName} />
-          {/* // TODO - maybe add google maps with location */}
-        </div>
-      </div>
+
     </>
   );
 }
