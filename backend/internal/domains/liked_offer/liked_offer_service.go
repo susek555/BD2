4dd99f258477046ff3d1a74cd33ec899d3f0875b
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-05-20 19:17:54 +0200

    Created service and handler for LikedOffer entity.

---
+++ backend/internal/domains/liked_offer/liked_offer_service.go
@@ -0,0 +1,22 @@
+package liked_offer
+
+type LikeOfferServiceInterface interface {
+	Create(offer *LikedOffer) error
+	Delete(offerID, userID uint) error
+}
+
+type LikedOfferService struct {
+	repo LikedOfferReposisotryInterface
+}
+
+func NewLikedOfferService(likedOfferRepo LikedOfferReposisotryInterface) LikeOfferServiceInterface {
+	return &LikedOfferService{repo: likedOfferRepo}
+}
+
+func (s *LikedOfferService) Create(offer *LikedOffer) error {
+	return s.repo.Create(offer)
+}
+
+func (s *LikedOfferService) Delete(offerID, userID uint) error {
+	return s.repo.Delete(offerID, userID)
+}
