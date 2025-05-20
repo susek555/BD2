93f2b55dd40eea1f9173540f5d0986c9f906a79a
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-05-21 00:56:21 +0200

    Basic database insertion added to provide dictionary entity.

---
+++ backend/data/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO manufacturers VALUES (1, "Audi");
+INSERT INTO manufacturers VALUES (2, "BMW");
+INSERT INTO manufacturers VALUES (3, "Opel");
+INSERT INTO manufacturers VALUES (4, "Toyota");
+INSERT INTO manufacturers VALUES (5, "Skoda");
+
+INSERT INTO models VALUES (1, "A3", 1);
+INSERT INTO models VALUES (2, "M3", 2);
+INSERT INTO models VALUES (3, "Astra", 3);
+INSERT INTO models VALUES (4, "Supra", 4);
+INSERT INTO models VALUES (5, "Octavia", 5);
