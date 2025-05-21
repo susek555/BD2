f76dd718e788fdce899922621913dc2a822574ce
KamilMarszalek <kamilmar553@gmail.com>
2025-05-21 22:03:02 +0200

    Add Notification model for handling notifications in the system

---
+++ backend/internal/domains/notification/notification_model.go
@@ -0,0 +1,13 @@
+package notification
+
+import "github.com/susek555/BD2/car-dealer-api/internal/domains/sale_offer"
+
+type Notification struct {
+	ID                    uint `json:"id" gorm:"primaryKey"`
+	OfferID               uint `json:"offer_id"`
+	*sale_offer.SaleOffer `json:"sale_offer" gorm:"foreignKey:OfferID;references:ID"`
+	Title                 string `json:"title"`
+	Description           string `json:"description"`
+	Date                  string `json:"date"`
+	Seen                  bool   `json:"seen"`
+}
