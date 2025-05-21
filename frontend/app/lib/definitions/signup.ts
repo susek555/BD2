99905c9414c0dfb39a367df79070d877f736da74
Damian D'Souza <dsouzadamian189@gmail.com>
2025-05-22 00:41:05 +0200

    modify signup error messages

--- frontend/app/lib/definitions/signup.ts
+++ frontend/app/lib/definitions/signup.ts
@@ -10,7 +10,7 @@ export const SignupFormSchema = z
     email: z.string().email({ message: 'Please enter a valid email.' }).trim(),
     password: z
       .string()
-      .min(2, { message: 'Be at least 2 characters long' })
+      .min(2, { message: 'Must be at least 2 characters long' })
       // .regex(/[a-zA-Z]/, { message: 'Contain at least one letter.' })
       // .regex(/[0-9]/, { message: 'Contain at least one number.' })
       // .regex(/[^a-zA-Z0-9]/, {
@@ -37,9 +37,9 @@ export const SignupFormSchema = z
           .min(1, { message: 'Business name is required' }),
         company_nip: z
           .string()
-          .min(10, { message: 'Be 10 characters long' })
-          .max(10, { message: 'Be 10 characters long' })
-          .regex(/^\d+$/, { message: 'Contain only digits' }),
+          .min(10, { message: 'Must be 10 characters long' })
+          .max(10, { message: 'Must be 10 characters long' })
+          .regex(/^\d+$/, { message: 'Can contain only digits' }),
       }),
     ]),
   )
