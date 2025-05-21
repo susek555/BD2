b10cf8ec601e400038d44ca69b2a1021e12bc748
Damian D'Souza <dsouzadamian189@gmail.com>
2025-05-22 00:41:42 +0200

    update login form to use InputField component

--- frontend/app/ui/signup/login-form.tsx
+++ frontend/app/ui/signup/login-form.tsx
@@ -6,8 +6,9 @@ import { signIn } from 'next-auth/react';
 import Link from 'next/link';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useState } from 'react';
-import { LoginError } from '../../lib/definitions/signup';
 import { Button } from '../(common)/button';
+import InputField from '../(common)/input-field';
+import { LoginError } from '../../lib/definitions/signup';
 
 export default function LoginForm({
   onLoginSuccess,
@@ -59,44 +60,28 @@ export default function LoginForm({
       <div className='flex-1 rounded-lg bg-gray-50 px-6 pt-8 pb-4'>
         <h1 className='mb-3 text-2xl'>Log in</h1>
         <div className='w-full'>
-          <div>
-            <label
-              className='mt-5 mb-3 block text-xs font-medium text-gray-900'
-              htmlFor='login'
-            >
-              Login
-            </label>
-            <div className='relative'>
-              <input
-                className='peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500'
-                id='login'
-                type='text'
-                name='login'
-                placeholder='Enter your email address or username'
-                required
-              />
-              <AtSymbolIcon className='pointer-events-none absolute top-1/2 left-3 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900' />
-            </div>
-          </div>
-          <div className='mt-4'>
-            <label
-              className='mt-5 mb-3 block text-xs font-medium text-gray-900'
-              htmlFor='password'
-            >
-              Password
-            </label>
-            <div className='relative'>
-              <input
-                className='peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500'
-                id='password'
-                type='password'
-                name='password'
-                placeholder='Enter password'
-                required
-              />
-              <KeyIcon className='pointer-events-none absolute top-1/2 left-3 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900' />
-            </div>
-          </div>
+          <InputField
+            id='login'
+            name='login'
+            label='Login'
+            placeholder='Enter your email address or username'
+            defaultValue=''
+            icon={AtSymbolIcon}
+            errors={[]}
+            required
+          />
+
+          <InputField
+            id='password'
+            name='password'
+            label='Password'
+            placeholder='Enter password'
+            type='password'
+            defaultValue=''
+            icon={KeyIcon}
+            errors={[]}
+            required
+          />
         </div>
         <Button className='mt-4 w-full' type='submit'>
           Log in <ArrowRightIcon className='ml-auto h-5 w-5 text-gray-50' />
