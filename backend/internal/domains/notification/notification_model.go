caeead19302686abb3fdb17356e49aab7199b2a4
KamilMarszalek <kamilmar553@gmail.com>
2025-05-22 00:20:23 +0200

    Refactor Notification model to make Offer field optional in JSON response

--- backend/internal/domains/notification/notification_model.go
+++ backend/internal/domains/notification/notification_model.go
@@ -5,7 +5,7 @@ import "github.com/susek555/BD2/car-dealer-api/internal/domains/sale_offer"
 type Notification struct {
 	ID          uint                  `json:"id" gorm:"primaryKey"`
 	OfferID     uint                  `json:"offer_id"`
-	Offer       *sale_offer.SaleOffer `json:"sale_offer" gorm:"foreignKey:OfferID;references:ID"`
+	Offer       *sale_offer.SaleOffer `json:"sale_offer,omitempty" gorm:"foreignKey:OfferID;references:ID"`
 	Title       string                `json:"title"`
 	Description string                `json:"description"`
 	Date        string                `json:"date"`
