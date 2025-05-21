9a593d8835a65c5d981ce8eb74c471d39c017f95
KamilMarszalek <kamilmar553@gmail.com>
2025-05-22 00:04:45 +0200

    Refactor InitializeScheduler to integrate notification service and related repositories

--- backend/internal/initializers/init_sched.go
+++ backend/internal/initializers/init_sched.go
@@ -4,6 +4,9 @@ import (
 	"context"
 
 	"github.com/susek555/BD2/car-dealer-api/internal/domains/bid"
+	"github.com/susek555/BD2/car-dealer-api/internal/domains/manufacturer"
+	"github.com/susek555/BD2/car-dealer-api/internal/domains/notification"
+	"github.com/susek555/BD2/car-dealer-api/internal/domains/sale_offer"
 	"github.com/susek555/BD2/car-dealer-api/internal/domains/scheduler"
 )
 
@@ -11,6 +14,12 @@ var Sched scheduler.SchedulerInterface
 
 func InitializeScheduler() {
 	repo := bid.NewBidRepository(DB)
-	Sched = scheduler.NewScheduler(repo, RedisClient)
+	notificationRepo := notification.NewNotificationRepository(DB)
+	saleOfferRepo := sale_offer.NewSaleOfferRepository(DB)
+	manufacturerRepo := manufacturer.NewManufacturerRepository(DB)
+	manufacturerService := manufacturer.NewManufacturerService(manufacturerRepo)
+	saleOfferService := sale_offer.NewSaleOfferService(saleOfferRepo, manufacturerService)
+	notificationService := notification.NewNotificationService(notificationRepo, saleOfferService)
+	Sched = scheduler.NewScheduler(repo, RedisClient, notificationService)
 	go Sched.Run(context.Background())
 }
