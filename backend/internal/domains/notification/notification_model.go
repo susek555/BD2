350884be18dbd9c3a66ff21727a15cc299e956a6
KamilMarszalek <kamilmar553@gmail.com>
2025-05-21 22:17:23 +0200

    Refactor Notification struct for improved readability

--- backend/internal/domains/notification/notification_model.go
+++ backend/internal/domains/notification/notification_model.go
@@ -3,10 +3,10 @@ package notification
 import "github.com/susek555/BD2/car-dealer-api/internal/domains/sale_offer"
 
 type Notification struct {
-	ID                    uint `json:"id" gorm:"primaryKey"`
-	OfferID               uint `json:"offer_id"`
-	*sale_offer.SaleOffer `json:"sale_offer" gorm:"foreignKey:OfferID;references:ID"`
-	Title                 string `json:"title"`
-	Description           string `json:"description"`
-	Date                  string `json:"date"`
+	ID          uint                  `json:"id" gorm:"primaryKey"`
+	OfferID     uint                  `json:"offer_id"`
+	Offer       *sale_offer.SaleOffer `json:"sale_offer" gorm:"foreignKey:OfferID;references:ID"`
+	Title       string                `json:"title"`
+	Description string                `json:"description"`
+	Date        string                `json:"date"`
 }
