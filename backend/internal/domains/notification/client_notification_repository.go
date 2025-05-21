65424db434864792139b9fc63b9113927e84c4da
KamilMarszalek <kamilmar553@gmail.com>
2025-05-21 22:26:02 +0200

    Implement ClientNotificationRepository for managing client notifications

--- backend/internal/domains/notification/client_notification_repository.go
+++ backend/internal/domains/notification/client_notification_repository.go
@@ -1,7 +1,45 @@
 package notification
 
+import "gorm.io/gorm"
+
 type ClientNotificationRepositoryInterface interface {
 	Create(clientNotification *ClientNotification) (*ClientNotification, error)
 	GetById(id uint) (*ClientNotification, error)
 	GetAll() ([]ClientNotification, error)
+}
+
+type ClientNotificationRepository struct {
+	DB *gorm.DB
+}
+
+func NewClientNotificationRepository(db *gorm.DB) ClientNotificationRepositoryInterface {
+	return &ClientNotificationRepository{
+		DB: db,
+	}
+}
+
+func (r *ClientNotificationRepository) Create(clientNotification *ClientNotification) (*ClientNotification, error) {
+	db := r.DB
+	if err := db.Create(clientNotification).Error; err != nil {
+		return nil, err
+	}
+	return clientNotification, nil
+}
+
+func (r *ClientNotificationRepository) GetById(id uint) (*ClientNotification, error) {
+	db := r.DB
+	var clientNotification ClientNotification
+	if err := db.First(&clientNotification, id).Error; err != nil {
+		return nil, err
+	}
+	return &clientNotification, nil
+}
+
+func (r *ClientNotificationRepository) GetAll() ([]ClientNotification, error) {
+	db := r.DB
+	var clientNotifications []ClientNotification
+	if err := db.Find(&clientNotifications).Error; err != nil {
+		return nil, err
+	}
+	return clientNotifications, nil
 }
