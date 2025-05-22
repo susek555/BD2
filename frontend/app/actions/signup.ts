b64ccc6f75f146ab1879f7f2b3dc920edaddf6fe
Damian D'Souza <dsouzadamian189@gmail.com>
2025-05-22 21:53:23 +0200

    formatting

--- frontend/app/actions/signup.ts
+++ frontend/app/actions/signup.ts
@@ -1,38 +1,49 @@
-import { registerResult, registerUser } from "@/app/lib/api/auth";
-import { SignupFormSchema, SignupFormState } from "@/app/lib/definitions/signup";
-import { permanentRedirect } from "next/navigation";
-
+import { AccountFieldValidationResult, registerUser } from '@/app/lib/api/auth';
+import {
+  SignupFormSchema,
+  SignupFormState,
+} from '@/app/lib/definitions/signup';
+import { permanentRedirect } from 'next/navigation';
 
 export async function signup(
   state: SignupFormState,
-  formData: FormData
+  formData: FormData,
 ): Promise<SignupFormState> {
-  console.log("Signup form data:", Object.fromEntries(formData.entries()));
+  console.log('Signup form data:', Object.fromEntries(formData.entries()));
 
   const formDataObj = Object.fromEntries(formData.entries());
   const validatedFields = SignupFormSchema.safeParse(formDataObj);
-  console.log("Signup validation result:", validatedFields);
+  console.log('Signup validation result:', validatedFields);
 
   if (!validatedFields.success) {
-    console.log("Signup validation errors:", validatedFields.error.flatten().fieldErrors);
+    console.log(
+      'Signup validation errors:',
+      validatedFields.error.flatten().fieldErrors,
+    );
     return {
       errors: validatedFields.error.flatten().fieldErrors,
       values: Object.fromEntries(
-        Object.entries(formDataObj).filter(([key]) => !key.includes('password'))
-      ) as SignupFormState['values']
+        Object.entries(formDataObj).filter(
+          ([key]) => !key.includes('password'),
+        ),
+      ) as SignupFormState['values'],
     };
   }
 
-  const signupResult: registerResult = await registerUser(validatedFields.data);
+  const signupResult: AccountFieldValidationResult = await registerUser(
+    validatedFields.data,
+  );
 
   if (signupResult.errors) {
     return {
       errors: signupResult.errors,
       values: Object.fromEntries(
-        Object.entries(formDataObj).filter(([key]) => !key.includes('password'))
-      ) as SignupFormState['values']
-    }
+        Object.entries(formDataObj).filter(
+          ([key]) => !key.includes('password'),
+        ),
+      ) as SignupFormState['values'],
+    };
   } else {
-    permanentRedirect("/")
+    permanentRedirect('/');
   }
 }
