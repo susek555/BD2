2e9edd3fe37fa265ded28b4a4a602ec9bc393083
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-05-22 21:58:37 +0200

    Removed unnecessary foreign key from auction - it will be later fixed after model refactor.

--- backend/internal/domains/bid/bid_model.go
+++ backend/internal/domains/bid/bid_model.go
@@ -1,17 +1,17 @@
 package bid
 
 import (
-	"github.com/susek555/BD2/car-dealer-api/internal/domains/sale_offer"
-	"github.com/susek555/BD2/car-dealer-api/internal/domains/user"
 	"time"
+
+	"github.com/susek555/BD2/car-dealer-api/internal/domains/user"
 )
 
 type Bid struct {
-	ID        uint                `json:"id" gorm:"primary_key"`
-	Amount    uint                `json:"amount" gorm:"not null;index"`
-	CreatedAt time.Time           `json:"created_at" gorm:"not null;index"`
-	AuctionID uint                `json:"auction_id" gorm:"not null;index"`
-	Auction   *sale_offer.Auction `gorm:"foreignKey:AuctionID;references:OfferID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	BidderID  uint                `json:"bidder_id" gorm:"not null;index"`
-	Bidder    *user.User          `gorm:"foreignKey:BidderID;references:ID"`
+	ID        uint      `json:"id" gorm:"primary_key"`
+	Amount    uint      `json:"amount" gorm:"not null;index"`
+	CreatedAt time.Time `json:"created_at" gorm:"not null;index"`
+	AuctionID uint      `json:"auction_id" gorm:"not null;index"`
+	// TODO: fix after refactor models dir `gorm:"foreignKey:AuctionID;references:OfferID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	BidderID uint       `json:"bidder_id" gorm:"not null;index"`
+	Bidder   *user.User `gorm:"foreignKey:BidderID;references:ID"`
 }
