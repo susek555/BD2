157b90766278df04495f6554b57baf5b9c869bb1
Damian D'Souza <dsouzadamian189@gmail.com>
2025-05-22 12:31:07 +0200

    refactor SignupFormSchema to use PersonalDataSchema

--- frontend/app/lib/definitions/signup.ts
+++ frontend/app/lib/definitions/signup.ts
@@ -1,52 +1,29 @@
 import { z } from 'zod';
+import { PersonalDataSchema } from './personal-data-schema';
 
-export const SignupFormSchema = z
-  .object({
-    selector: z.enum(['P', 'C']),
-    username: z
-      .string()
-      .min(2, { message: 'Username must be at least 2 characters long.' })
-      .trim(),
-    email: z.string().email({ message: 'Please enter a valid email.' }).trim(),
-    password: z
-      .string()
-      .min(2, { message: 'Must be at least 2 characters long' })
-      // .regex(/[a-zA-Z]/, { message: 'Contain at least one letter.' })
-      // .regex(/[0-9]/, { message: 'Contain at least one number.' })
-      // .regex(/[^a-zA-Z0-9]/, {
-      //   message: 'Contain at least one special character.',
-      // })
-      .trim(),
-    confirm_password: z.string().trim(),
-  })
-  .refine((data) => data.password === data.confirm_password, {
-    message: "Passwords don't match",
-    path: ['confirm_password'],
-  })
-  .and(
-    z.discriminatedUnion('selector', [
-      z.object({
-        selector: z.literal('P'),
-        person_name: z.string().min(1, { message: 'Name is required' }),
-        person_surname: z.string().min(1, { message: 'Surname is required' }),
-      }),
-      z.object({
-        selector: z.literal('C'),
-        company_name: z
-          .string()
-          .min(1, { message: 'Company name is required' }),
-        company_nip: z
-          .string()
-          .min(10, { message: 'Must be 10 characters long' })
-          .max(10, { message: 'Must be 10 characters long' })
-          .regex(/^\d+$/, { message: 'Can contain only digits' }),
-      }),
-    ]),
-  )
-  .transform((data) => {
-    const { confirm_password: _, ...rest } = data;
-    return rest;
-  });
+export const SignupFormSchema = PersonalDataSchema.and(
+  z
+    .object({
+      password: z
+        .string()
+        .min(2, { message: 'Must be at least 2 characters long' })
+        // .regex(/[a-zA-Z]/, { message: 'Contain at least one letter.' })
+        // .regex(/[0-9]/, { message: 'Contain at least one number.' })
+        // .regex(/[^a-zA-Z0-9]/, {
+        //   message: 'Contain at least one special character.',
+        // })
+        .trim(),
+      confirm_password: z.string().trim(),
+    })
+    .refine((data) => data.password === data.confirm_password, {
+      message: "Passwords don't match",
+      path: ['confirm_password'],
+    })
+    .transform((data) => {
+      const { confirm_password: _, ...rest } = data;
+      return rest;
+    }),
+);
 
 export type SignupForm = z.input<typeof SignupFormSchema>;
 
