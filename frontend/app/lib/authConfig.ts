73644f2383e2e824ad30b49d8d0ed568952baf91
Damian D'Souza <dsouzadamian189@gmail.com>
2025-05-22 21:58:04 +0200

    handle update in jwt callback

--- frontend/app/lib/authConfig.ts
+++ frontend/app/lib/authConfig.ts
@@ -1,8 +1,8 @@
 import { API_URL } from '@/app/lib/constants';
 import { ExtendedJWT } from '@/types/next-auth';
 import camelcaseKeys from 'camelcase-keys';
-import { NextAuthOptions, User } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
+import { NextAuthOptions, User } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 const ACCESS_TOKEN_LIFETIME = 2 * 60 * 60 * 1000; // 2 hours
 // const ACCESS_TOKEN_LIFETIME = 30 * 1000; // 30 seconds
@@ -18,7 +18,9 @@ async function updateAccessToken(refreshToken: string): Promise<string> {
     });
 
     if (!response.ok) {
-      throw new Error(`Refresh token error: ${response.status} ${response.statusText}`);
+      throw new Error(
+        `Refresh token error: ${response.status} ${response.statusText}`,
+      );
     }
 
     const data = await response.json();
@@ -38,18 +40,18 @@ async function updateAccessToken(refreshToken: string): Promise<string> {
 export const authConfig: NextAuthOptions = {
   providers: [
     CredentialsProvider({
-      name: "Credentials",
+      name: 'Credentials',
       credentials: {
-        login: { label: "Login", type: "text" },
-        password: { label: "Password", type: "password" }
+        login: { label: 'Login', type: 'text' },
+        password: { label: 'Password', type: 'password' },
       },
       async authorize(credentials) {
         // Add logic here to look up the user from the credentials supplied
 
         const res = await fetch(`${API_URL}/auth/login`, {
-          method: "POST",
+          method: 'POST',
           headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
           },
           body: JSON.stringify({
             login: credentials?.login,
@@ -57,49 +59,72 @@ export const authConfig: NextAuthOptions = {
           }),
         });
         const rawUser = await res.json();
-        console.log("Login errors:", rawUser.errors);
+        console.log('Login errors:', rawUser.errors);
 
         if (!rawUser.errors) {
           const user: User = camelcaseKeys(rawUser, { deep: true });
           // Any object returned will be saved in `user` property of the JWT
           return user;
         } else {
-          throw new Error(JSON.stringify(rawUser.errors))
+          throw new Error(JSON.stringify(rawUser.errors));
           // throw new Error(rawUser.errors);
           // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
         }
-      }
-    })
+      },
+    }),
   ],
   callbacks: {
-    async jwt({ token, user }) {
-      console.log("JWT callback");
-      const jwtToken = token as ExtendedJWT
+    async jwt({ token, user, trigger, session }) {
+      console.log('JWT callback');
+      const jwtToken = token as ExtendedJWT;
 
-      // If this is the first sign in, add user data to token
+      if (trigger === 'update' && session) {
+        console.log('Update session data', session);
+
+        const isUserProfile =
+          session.selector &&
+          // typeof session.id === 'number' && // uncomment when main backend branch sends id on login
+          typeof session.username === 'string' &&
+          typeof session.email === 'string' &&
+          (session.selector === 'P' || session.selector === 'C');
+        console.log('isUserProfile', isUserProfile);
+
+        const isIdPassword =
+          // typeof session.id === 'number' &&
+          typeof session.password === 'string' &&
+          Object.keys(session).length === 2;
+
+        if (isUserProfile || isIdPassword) {
+          Object.assign(jwtToken, session);
+        }
+      }
+
       if (user) {
-        console.log("JWT callback initial login")
+        console.log('JWT callback initial login');
         Object.assign(jwtToken, user);
         jwtToken.accessTokenExpires = Date.now() + ACCESS_TOKEN_LIFETIME;
       }
 
-      // Return previous token if the access token has not expired yet
-      if (jwtToken.accessTokenExpires && Date.now() < jwtToken.accessTokenExpires) {
-        console.log("JWT callback still valid")
+      if (
+        jwtToken.accessTokenExpires &&
+        Date.now() < jwtToken.accessTokenExpires
+      ) {
+        console.log('JWT callback still valid');
         return jwtToken;
       }
 
-      // Access token has expired, refresh it
       try {
-        console.log("Token expired, refreshing...");
+        console.log('Token expired, refreshing...');
         const newAccessToken = await updateAccessToken(jwtToken.refreshToken);
         jwtToken.accessToken = newAccessToken;
         jwtToken.accessTokenExpires = Date.now() + ACCESS_TOKEN_LIFETIME;
-        console.log(`${Math.round((jwtToken.accessTokenExpires - Date.now()) / (1000 * 60))} minutes`)
+        console.log(
+          `${Math.round((jwtToken.accessTokenExpires - Date.now()) / (1000 * 60))} minutes`,
+        );
         return { ...jwtToken };
       } catch (error) {
-        console.error("Error refreshing token:", error);
-        return { ...token, error: "RefreshTokenError" };
+        console.error('Error refreshing token:', error);
+        return { ...token, error: 'RefreshTokenError' };
       }
     },
 
@@ -112,6 +137,6 @@ export const authConfig: NextAuthOptions = {
     signIn: '/login',
   },
   session: {
-    strategy: "jwt",
-  }
-}
+    strategy: 'jwt',
+  },
+};
