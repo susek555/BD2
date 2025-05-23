a785ef0441721e3d0a51279e7eb14d987f06c74f
Damian D'Souza <dsouzadamian189@gmail.com>
2025-05-23 14:24:52 +0200

    add change password form

---
+++ frontend/app/ui/account/settings/change-password-form.tsx
@@ -0,0 +1,45 @@
+import { KeyIcon } from '@heroicons/react/24/outline';
+import { Button } from '../../(common)/button';
+import InputField from '../../(common)/input-field';
+
+export default function ChangePasswordForm() {
+  return (
+    <form className='space-y-3'>
+      <InputField
+        id='currentPassword'
+        name='currentPassword'
+        label='Current Password'
+        placeholder='Enter your current password'
+        defaultValue={''}
+        icon={KeyIcon}
+        errors={[]}
+        required
+      />
+      <InputField
+        id='newPassword'
+        name='newPassword'
+        label='New Password'
+        placeholder='Enter your new password'
+        defaultValue={''}
+        icon={KeyIcon}
+        errors={[]}
+        required
+      />
+      <InputField
+        id='confirmNewPassword'
+        name='confirmNewPassword'
+        label='Confirm New Password'
+        placeholder='Confirm your new password'
+        defaultValue={''}
+        icon={KeyIcon}
+        errors={[]}
+        required
+      />
+      <div className='flex justify-end'>
+        <Button type='submit' className='mt-2 px-4'>
+          Change Password
+        </Button>
+      </div>
+    </form>
+  );
+}
