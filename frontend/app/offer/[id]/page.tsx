8f20837b0e6e74b5cb13b3942a5a4cf9e8a81cc7
Damian D'Souza <dsouzadamian189@gmail.com>
2025-05-23 17:26:23 +0200

    add average rating card to seller details

--- frontend/app/offer/[id]/page.tsx
+++ frontend/app/offer/[id]/page.tsx
@@ -1,13 +1,14 @@
-import { fetchOfferDetails } from "@/app/lib/data/offer/data";
-import Photos from "@/app/ui/offer/[id]/photos";
-import Price from "@/app/ui/offer/[id]/price";
-import OfferDescription from "@/app/ui/offer/[id]/description";
-import OfferDetailsTable from "@/app/ui/offer/[id]/details-table";
-import UserDetails from "@/app/ui/offer/[id]/user-details";
-import Favourite from "@/app/ui/offer/[id]/favourite";
-import { authConfig } from "@/app/lib/authConfig";
-import { getServerSession } from "next-auth/next";
-import OwnerView from "@/app/ui/offer/[id]/owner-view";
+import { authConfig } from '@/app/lib/authConfig';
+import { fetchOfferDetails } from '@/app/lib/data/offer/data';
+import { fetchAverageRating } from '@/app/lib/data/reviews/data';
+import OfferDescription from '@/app/ui/offer/[id]/description';
+import OfferDetailsTable from '@/app/ui/offer/[id]/details-table';
+import Favourite from '@/app/ui/offer/[id]/favourite';
+import OwnerView from '@/app/ui/offer/[id]/owner-view';
+import Photos from '@/app/ui/offer/[id]/photos';
+import Price from '@/app/ui/offer/[id]/price';
+import UserDetails from '@/app/ui/offer/[id]/user-details';
+import { getServerSession } from 'next-auth/next';
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const { params } = props;
@@ -19,68 +20,80 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
     throw new Error('Offer not found');
   }
 
-    const session = await getServerSession(authConfig);
-    const username = session?.user?.username;
+  const sellerAverageRating = await fetchAverageRating(offer.sellerId);
 
-    console.log("username", username);
-    console.log("sellerName", offer.sellerName);
-    console.log("condition",  username === offer.sellerName);
+  const session = await getServerSession(authConfig);
+  const username = session?.user?.username;
 
-    return (
-        <>
-            <div className="my-3" />
-            <div className="flex flex-row gap-10 md:px-20">
-                <div className="flex flex-col gap-4 w-full">
-                    <div className="w-full h-full border-0 border-gray-300">
-                        <div className="w-full static">
-                            <Photos imagesURLs={offer.imagesURLs} />
-                        </div>
-                    </div>
-                    <div className="my-3" />
-                    <OfferDescription description={offer.description} />
-                    <div className="my-3" />
-                    <OfferDetailsTable details={offer.details} />
-                    <div className="my-10" />
-                </div>
-                <div className="md:w-120 h-full md:h-130">
-                    <div className="p-4">
-                        <div className="flex flex-row gap-5">
-                            <h1 className="text-3xl font-bold">{offer.name}</h1>
-                            {username !== offer.sellerName && (
-                                <Favourite isFavourite={offer.is_favourite} id={id}/>
-                            )}
-                        </div>
-                    </div>
-                    <div className="my-10" />
-                        {offer.can_edit || offer.can_delete || username === offer.sellerName ? (
-                            <>
-                                <OwnerView
-                                    can_edit={offer.can_edit}
-                                    can_delete={offer.can_delete}
-                                    offer_id={id}
-                                    isAuction={offer.isAuction}
-                                />
-                                <div className="my-5" />
-                            </>
-                        ) : (
-                            <></>
-                        ) }
-                        <>
-                            <Price data={{
-                                id: id,
-                                price: offer.price ?? 0,
-                                isAuction: offer.isAuction,
-                                auction: offer.auctionData,
-                                isActive: offer.isActive,
-                                priceOnly: offer.can_edit || offer.can_delete || username === offer.sellerName,
-                            }} />
-                        </>
+  console.log('username', username);
+  console.log('sellerName', offer.sellerName);
+  console.log('condition', username === offer.sellerName);
 
-                    <div className="my-4" />
-                    <UserDetails sellerName={offer.sellerName} />
-                    {/* // TODO - maybe add google maps with location */}
-                </div>
+  return (
+    <>
+      <div className='my-3' />
+      <div className='flex flex-row gap-10 md:px-20'>
+        <div className='flex w-full flex-col gap-4'>
+          <div className='h-full w-full border-0 border-gray-300'>
+            <div className='static w-full'>
+              <Photos imagesURLs={offer.imagesURLs} />
             </div>
-        </>
-    );
+          </div>
+          <div className='my-3' />
+          <OfferDescription description={offer.description} />
+          <div className='my-3' />
+          <OfferDetailsTable details={offer.details} />
+          <div className='my-10' />
+        </div>
+        <div className='h-full md:h-130 md:w-120'>
+          <div className='p-4'>
+            <div className='flex flex-row gap-5'>
+              <h1 className='text-3xl font-bold'>{offer.name}</h1>
+              {username !== offer.sellerName && (
+                <Favourite isFavourite={offer.is_favourite} id={id} />
+              )}
+            </div>
+          </div>
+          <div className='my-10' />
+          {offer.can_edit ||
+          offer.can_delete ||
+          username === offer.sellerName ? (
+            <>
+              <OwnerView
+                can_edit={offer.can_edit}
+                can_delete={offer.can_delete}
+                offer_id={id}
+                isAuction={offer.isAuction}
+              />
+              <div className='my-5' />
+            </>
+          ) : (
+            <></>
+          )}
+          <>
+            <Price
+              data={{
+                id: id,
+                price: offer.price ?? 0,
+                isAuction: offer.isAuction,
+                auction: offer.auctionData,
+                isActive: offer.isActive,
+                priceOnly:
+                  offer.can_edit ||
+                  offer.can_delete ||
+                  username === offer.sellerName,
+              }}
+            />
+          </>
+
+          <div className='my-4' />
+          <UserDetails
+            sellerName={offer.sellerName}
+            sellerAverageRating={sellerAverageRating}
+          />
+          {/* // TODO - maybe add google maps with location */}
+        </div>
+      </div>
+    </>
+  );
 }
--- frontend/app/ui/offer/[id]/user-details.tsx
+++ frontend/app/ui/offer/[id]/user-details.tsx
@@ -1,23 +1,31 @@
-import { UserCircleIcon } from "@heroicons/react/20/solid";
+import { UserCircleIcon } from '@heroicons/react/20/solid';
+import { AverageRatingCard } from '../../review/average-rating-card';
 
-export default function UserDetails({ sellerName }: { sellerName: string }) {
-    return (
-        <div className="flex flex-col gap-4 w-full md:w-120 h-full md:h-40 border-gray-300 border">
-            <div className="flex justify-center items-center flex-col h-full gap-2">
-                <p className="text-2xl">Seller</p>
-                <a
-                    href={`/account/${sellerName}`}
-                    // TODO - add link to user profile
-                    className="flex flex-row justify-center md:w-100 items-center bg-gray-100 p-4 rounded-md hover:bg-gray-200 transition"
-                >
-                    <div className="flex flex-row gap-2 items-center">
-                        <div className="w-8 h-8 rounded-full bg-blue-500 flex justify-center items-center">
-                            <UserCircleIcon className="w-8 h-8 text-white" />
-                        </div>
-                        <p className="font-bold text-2xl">{sellerName}</p>
-                    </div>
-                </a>
+export default function UserDetails({
+  sellerName,
+  sellerAverageRating,
+}: {
+  sellerName: string;
+  sellerAverageRating: number;
+}) {
+  return (
+    <div className='flex h-full w-full flex-col gap-4 border border-gray-300 md:h-40 md:w-120'>
+      <div className='flex h-full flex-col items-center justify-center gap-2'>
+        <p className='text-2xl'>Seller</p>
+        <a
+          href={`/account/${sellerName}`}
+          // TODO - add link to user profile
+          className='flex flex-row items-center justify-center rounded-md bg-gray-100 p-4 transition hover:bg-gray-200 md:w-100'
+        >
+          <div className='flex flex-row items-center gap-2'>
+            <div className='flex h-8 w-8 items-center justify-center rounded-full bg-blue-500'>
+              <UserCircleIcon className='h-8 w-8 text-white' />
             </div>
-        </div>
-    );
+            <p className='text-2xl font-bold'>{sellerName}</p>
+          </div>
+          <AverageRatingCard rating={sellerAverageRating} />
+        </a>
+      </div>
+    </div>
+  );
 }
