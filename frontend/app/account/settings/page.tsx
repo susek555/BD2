d9125f098199be61c0e29417ba154078ebb702f4
Damian D'Souza <dsouzadamian189@gmail.com>
2025-05-23 14:25:03 +0200

    put change password form on settings page

--- frontend/app/account/settings/page.tsx
+++ frontend/app/account/settings/page.tsx
@@ -1,14 +1,21 @@
 import { cachedSessionData } from '@/app/lib/data/account/data';
+import ChangePasswordForm from '@/app/ui/account/settings/change-password-form';
 import ChangePersonalDataForm from '@/app/ui/account/settings/change-personal-data-form';
 
 export default async function SettingsPage() {
   const userProfile = await cachedSessionData();
   return (
     <div className='p-6'>
-      <h2 className='mb-4 text-xl font-semibold text-gray-900'>
-        Account Settings
-      </h2>
-      <ChangePersonalDataForm userProfile={userProfile} />
+      <div className='grid grid-cols-1 gap-6 md:grid-cols-2'>
+        <div className='pl-20 pr-2'>
+          <h2 className='mb-4 text-xl font-semibold'>Edit Profile</h2>
+          <ChangePersonalDataForm userProfile={userProfile} />
+        </div>
+        <div className='pl-2 pr-20'>
+          <h2 className='mb-4 text-xl font-semibold'>Change Password</h2>
+          <ChangePasswordForm />
+        </div>
+      </div>
     </div>
   );
 }
