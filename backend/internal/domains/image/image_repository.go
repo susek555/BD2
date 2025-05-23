7bff8864d9f6a3ecf823651341e42a04fc46e0c9
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-05-23 17:54:12 +0200

    Added basic image repository.

---
+++ backend/internal/domains/image/image_repository.go
@@ -0,0 +1,29 @@
+package image
+
+import (
+	"github.com/susek555/BD2/car-dealer-api/internal/domains/models"
+	"gorm.io/gorm"
+)
+
+type ImageRepositoryInterface interface {
+	Create(image *models.Image) error
+	GetImagesByOfferID(offerID uint) ([]models.Image, error)
+}
+
+type ImageRepository struct {
+	DB *gorm.DB
+}
+
+func NewImageRepository(db *gorm.DB) ImageRepositoryInterface {
+	return &ImageRepository{DB: db}
+}
+
+func (r *ImageRepository) Create(image *models.Image) error {
+	return r.DB.Create(&image).Error
+}
+
+func (r *ImageRepository) GetImagesByOfferID(offerID uint) ([]models.Image, error) {
+	var images []models.Image
+	err := r.DB.Where("offer_id = ?", offerID).Find(&images).Error
+	return images, err
+}
