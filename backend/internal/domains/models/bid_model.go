837165bddd74e0755bf57d2842dff041a65cefb2
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-05-23 16:31:58 +0200

    Foreign keys constraints fixed.

--- backend/internal/domains/models/bid_model.go
+++ backend/internal/domains/models/bid_model.go
@@ -9,8 +9,7 @@ type Bid struct {
 	Amount    uint      `json:"amount" gorm:"not null;index"`
 	CreatedAt time.Time `json:"created_at" gorm:"not null;index"`
 	AuctionID uint      `json:"auction_id" gorm:"not null;index"`
-	// TODO: fix after refactor models dir `gorm:"foreignKey:AuctionID;references:OfferID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	BidderID uint     `json:"bidder_id" gorm:"not null;index"`
-	Bidder   *User    `gorm:"foreignKey:BidderID;references:ID"`
-	Auction  *Auction `gorm:"foreignKey:AuctionID;references:OfferID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	BidderID  uint      `json:"bidder_id" gorm:"not null;index"`
+	Bidder    *User     `gorm:"foreignKey:BidderID;references:ID"`
+	Auction   *Auction  `gorm:"foreignKey:AuctionID;references:OfferID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
