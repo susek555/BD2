46af0d409631716fef6b7b63a7caa65e4a6ece6c
Damian D'Souza <dsouzadamian189@gmail.com>
2025-05-23 16:06:54 +0200

    remove unnecessary code from user update endpoints

--- frontend/app/api/account/update/profile/route.ts
+++ frontend/app/api/account/update/profile/route.ts
@@ -1,16 +1,5 @@
-import { UserProfile } from '@/app/lib/definitions/user';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function PUT(request: NextRequest) {
-  try {
-    const userData: UserProfile = await request.json();
-
-    return NextResponse.json({ status: 200 });
-  } catch (error) {
-    console.error('Error updating profile:', error);
-    return NextResponse.json(
-      { errors: { other: ['Failed to update profile'] } },
-      { status: 500 },
-    );
-  }
+  return NextResponse.json({ status: 200 });
 }
