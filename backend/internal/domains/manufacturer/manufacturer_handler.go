6b80045550f7dd350c1c734711daebedd86f6ebd
KamilMarszalek <kamilmar553@gmail.com>
2025-05-23 22:06:22 +0200

    Fix success response documentation to reference models.Manufacturer in GetAllManufactures

--- backend/internal/domains/manufacturer/manufacturer_handler.go
+++ backend/internal/domains/manufacturer/manufacturer_handler.go
@@ -21,7 +21,7 @@ func NewHandler(s ManufacturerServiceInterface) *Handler {
 //	@Tags			car
 //	@Accept			json
 //	@Produce		json
-//	@Success		200	{array}		Manufacturer			"List of manufacturers"
+//	@Success		200	{array}		models.Manufacturer			"List of manufacturers"
 //	@Failure		500	{object}	custom_errors.HTTPError	"Internal server error"
 //	@Router			/car/manufacturers [get]
 func (h *Handler) GetAllManufactures(c *gin.Context) {
