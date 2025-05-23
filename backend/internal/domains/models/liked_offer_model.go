029c972cbf13dd7b0662e18a4956f45c04120e5d
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-05-23 16:31:58 +0200

    Foreign keys constraints fixed.

--- backend/internal/domains/models/liked_offer_model.go
+++ backend/internal/domains/models/liked_offer_model.go
@@ -1,6 +1,8 @@
 package models
 
 type LikedOffer struct {
-	UserID  uint `json:"user_id" gorm:"primaryKey;constraint:OnDelete:CASCADE,OnUpdate:CASCADE"`
-	OfferID uint `json:"offer_id" gorm:"primaryKey;constraint:OnDelete:CASCADE,OnUpdate:CASCADE"`
+	UserID    uint       `json:"user_id"`
+	OfferID   uint       `json:"offer_id"`
+	SaleOffer *SaleOffer `gorm:"foreignKey:OfferID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	User      *User      `gorm:"foreignKey:UserID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
