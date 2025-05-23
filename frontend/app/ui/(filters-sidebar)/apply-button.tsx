8b0e6b74337558c0a3bcffcbb5ca6fe4edeedb5f
Michal <01187288@pw.edu.pl>
2025-05-23 17:05:22 +0200

    Corrected apply button text

--- frontend/app/ui/(filters-sidebar)/apply-button.tsx
+++ frontend/app/ui/(filters-sidebar)/apply-button.tsx
@@ -17,7 +17,7 @@ export default function ApplyButton() {
             className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
             onClick={() => handleApply()}
         >
-            Mark all as read
+            Apply
         </button>
     )
 }
