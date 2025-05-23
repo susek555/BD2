c239962f12f01812c53191ff708d3f140b481844
Michal <01187288@pw.edu.pl>
2025-05-23 12:49:41 +0200

    Displayed notifications table

--- frontend/app/account/notifications/page.tsx
+++ frontend/app/account/notifications/page.tsx
@@ -1,9 +1,33 @@
+import { fetchNotifications } from "@/app/lib/data/notifications/data";
+import { NotificationSearchParams } from "@/app/lib/definitions/notification";
+import NotificationsTable from "@/app/ui/(notifications-table)/notifications-table";
 import Pagination from "@/app/ui/(offers-table)/pagination";
 import NotificationsFilterBox from "@/app/ui/account/notifications/notifications-filter-box";
 import { Suspense } from "react";
 
-export default function Page() {
+export default async function Page({
+    searchParams,
+}: {
+    searchParams?: Promise<{
+        orderKey?: 'newest' | 'unread';
+        page?: string;
+    }>
+}) {
+  const params = (await searchParams) || {};
+  const orderKey = params.orderKey || 'unread';
+  const page = Number(params.page || 1);
+
+  const notificationsSearchParams: NotificationSearchParams = {
+    order_key: orderKey,
+    pagination: {
+      page: page,
+      page_size: 8,
+    },
+  };
+
   const totalPages = 10; // TODO get page count from API
+  const totalNotifications = 100; // TODO get total notifications from API
+  const { newNotifications, notifications } = await fetchNotifications();
 
   return (
       <div className='flex flex-grow flex-row'>
@@ -13,13 +37,7 @@ export default function Page() {
 
         <div className='flex flex-1 flex-col'>
           <div className='flex-1, p-6 px-12'>
-            {/* <Suspense fallback={<ReviewGridSkeleton />}> */}
-              {/* <ReviewGrid
-                variant={variant}
-                userId={userId}
-                searchParams={reviewSearchParams}
-              /> */}
-            {/* </Suspense> */}
+            <NotificationsTable notifications={notifications} />
           </div>
 
           <div className='mt-5 flex w-full justify-center pb-4'>
--- frontend/app/lib/definitions/notification.ts
+++ frontend/app/lib/definitions/notification.ts
@@ -6,3 +6,11 @@ export interface Notification {
     date: string;
     is_read: boolean;
 }
+
+export interface NotificationSearchParams {
+    order_key?: string;
+    pagination?: {
+        page: number;
+        page_size: number;
+    };
+}
