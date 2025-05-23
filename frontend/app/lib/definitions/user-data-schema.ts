55f5dc7ac94e2cb93583f9510169b178d59d27b6
Damian D'Souza <dsouzadamian189@gmail.com>
2025-05-23 15:04:01 +0200

    rename schema file, add password form schema

--- frontend/app/lib/definitions/user-data-schema.ts
+++ frontend/app/lib/definitions/user-data-schema.ts
@@ -29,3 +29,26 @@ export const PersonalDataSchema = z
       }),
     ]),
   );
+
+export const ChangePasswordSchema = z
+  .object({
+    currentPassword: z.string(),
+    newPassword: z
+      .string()
+      .min(2, { message: 'Must be at least 2 characters long' })
+      // .regex(/[a-zA-Z]/, { message: 'Contain at least one letter.' })
+      // .regex(/[0-9]/, { message: 'Contain at least one number.' })
+      // .regex(/[^a-zA-Z0-9]/, {
+      //   message: 'Contain at least one special character.',
+      // })
+      .trim(),
+    confirmNewPassword: z.string().trim(),
+  })
+  .refine((data) => data.newPassword === data.confirmNewPassword, {
+    message: "Passwords don't match",
+    path: ['confirmNewPassword'],
+  })
+  .transform((data) => {
+    const { confirmNewPassword: _, ...rest } = data;
+    return rest;
+  });
