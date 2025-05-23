d7b983a86de8105db84bb01f0ad63f0b6f5ae482
Damian D'Souza <dsouzadamian189@gmail.com>
2025-05-23 16:06:54 +0200

    remove unnecessary code from user update endpoints

--- frontend/app/api/account/update/password/route.ts
+++ frontend/app/api/account/update/password/route.ts
@@ -1,16 +1,5 @@
-import { PasswordData } from '@/app/lib/definitions/user';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function PUT(request: NextRequest) {
-  try {
-    const userData: PasswordData = await request.json();
-
-    return NextResponse.json({ status: 200 });
-  } catch (error) {
-    console.error('Error changing password:', error);
-    return NextResponse.json(
-      { errors: { other: ['Failed to change password'] } },
-      { status: 500 },
-    );
-  }
+  return NextResponse.json({ status: 200 });
 }
