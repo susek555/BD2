d78f9d2944e93f8dbb48e5f6c68c347ffe5f5bc5
Piotr Szkoda <piotr.szkoda.mail@gmail.com>
2025-05-24 23:25:33 +0200

    Fixed the timezone errors in postgres

--- backend/internal/test/user_tests/user_helper.go
+++ backend/internal/test/user_tests/user_helper.go
@@ -20,7 +20,7 @@ import (
 // -----
 
 func setupDB() (*gorm.DB, error) {
-	dsn := "host=localhost user=bd2_user password=bd2_password dbname=bd2_test port=5432 sslmode=disable TimeZone=Europe/Warsaw"
+	dsn := "host=localhost user=bd2_user password=bd2_password dbname=bd2_test port=5432 sslmode=disable TimeZone=UTC"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
