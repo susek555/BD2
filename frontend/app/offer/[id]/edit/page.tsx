980765304fe116410e99c10e6cc4a9e9732a9a09
Michal <01187288@pw.edu.pl>
2025-05-26 19:53:58 +0200

    displayed temporary initial values for edit offer form

--- frontend/app/offer/[id]/edit/page.tsx
+++ frontend/app/offer/[id]/edit/page.tsx
@@ -1,4 +1,5 @@
 import { fetchOfferFormData } from "@/app/lib/data/filters-sidebar/data";
+import { OfferFormState } from "@/app/lib/definitions/offer-form";
 import { OfferForm, offerActionEnum } from "@/app/ui/offer/add-offer-form";
 import { Suspense } from "react";
 
@@ -9,12 +10,42 @@ export default async function Page(props: { params: Promise<{id: string }> }) {
 
     const formData = await fetchOfferFormData();
 
+    //TODO fetch initial data from the API - probably need a wrapper
+
+    const initialData: Partial<OfferFormState['values']> = {
+        producer: "Audi",
+        model: "Audi A3",
+        color: "Black",
+        fuel_type: "Petrol",
+        transmission: "Manual",
+        drive: "FWD",
+        production_year: 2020,
+        mileage: 15000,
+        number_of_doors: 4,
+        number_of_seats: 5,
+        number_of_gears: 6,
+        engine_power: 150,
+        engine_capacity: 2000,
+        registration_date: "2020-01-01",
+        registration_number: "ABC123",
+        vin: "WVWZZZ1JZ9W123456",
+        description: "This is a test offer description.",
+        price: 20000,
+        is_auction: false,
+        margin: 8,
+    }
+
     return (
         <div className="px-20 py-10">
             <h1 className="text-3xl font-bold pl-10">Edit Offer</h1>
             <div className="m-5"/>
             <Suspense fallback={<div>Loading...</div>}>
-                <OfferForm inputsData = {formData} apiAction={offerActionEnum.EDIT_OFFER} id={id}/>
+                <OfferForm
+                    inputsData = {formData}
+                    initialValues={initialData}
+                    apiAction={offerActionEnum.EDIT_OFFER}
+                    id={id}
+                />
             </Suspense>
         </div>
     );
