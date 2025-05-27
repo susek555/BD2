a49cf3b6651ea8e69e383cad42f2b02ff8ba43ec
KamilMarszalek <kamilmar553@gmail.com>
2025-05-27 20:33:13 +0200

    Refactor bid DTOs to enhance ProcessingBidDTO structure and remove unused Auction field from RetrieveBidDTO

--- backend/internal/domains/bid/bid_dto.go
+++ backend/internal/domains/bid/bid_dto.go
@@ -9,9 +9,15 @@ type CreateBidDTO struct {
 	Amount    uint `json:"amount" binding:"required"`
 }
 
+type ProcessingBidDTO struct {
+	AuctionID uint            `json:"auction_id" binding:"required"`
+	BidderID  uint            `json:"bidder_id" binding:"required"`
+	Amount    uint            `json:"amount" binding:"required"`
+	Auction   *models.Auction `json:"auction,omitempty"`
+}
+
 type RetrieveBidDTO struct {
 	AuctionID uint `json:"auction_id" binding:"required"`
 	BidderID  uint `json:"bidder_id" binding:"required"`
 	Amount    uint `json:"amount" binding:"required"`
-	Auction   *models.Auction
 }
