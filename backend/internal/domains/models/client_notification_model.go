1ea664d574789702cc480cd38709528baf571940
KamilMarszalek <kamilmar553@gmail.com>
2025-05-27 20:43:40 +0200

    Fix formatting in ClientNotification struct for consistency

--- backend/internal/domains/models/client_notification_model.go
+++ backend/internal/domains/models/client_notification_model.go
@@ -1,10 +1,10 @@
 package models
 
 type ClientNotification struct {
-	ID             uint `json:"id" gorm:"primaryKey"`
-	NotificationID uint `json:"notification_id"`
-	UserID         uint `json:"user_id"`
-	Seen           bool `json:"seen"`
-	*Notification  `json:"notification" gorm:"foreignKey:NotificationID;references:ID"`
-	*User          `json:"user" gorm:"foreignKey:UserID;references:ID"`
+	ID             uint          `json:"id" gorm:"primaryKey"`
+	NotificationID uint          `json:"notification_id"`
+	UserID         uint          `json:"user_id"`
+	Seen           bool          `json:"seen"`
+	Notification   *Notification `json:"notification" gorm:"foreignKey:NotificationID;references:ID"`
+	User           *User         `json:"user" gorm:"foreignKey:UserID;references:ID"`
 }
