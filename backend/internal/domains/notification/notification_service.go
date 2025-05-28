76a42267e64767a6d91c77792e5d240778bdf89c
KamilMarszalek <kamilmar553@gmail.com>
2025-05-28 19:42:27 +0200

    Refactor notification messages to remove unnecessary newline characters

--- backend/internal/domains/notification/notification_service.go
+++ backend/internal/domains/notification/notification_service.go
@@ -25,15 +25,15 @@ func NewNotificationService(notificationRepository NotificationRepositoryInterfa
 
 func (s *NotificationService) CreateOutbidNotification(notification *models.Notification, amount int64, offer *models.Auction) error {
 	notification.CreatedAt = time.Now().UTC()
-	notification.Title = fmt.Sprintf("Someone outbid you on %s %s \n", offer.Offer.Car.Model.Manufacturer.Name, offer.Offer.Car.Model.Name)
-	notification.Description = fmt.Sprintf("You were outbid on your offer for %s %s. \n New price: %d \n", offer.Offer.Car.Model.Manufacturer.Name, offer.Offer.Car.Model.Name, amount)
+	notification.Title = fmt.Sprintf("Someone outbid you on %s %s", offer.Offer.Car.Model.Manufacturer.Name, offer.Offer.Car.Model.Name)
+	notification.Description = fmt.Sprintf("New price: %d", amount)
 	return s.NotificationRepository.Create(notification)
 }
 
 func (s *NotificationService) CreateEndAuctionNotification(notification *models.Notification, winner string, winningBid int64, offer *models.SaleOffer) error {
 	notification.CreatedAt = time.Now().UTC()
-	notification.Title = fmt.Sprintf("Auction ended for %s %s \n", offer.Car.Model.Manufacturer.Name, offer.Car.Model.Name)
-	notification.Description = fmt.Sprintf("The auction for %s %s has ended. \n Winner: %s \n Winning bid: %d", offer.Car.Model.Manufacturer.Name, offer.Car.Model.Name, winner, winningBid)
+	notification.Title = fmt.Sprintf("Auction ended for %s %s", offer.Car.Model.Manufacturer.Name, offer.Car.Model.Name)
+	notification.Description = fmt.Sprintf("The auction for %s %s has ended. Winner: %s \n Winning bid: %d", offer.Car.Model.Manufacturer.Name, offer.Car.Model.Name, winner, winningBid)
 	return s.NotificationRepository.Create(notification)
 }
 
