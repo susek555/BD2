08dfa6ce334681d022549955ba5ee16e605ac513
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-05-28 15:24:48 +0200

    Added validation - sale-offer can have only 10 images.

--- backend/internal/domains/image/image_errors.go
+++ backend/internal/domains/image/image_errors.go
@@ -9,6 +9,7 @@ import (
 
 var (
 	ErrInvalidOfferID = errors.New("offer does not belong to logged-in user")
+	ErrTooManyImages  = errors.New("you can only attach 10 photos to one offer")
 )
 
 var ErrorMap = map[error]int{
