37ebf749c40c30668a386a8d74f8a41fb43ba0f6
KamilMarszalek <kamilmar553@gmail.com>
2025-05-28 19:37:57 +0200

    Add MapNotificationToDTO function for notification data mapping

--- backend/internal/domains/notification/notification_mapping.go
+++ backend/internal/domains/notification/notification_mapping.go
@@ -1,6 +1,9 @@
 package notification
 
-import "github.com/susek555/BD2/car-dealer-api/internal/domains/models"
+import (
+	"github.com/susek555/BD2/car-dealer-api/internal/domains/models"
+	"github.com/susek555/BD2/car-dealer-api/pkg/formats"
+)
 
 func MapToClientNotification(not *models.Notification, userID uint) *models.ClientNotification {
 	return &models.ClientNotification{
@@ -15,6 +18,20 @@ func MapToNotification(clientNotification *models.ClientNotification) *models.No
 		return nil
 	}
 	return clientNotification.Notification
+}
+
+func MapNotificationToDTO(notification *models.Notification) *RetrieveNotificationDTO {
+	if notification == nil {
+		return nil
+	}
+	createdAt := notification.CreatedAt.Format(formats.DateTimeLayout)
+	return &RetrieveNotificationDTO{
+		ID:          notification.ID,
+		Title:       notification.Title,
+		Description: notification.Description,
+		CreatedAt:   createdAt,
+		OfferID:     notification.OfferID,
+	}
 }
 
 func MapToNotifications(clientNotifications []models.ClientNotification) []models.Notification {
