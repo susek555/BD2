bfdabb7023f86d673f7106cf638f0d9f15adae22
KamilMarszalek <kamilmar553@gmail.com>
2025-05-28 14:08:58 +0200

    Add missing semicolon

--- database/schema.sql
+++ database/schema.sql
@@ -168,7 +168,7 @@ CREATE TABLE images (
     id SERIAL PRIMARY KEY,
     offer_id INTEGER REFERENCES sale_offers(id),
     url VARCHAR(200) NOT NULL
-)
+);
 
 INSERT INTO manufacturers (name) VALUES
 ('Toyota'),
