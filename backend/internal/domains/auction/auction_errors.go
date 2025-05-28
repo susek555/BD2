dc8ba97232758af5cb7d1e83effa0b3617db08e4
KamilMarszalek <kamilmar553@gmail.com>
2025-05-28 16:32:16 +0200

    Add ErrModificationForbidden error for auction offer validation

--- backend/internal/domains/auction/auction_errors.go
+++ backend/internal/domains/auction/auction_errors.go
@@ -4,3 +4,4 @@ import "errors"
 
 var ErrBuyNowPriceLessThan1 = errors.New("buy now price must be greater than 0")
 var ErrBuyNowPriceLessThanOfferPrice = errors.New("buy now price must be greater than offer price")
+var ErrModificationForbidden = errors.New("provided offer does not belong to logged in user")
