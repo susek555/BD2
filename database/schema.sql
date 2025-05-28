0ccedf37efa5bd2a0990219d04c534fe3e0ed92a
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-05-28 20:55:22 +0200

    Made url and public_id unique.

--- database/schema.sql
+++ database/schema.sql
@@ -161,8 +161,8 @@ CREATE TABLE reviews (
 CREATE TABLE images (
     id SERIAL PRIMARY KEY,
     offer_id INTEGER REFERENCES sale_offers(id),
-    url VARCHAR(200) NOT NULL,
-    public_id VARCHAR(200) NOT NULL
+    url VARCHAR(200) NOT NULL UNIQUE,
+    public_id VARCHAR(200) NOT NULL UNIQUE
 );
 
 INSERT INTO manufacturers (name) VALUES
