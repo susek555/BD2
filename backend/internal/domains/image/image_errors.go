ef68cef90a29ac076c7dfad93aeea75dd30e3f28
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-05-28 21:38:50 +0200

    Renamed errors.

--- backend/internal/domains/image/image_errors.go
+++ backend/internal/domains/image/image_errors.go
@@ -8,11 +8,11 @@ import (
 )
 
 var (
-	ErrInvalidOfferID = errors.New("offer does not belong to logged-in user")
-	ErrTooManyImages  = errors.New("you can only attach 10 photos to one offer")
+	ErrOfferNotOwned = errors.New("offer does not belong to logged-in user")
+	ErrTooManyImages = errors.New("you can only attach 10 photos to one offer")
 )
 
 var ErrorMap = map[error]int{
-	ErrInvalidOfferID:      http.StatusForbidden,
+	ErrOfferNotOwned:       http.StatusForbidden,
 	gorm.ErrRecordNotFound: http.StatusNotFound,
 }
