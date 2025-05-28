c641cd48b6ad749ab6ffdc78048789c70f2030c1
KamilMarszalek <kamilmar553@gmail.com>
2025-05-28 19:49:23 +0200

    Refactor notification messages in CreateOutbidNotification and CreateEndAuctionNotification to use templates for title and description

--- backend/internal/domains/notification/notification_service.go
+++ backend/internal/domains/notification/notification_service.go
@@ -25,15 +25,15 @@ func NewNotificationService(notificationRepository NotificationRepositoryInterfa
 
 func (s *NotificationService) CreateOutbidNotification(notification *models.Notification, amount int64, offer *models.Auction) error {
 	notification.CreatedAt = time.Now().UTC()
-	notification.Title = fmt.Sprintf("Someone outbid you on %s %s", offer.Offer.Car.Model.Manufacturer.Name, offer.Offer.Car.Model.Name)
-	notification.Description = fmt.Sprintf("New price: %d", amount)
+	notification.Title = fmt.Sprintf(OutbidTitleTemplate, offer.Offer.Car.Model.Manufacturer.Name, offer.Offer.Car.Model.Name)
+	notification.Description = fmt.Sprintf(OutbidDescriptionTemplate, amount)
 	return s.NotificationRepository.Create(notification)
 }
 
 func (s *NotificationService) CreateEndAuctionNotification(notification *models.Notification, winner string, winningBid int64, offer *models.SaleOffer) error {
 	notification.CreatedAt = time.Now().UTC()
-	notification.Title = fmt.Sprintf("Auction ended for %s %s", offer.Car.Model.Manufacturer.Name, offer.Car.Model.Name)
-	notification.Description = fmt.Sprintf("The auction for %s %s has ended. Winner: %s \n Winning bid: %d", offer.Car.Model.Manufacturer.Name, offer.Car.Model.Name, winner, winningBid)
+	notification.Title = fmt.Sprintf(EndAuctionTitleTemplate, offer.Car.Model.Manufacturer.Name, offer.Car.Model.Name)
+	notification.Description = fmt.Sprintf(EndAuctionDescriptionTemplate, offer.Car.Model.Manufacturer.Name, offer.Car.Model.Name, winner, winningBid)
 	return s.NotificationRepository.Create(notification)
 }
 
