f4ed1f5b0a5a713507971ecf5c98d65042eef2dc
KamilMarszalek <kamilmar553@gmail.com>
2025-05-28 19:49:18 +0200

    Add notification message templates for auction events

---
+++ backend/internal/domains/notification/notification_messages.go
@@ -0,0 +1,6 @@
+package notification
+
+var OutbidTitleTemplate = "Someone outbid you on %s %s"
+var OutbidDescriptionTemplate = "New price: %d"
+var EndAuctionTitleTemplate = "Auction ended for %s %s"
+var EndAuctionDescriptionTemplate = "The auction for %s %s has ended. Winner: %s Winning bid: %d"
