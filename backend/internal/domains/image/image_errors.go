154bb7b0a1f89173ef1f75cd1cdad0bf4d30cc2b
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-05-29 20:19:29 +0200

    Bug fixes.

--- backend/internal/domains/image/image_errors.go
+++ backend/internal/domains/image/image_errors.go
@@ -8,11 +8,14 @@ import (
 )
 
 var (
-	ErrOfferNotOwned = errors.New("offer does not belong to logged-in user")
 	ErrTooManyImages = errors.New("you can only attach 10 photos to one offer")
+	ErrOfferNotOwned = errors.New("offer does not belong to logged-in user")
+	ErrZeroImages    = errors.New("offer has no images - there is nothing to delete")
 )
 
 var ErrorMap = map[error]int{
+	ErrTooManyImages:       http.StatusBadRequest,
+	ErrZeroImages:          http.StatusBadRequest,
 	ErrOfferNotOwned:       http.StatusForbidden,
 	gorm.ErrRecordNotFound: http.StatusNotFound,
 }
