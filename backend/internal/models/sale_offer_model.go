7bd697edafba60194a2dc48370c4bd582c928875
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-05-30 17:33:36 +0200

    Enum constraints added.

--- backend/internal/models/sale_offer_model.go
+++ backend/internal/models/sale_offer_model.go
@@ -9,12 +9,12 @@ import (
 type SaleOffer struct {
 	ID          uint              `json:"id" gorm:"primaryKey"`
 	UserID      uint              `json:"user_id"`
-	User        *User             `gorm:"foreignKey:UserID;references:ID"`
 	Description string            `json:"description"`
 	Price       uint              `json:"price"`
-	Margin      enums.MarginValue `json:"margin"`
 	DateOfIssue time.Time         `json:"date_of_issue"`
-	Status      enums.Status      `json:"status"`
+	Margin      enums.MarginValue `json:"margin" gorm:"type:MARGIN_VALUE"`
+	Status      enums.Status      `json:"status" gorm:"type:OFFER_STATUS"`
+	User        *User             `gorm:"foreignKey:UserID;references:ID"`
 	Car         *Car              `gorm:"foreignKey:OfferID;references:ID"`
 	Auction     *Auction          `gorm:"foreignKey:OfferID;references:ID"`
 }
