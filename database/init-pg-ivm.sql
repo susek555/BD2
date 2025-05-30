98847e4494e15930616ed79fb3b2e309492a39ab
KamilMarszalek <kamilmar553@gmail.com>
2025-05-30 15:56:57 +0200

    Add initial view and indexes for user offer interactions

--- database/init-pg-ivm.sql
+++ database/init-pg-ivm.sql
@@ -1,5 +1,40 @@
 CREATE EXTENSION IF NOT EXISTS pg_ivm;
 
--- Necessary privileges
 GRANT USAGE ON SCHEMA public TO bd2_user;
 GRANT CREATE ON SCHEMA public TO bd2_user;
+
+
+SELECT pgivm.create_immv(
+  'offer_creators',          
+  $$ SELECT o.user_id  AS user_id,
+           o.id       AS offer_id
+     FROM  sale_offers o $$);
+
+CREATE UNIQUE INDEX ON offer_creators (user_id, offer_id);
+
+SELECT pgivm.create_immv(
+  'offer_bidders',
+  $$ SELECT b.bidder_id  AS user_id,
+           b.auction_id AS offer_id
+     FROM  bids b
+     JOIN  sale_offers o ON o.id = b.auction_id $$);
+
+CREATE UNIQUE INDEX ON offer_bidders (user_id, offer_id);
+
+
+SELECT pgivm.create_immv(
+  'offer_likers',
+  $$ SELECT l.user_id  AS user_id,
+           l.offer_id AS offer_id
+     FROM  liked_offers l $$);
+
+CREATE UNIQUE INDEX ON offer_likers (user_id, offer_id);
+
+
+CREATE VIEW user_offer_interactions AS
+SELECT user_id, offer_id FROM offer_creators
+UNION
+SELECT user_id, offer_id FROM offer_bidders
+UNION
+SELECT user_id, offer_id FROM offer_likers;
+
