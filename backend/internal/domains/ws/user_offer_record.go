d7525a04e843bfb43e448ca837536d61d692f16c
KamilMarszalek <kamilmar553@gmail.com>
2025-05-30 23:08:54 +0200

    Add GORM column tags to UserOfferRecord fields for database mapping

--- backend/internal/domains/ws/user_offer_record.go
+++ backend/internal/domains/ws/user_offer_record.go
@@ -1,6 +1,6 @@
 package ws
 
 type UserOfferRecord struct {
-	OfferID string `json:"offer_id"`
-	UserID  string `json:"user_id"`
+	OfferID string `json:"offer_id" gorm:"column:offer_id"`
+	UserID  string `json:"user_id" gorm:"column:user_id"`
 }
