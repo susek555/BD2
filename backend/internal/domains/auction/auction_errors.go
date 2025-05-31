1b801cc2039775a275eb4d4ac04bc86486035c55
KamilMarszalek <kamilmar553@gmail.com>
2025-05-31 14:34:51 +0200

    Add error for attempting to buy own auction

--- backend/internal/domains/auction/auction_errors.go
+++ backend/internal/domains/auction/auction_errors.go
@@ -5,3 +5,4 @@ import "errors"
 var ErrBuyNowPriceLessThan1 = errors.New("buy now price must be greater than 0")
 var ErrBuyNowPriceLessThanOfferPrice = errors.New("buy now price must be greater than offer price")
 var ErrModificationForbidden = errors.New("provided offer does not belong to logged in user")
+var ErrAuctionOwnedByUser = errors.New("you cannot buy your own auction")
