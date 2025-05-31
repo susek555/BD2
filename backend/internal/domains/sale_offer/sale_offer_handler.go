f67c28e5f275b267485de2e35e2c4ba39d8c0c96
KamilMarszalek <kamilmar553@gmail.com>
2025-05-31 14:26:42 +0200

    Add Buy handler to process purchase offers

--- backend/internal/domains/sale_offer/sale_offer_handler.go
+++ backend/internal/domains/sale_offer/sale_offer_handler.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/susek555/BD2/car-dealer-api/internal/domains/auth"
 	"github.com/susek555/BD2/car-dealer-api/internal/domains/ws"
 	"github.com/susek555/BD2/car-dealer-api/pkg/custom_errors"
 	"github.com/susek555/BD2/car-dealer-api/pkg/pagination"
@@ -231,6 +232,25 @@ func (h *Handler) GetOrderKeys(c *gin.Context) {
 		keys = append(keys, k)
 	}
 	c.JSON(http.StatusOK, gin.H{"order_keys": keys})
+}
+
+func (h *Handler) Buy(c *gin.Context) {
+	offerID, err := strconv.ParseUint(c.Param("id"), 10, 32)
+	if err != nil {
+		custom_errors.HandleError(c, err, ErrorMap)
+		return
+	}
+	userID, err := auth.GetUserId(c)
+	if err != nil {
+		custom_errors.HandleError(c, err, ErrorMap)
+		return
+	}
+	err = h.service.Buy(uint(offerID), userID)
+	if err != nil {
+		custom_errors.HandleError(c, err, ErrorMap)
+		return
+	}
+	c.Status(http.StatusOK)
 }
 
 func getOptionalUserID(c *gin.Context) *uint {
