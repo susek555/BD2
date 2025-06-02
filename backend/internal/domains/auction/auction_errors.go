c0ed46bb14355b79adf366df95e01ba7f70130ef
KamilMarszalek <kamilmar553@gmail.com>
2025-06-02 23:07:19 +0200

    Add new error for new price validation in auction process

--- backend/internal/domains/auction/auction_errors.go
+++ backend/internal/domains/auction/auction_errors.go
@@ -7,3 +7,4 @@ var ErrBuyNowPriceLessThanOfferPrice = errors.New("buy now price must be greater
 var ErrModificationForbidden = errors.New("provided offer does not belong to logged in user")
 var ErrAuctionOwnedByUser = errors.New("you cannot buy your own auction")
 var ErrAuctionAlreadySold = errors.New("auction is already sold - cannot buy it again")
+var ErrNewPriceLessThanOfferPrice = errors.New("new price must be greater than offer price")
