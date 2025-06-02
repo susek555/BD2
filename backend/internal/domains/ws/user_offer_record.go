54e9cbb5d736a11f3c9a187691e41898383608ca
KamilMarszalek <kamilmar553@gmail.com>
2025-06-02 20:18:48 +0200

    Change UserOfferRecord fields from string to uint for OfferID and UserID

--- backend/internal/domains/ws/user_offer_record.go
+++ backend/internal/domains/ws/user_offer_record.go
@@ -1,6 +1,6 @@
 package ws
 
 type UserOfferRecord struct {
-	OfferID string `json:"offer_id" gorm:"column:offer_id"`
-	UserID  string `json:"user_id" gorm:"column:user_id"`
+	OfferID uint `json:"offer_id" gorm:"column:offer_id"`
+	UserID  uint `json:"user_id" gorm:"column:user_id"`
 }
