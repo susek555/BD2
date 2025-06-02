919eae43cfd588b29492e8d62875db5d78292229
KamilMarszalek <kamilmar553@gmail.com>
2025-06-02 22:56:50 +0200

    Add InitialPrice field to Auction model for enhanced auction details

--- backend/internal/models/auction_model.go
+++ backend/internal/models/auction_model.go
@@ -5,8 +5,9 @@ import (
 )
 
 type Auction struct {
-	OfferID     uint       `json:"id" gorm:"primaryKey"`
-	DateEnd     time.Time  `json:"date_end"`
-	BuyNowPrice uint       `json:"buy_now_price"`
-	Offer       *SaleOffer `gorm:"foreignKey:OfferID;references:ID"`
+	OfferID      uint       `json:"id" gorm:"primaryKey"`
+	DateEnd      time.Time  `json:"date_end"`
+	BuyNowPrice  uint       `json:"buy_now_price"`
+	Offer        *SaleOffer `gorm:"foreignKey:OfferID;references:ID"`
+	InitialPrice uint       `json:"initial_price"`
 }
