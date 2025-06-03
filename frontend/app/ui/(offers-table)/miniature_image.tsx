e84d86332b6d77f5f31e487f726677c3118b4a2a
Michal <01187288@pw.edu.pl>
2025-06-03 16:47:11 +0200

    Fixed not loaded minature images placeholders

--- frontend/app/ui/(offers-table)/miniature_image.tsx
+++ frontend/app/ui/(offers-table)/miniature_image.tsx
@@ -1,10 +1,40 @@
+'use client'
+
+import { useState } from "react";
+
 interface MiniatureImageProps {
     url: string;
 }
 
 export default function MainImage({ url }: MiniatureImageProps) {
+    const [imageSrc, setImageSrc] = useState<string>(url);
+    const [loading, setLoading] = useState(true);
+
+    const handleError = () => {
+        const fallbackImage = "/(home)/car_placeholder.png";
+        setImageSrc(fallbackImage); // Ustawienie obrazka zastępczego
+        console.log(`Failed to load image: ${url}. Placeholder was loaded.`);
+        setLoading(false); // Błąd ładowania kończy stan ładowania
+    };
+
+    const handleLoad = () => {
+        setLoading(false); // Zdjęcie zostało załadowane
+    };
+
     return (
-        // eslint-disable-next-line @next/next/no-img-element
-        <img src={url} alt="Car image" className="h-35 w-70 object-cover rounded" />
+        <div className="relative h-35 w-70 object-cover rounded">
+            {loading && (
+                <div className="absolute inset-0 flex items-center justify-center bg-gray-100">
+                    <div className="loader animate-spin rounded-full border-4 border-gray-300 border-t-gray-600 w-12 h-12"></div>
+                </div>
+            )}
+            <img
+                src={imageSrc}
+                alt="Car image"
+                className={`h-35 w-70 object-cover rounded ${loading ? "hidden" : "block"}`}
+                onLoad={handleLoad}
+                onError={() => handleError()}
+            />
+        </div>
     );
 }
