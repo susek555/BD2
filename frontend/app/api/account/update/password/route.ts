16077ead867d67e8e1b165d5a0e75d2d480caf43
Damian D'Souza <dsouzadamian189@gmail.com>
2025-06-04 23:10:32 +0200

    implement update password endpoint

--- frontend/app/api/account/update/password/route.ts
+++ frontend/app/api/account/update/password/route.ts
@@ -1,5 +1,22 @@
+import { fetchWithRefresh } from '@/app/lib/api/fetchWithRefresh';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function PUT(request: NextRequest) {
-  return NextResponse.json({ status: 200 });
+  const passwordData = await request.json();
+  const response = await fetchWithRefresh(
+    `${process.env.API_URL}/auth/change-password`,
+    {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(passwordData),
+    },
+  );
+
+  return new NextResponse(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers: response.headers,
+  });
 }
