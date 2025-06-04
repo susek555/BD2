6db5ea16139a2397dc461ab826201234bbd148df
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-06-04 19:54:23 +0200

    Removed unnecessary var keyword.

--- backend/internal/domains/auction/auction_errors.go
+++ backend/internal/domains/auction/auction_errors.go
@@ -2,10 +2,12 @@ package auction
 
 import "errors"
 
-var ErrBuyNowPriceLessThan1 = errors.New("buy now price must be greater than 0")
-var ErrBuyNowPriceLessThanOfferPrice = errors.New("buy now price must be greater than offer price")
-var ErrModificationForbidden = errors.New("provided offer does not belong to logged in user")
-var ErrAuctionOwnedByUser = errors.New("you cannot buy your own auction")
-var ErrAuctionAlreadySold = errors.New("auction is already sold - cannot buy it again")
-var ErrNewPriceLessThanOfferPrice = errors.New("new price must be greater than offer price")
-var ErrAuctionNotPublished = errors.New("auction is not published")
+var (
+	ErrBuyNowPriceLessThan1          = errors.New("buy now price must be greater than 0")
+	ErrBuyNowPriceLessThanOfferPrice = errors.New("buy now price must be greater than offer price")
+	ErrModificationForbidden         = errors.New("provided offer does not belong to logged in user")
+	ErrAuctionOwnedByUser            = errors.New("you cannot buy your own auction")
+	ErrAuctionAlreadySold            = errors.New("auction is already sold - cannot buy it again")
+	ErrNewPriceLessThanOfferPrice    = errors.New("new price must be greater than offer price")
+	ErrAuctionNotPublished           = errors.New("auction is not published")
+)
