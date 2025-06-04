7be05c8570d585f24aedf6e9cf4c13eb20c0ebd8
Damian D'Souza <dsouzadamian189@gmail.com>
2025-06-04 23:10:46 +0200

    implement update user profile endpoint

--- frontend/app/api/account/update/profile/route.ts
+++ frontend/app/api/account/update/profile/route.ts
@@ -1,5 +1,20 @@
+import { fetchWithRefresh } from '@/app/lib/api/fetchWithRefresh';
 import { NextRequest, NextResponse } from 'next/server';
+import snakecaseKeys from 'snakecase-keys';
 
 export async function PUT(request: NextRequest) {
-  return NextResponse.json({ status: 200 });
+  const userProfile = snakecaseKeys(await request.json());
+  const response = await fetchWithRefresh(`${process.env.API_URL}/users`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userProfile),
+  });
+
+  return new NextResponse(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers: response.headers,
+  });
 }
