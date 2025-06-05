002e256d8442ef9d92b43ac39e1eb7ce55378b83
Damian D'Souza <dsouzadamian189@gmail.com>
2025-06-05 22:25:41 +0200

    implement user favorites request

--- frontend/app/api/account/favorites/route.ts
+++ frontend/app/api/account/favorites/route.ts
@@ -1,49 +1,19 @@
-import { SaleOffer } from '@/app/lib/definitions/SaleOffer';
+import { fetchWithRefresh } from '@/app/lib/api/fetchWithRefresh';
 import { NextResponse } from 'next/server';
 
-export async function GET(request: Request) {
-  const offers: SaleOffer[] = [
+export async function POST(request: Request) {
+  const body = await request.text();
+  const response = await fetchWithRefresh(
+    `${process.env.API_URL}/sale-offer/filtered`,
     {
-      id: '1',
-      main_url: '/images/offers/1.jpg',
-      name: 'Audi A4',
-      production_year: 2000,
-      mileage: 150000,
-      color: 'Green',
-      price: 10000,
-      is_auction: true,
-      is_liked: true,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: body,
     },
-    {
-      main_url: '/images/offers/2.jpg',
-      id: '2',
-      name: 'Volkswagen Golf',
-      production_year: 2005,
-      mileage: 120000,
-      color: 'Blue',
-      price: 15000,
-      is_auction: false,
-      is_liked: true,
-    },
-    {
-      main_url: '/images/offers/3.jpg',
-      id: '3',
-      name: 'Porsche 911',
-      production_year: 2010,
-      mileage: 80000,
-      color: 'Red',
-      price: 50000,
-      is_auction: true,
-      is_liked: true,
-    },
-  ];
-  const pagination = {
-    total_pages: 1,
-    total_records: 3,
-  };
+  );
 
-  return NextResponse.json({
-    offers,
-    pagination
-  });
+  const data = await response.json();
+  return NextResponse.json(data);
 }
