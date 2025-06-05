ffb86ae4865b5abee7e5ed3b0fcc1a8b822e3605
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-06-05 18:19:41 +0200

    Deleted unused method.

--- backend/internal/models/auction_model.go
+++ backend/internal/models/auction_model.go
@@ -8,10 +8,6 @@ type Auction struct {
 	OfferID      uint       `json:"id" gorm:"primaryKey"`
 	DateEnd      time.Time  `json:"date_end"`
 	BuyNowPrice  uint       `json:"buy_now_price"`
-	Offer        *SaleOffer `gorm:"foreignKey:OfferID;references:ID"`
 	InitialPrice uint       `json:"initial_price"`
-}
-
-func (a *Auction) BelongsToUser(userID uint) bool {
-	return a.Offer.UserID == userID
+	Offer        *SaleOffer `gorm:"foreignKey:OfferID;references:ID"`
 }
