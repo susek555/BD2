872a28ac54b092b8889b96346f1b0b6e1ef5cf28
Damian D'Souza <dsouzadamian189@gmail.com>
2025-06-05 22:55:09 +0200

    implement rating distribution request

--- frontend/app/api/reviews/distribution/[id]/route.ts
+++ frontend/app/api/reviews/distribution/[id]/route.ts
@@ -1,11 +1,9 @@
 export async function GET(
   request: Request,
-  ) {
-  const data = {
-    '1': 33,
-    '2': 33,
-    '3': 0,
-    '4': 0,
-    '5': 33,
-  };  return new Response(JSON.stringify(data));
+  { params }: { params: Promise<{ id: string }> },
+) {
+  const { id } = await params;
+  const response = await fetch(`${process.env.API_URL}/review/frequency/${id}`);
+  const data = await response.json();
+  return new Response(JSON.stringify(data));
 }
