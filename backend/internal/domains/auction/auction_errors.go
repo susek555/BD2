67e54dac0c496fa9eaf60963756bbd276a6ab273
KamilMarszalek <kamilmar553@gmail.com>
2025-06-07 00:34:06 +0200

    Remove unused auction error messages for clarity and maintainability

--- backend/internal/domains/auction/auction_errors.go
+++ backend/internal/domains/auction/auction_errors.go
@@ -5,8 +5,5 @@ import "errors"
 var (
 	ErrBuyNowPriceLessThan1          = errors.New("buy now price must be greater than 0")
 	ErrBuyNowPriceLessThanOfferPrice = errors.New("buy now price must be greater than offer price")
-	ErrAuctionOwnedByUser            = errors.New("you cannot buy your own auction")
-	ErrAuctionNotOwned               = errors.New("provided offer does not belong to logged in user")
 	ErrNewPriceLessThanOfferPrice    = errors.New("new price must be greater than offer price")
-	ErrAuctionNotPublished           = errors.New("auction is not published")
 )
