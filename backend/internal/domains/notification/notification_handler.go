a241c32752144558e61d596d3b8b0a76caf1191f
KamilMarszalek <kamilmar553@gmail.com>
2025-06-07 00:57:30 +0200

    Implement MarkAsSeen and MarkAsUnseen methods in notification handler

--- backend/internal/domains/notification/notification_handler.go
+++ backend/internal/domains/notification/notification_handler.go
@@ -2,6 +2,7 @@ package notification
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/susek555/BD2/car-dealer-api/internal/domains/auth"
@@ -50,4 +51,72 @@ func (h *Handler) GetFilteredNotifications(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, notifications)
+}
+
+// MarkAsSeen godoc
+// @Summary Mark notification as seen
+// @Description Updates the seen status of a specific notification to true for the authenticated user
+// @Tags notifications
+// @Accept json
+// @Produce json
+// @Param id path int true "Notification ID"
+// @Success 200 "Status OK"
+// @Failure 400 {object} custom_errors.HTTPError "Bad Request - Invalid notification ID or update failed"
+// @Failure 401 {object} custom_errors.HTTPError "Unauthorized - User authentication required"
+// @Router /notifications/seen/{id} [put]
+// @Security BearerAuth
+func (h *Handler) MarkAsSeen(c *gin.Context) {
+	notificationID, err := strconv.ParseUint(c.Param("id"), 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, custom_errors.NewHTTPError(err.Error()))
+		return
+	}
+
+	userID, err := auth.GetUserID(c)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, custom_errors.NewHTTPError(err.Error()))
+		return
+	}
+
+	err = h.service.UpdateSeenStatus(uint(notificationID), userID, true)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, custom_errors.NewHTTPError(err.Error()))
+		return
+	}
+
+	c.Status(http.StatusOK)
+}
+
+// MarkAsUnseen godoc
+// @Summary Mark notification as unseen
+// @Description Updates the seen status of a notification to unseen for the authenticated user
+// @Tags notifications
+// @Accept json
+// @Produce json
+// @Param id path int true "Notification ID"
+// @Success 200 "OK"
+// @Failure 400 {object} custom_errors.HTTPError "Bad request"
+// @Failure 401 {object} custom_errors.HTTPError "Unauthorized"
+// @Router /notifications/{id}/unseen [put]
+// @Security BearerAuth
+func (h *Handler) MarkAsUnseen(c *gin.Context) {
+	notificationID, err := strconv.ParseUint(c.Param("id"), 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, custom_errors.NewHTTPError(err.Error()))
+		return
+	}
+
+	userID, err := auth.GetUserID(c)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, custom_errors.NewHTTPError(err.Error()))
+		return
+	}
+
+	err = h.service.UpdateSeenStatus(uint(notificationID), userID, false)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, custom_errors.NewHTTPError(err.Error()))
+		return
+	}
+
+	c.Status(http.StatusOK)
 }
