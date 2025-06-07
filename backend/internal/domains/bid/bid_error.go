39c6a07d6cd8ade55dbecabdfc15ac8a4b7e0a9c
KamilMarszalek <kamilmar553@gmail.com>
2025-06-07 09:33:47 +0200

    Add error for bidder attempting to bid on their own auction

--- backend/internal/domains/bid/bid_error.go
+++ backend/internal/domains/bid/bid_error.go
@@ -3,3 +3,4 @@ package bid
 import "errors"
 
 var ErrAuctionNotPublished = errors.New("auction is not published")
+var ErrBidderIsAuctionOwner = errors.New("bidder cannot bid on their own auction")
