277fda9d3251620c70396c04a9b5bd79d93a0102
Damian D'Souza <dsouzadamian189@gmail.com>
2025-06-09 19:49:06 +0200

    implement listing delete endpoint

--- frontend/app/api/listing/[id]/route.ts
+++ frontend/app/api/listing/[id]/route.ts
@@ -1,29 +1,13 @@
-export async function PUT(
-  request: Request,
-  { params }: { params: Promise<{ id: string }> },
-) {
-  const { id } = await params;
-  console.log('Update listing id:', id);
-  // TODO implement API call to update listing
-  return Response.json({ success: true });
-}
-
-export async function GET(
-  request: Request,
-  { params }: { params: Promise<{ id: string }> },
-) {
-  const { id } = await params;
-  console.log('Get listing id:', id);
-  // TODO implement API call to get listing
-  return Response.json({ id: id });
-}
+import { fetchWithRefresh } from '@/app/lib/api/fetchWithRefresh';
 
 export async function DELETE(
   request: Request,
   { params }: { params: Promise<{ id: string }> },
 ) {
   const { id } = await params;
-  console.log('Deleted listing id:', id);
-  // TODO implement API call to delete listing
-  return Response.json({ success: true });
+  const response = await fetchWithRefresh(
+    `${process.env.API_URL}/sale-offer/id/${id}`,
+  );
+
+  return response.json();
 }
