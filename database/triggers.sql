e329cf124928ddedcbceebc8e88bff2abe3dc1e7
KamilMarszalek <kamilmar553@gmail.com>
2025-06-09 16:21:18 +0200

    Add trigger to delete unsold offers when a user is deleted

--- database/triggers.sql
+++ database/triggers.sql
@@ -9,3 +9,21 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE TRIGGER auction_insert_trigger
 AFTER INSERT ON auctions
 FOR EACH ROW EXECUTE FUNCTION set_is_auction_true();
+
+CREATE OR REPLACE FUNCTION delete_unsold_offers()
+RETURNS TRIGGER AS $$
+BEGIN
+    DELETE FROM sale_offers so 
+    WHERE so.user_id = OLD.id
+    AND NOT EXISTS (
+        SELECT 1 FROM purchases p 
+        WHERE p.offer_id = so.id
+    );
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER delete_unsold_offers_trigger
+BEFORE DELETE ON users
+FOR EACH ROW
+EXECUTE FUNCTION delete_unsold_offers();
