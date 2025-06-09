d5bcfe42d37774ac2d64e5fe999b8cf5ad9d0264
KamilMarszalek <kamilmar553@gmail.com>
2025-06-09 21:15:19 +0200

    Refactor MapToProcessingDTO to accept Offer parameter for improved bid processing

--- backend/internal/domains/bid/bid_mapping.go
+++ backend/internal/domains/bid/bid_mapping.go
@@ -3,15 +3,16 @@ package bid
 import (
 	"time"
 
+	"github.com/susek555/BD2/car-dealer-api/internal/domains/notification"
 	"github.com/susek555/BD2/car-dealer-api/internal/models"
 )
 
-func MapToProcessingDTO(b *models.Bid) *ProcessingBidDTO {
+func MapToProcessingDTO(b *models.Bid, offer notification.SaleOfferInterface) *ProcessingBidDTO {
 	return &ProcessingBidDTO{
 		AuctionID: b.AuctionID,
 		BidderID:  b.BidderID,
 		Amount:    b.Amount,
-		Auction:   b.Auction,
+		Offer:     offer,
 	}
 }
 
