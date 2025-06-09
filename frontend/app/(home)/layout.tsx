9d4fc174fcca9826aae6dff4bedb8804dee8bbfd
Damian D'Souza <dsouzadamian189@gmail.com>
2025-06-09 16:12:19 +0200

    add Toaster to main layout

--- frontend/app/(home)/layout.tsx
+++ frontend/app/(home)/layout.tsx
@@ -1,14 +1,17 @@
-import { TopBar } from "@/app/ui/(topbar)/topbar";
+import { TopBar } from '@/app/ui/(topbar)/topbar';
+import { Toaster } from 'react-hot-toast';
 
-export default function Layout({children}: {children: React.ReactNode}) {
-    return (
-      <div className="flex flex-grow flex-col">
-        <div className="h-full flex-none md:h-10">
-          <TopBar />
-        </div>
-        {children}
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className='flex flex-grow flex-col'>
+      <div className='h-full flex-none md:h-10'>
+        <TopBar />
       </div>
-    )
+      <Toaster />
+
+      {children}
+    </div>
+  );
 }
 
 // export default function Layout({ children }: { children: React.ReactNode }) {
